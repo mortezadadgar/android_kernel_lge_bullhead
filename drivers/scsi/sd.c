27dd7f2158f67402ac1a6e02d60806d49cd1b895
Derek Basehore <dbasehore@chromium.org>
2012-08-08 14:53:08 -0700

    CHROMIUM: scsi: don't wait for disk to wakeup on resume
    This is done in the same way as the ata ports. Instead of waiting for the disk
    to resume in sd_resume, we put a function into the workqueue and return. We also
    make sure that a race does not happen between resume and suspend. This is done
    by making the suspend code wait until the work_struct in the ata_port finishes.
    
    There does not seem to be any differences between what this change does and what
    happened before when it breaks. It seems that the disk will be woken up in
    another place if the function to wake it up in resume fails.
    
    Note: I have not decided exactly what to do for error paths and therefore there
    are currently none when ata ports or disks fail to resume
    
    BUG=chrome-os-partner:11780
    TEST=power_Resume, powerd_suspend, opening and closing laptop, commenting out
    the line that wakes up the disk and seeing the system behaviour (same before and
    after change)
    
    Change-Id: I05ba125ef55c2cc9e6849d9fec693c574f81221a
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/29673

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -51,6 +51,7 @@
 #include <linux/async.h>
 #include <linux/slab.h>
 #include <linux/pm_runtime.h>
+#include <linux/workqueue.h>
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
 
@@ -106,6 +107,7 @@ static int  sd_probe(struct device *);
 static int  sd_remove(struct device *);
 static void sd_shutdown(struct device *);
 static int sd_suspend(struct device *);
+static int __sd_resume(struct work_struct *work);
 static int sd_resume(struct device *);
 static void sd_rescan(struct device *);
 static int sd_done(struct scsi_cmnd *);
@@ -2888,6 +2890,7 @@ static int sd_probe(struct device *dev)
 	sdkp = kzalloc(sizeof(*sdkp), GFP_KERNEL);
 	if (!sdkp)
 		goto out;
+	INIT_WORK(&sdkp->resume_work, __sd_resume);
 
 	gd = alloc_disk(SD_MINORS);
 	if (!gd)
@@ -3079,6 +3082,10 @@ static int sd_suspend(struct device *dev)
 	if (!sdkp)
 		return 0;	/* this can happen */
 
+	/* Avoid race condition with resume */
+	if (work_pending(&sdkp->resume_work))
+		flush_work_sync(&sdkp->resume_work);
+
 	if (sdkp->WCE) {
 		sd_printk(KERN_NOTICE, sdkp, "Synchronizing SCSI cache\n");
 		ret = sd_sync_cache(sdkp);
@@ -3096,9 +3103,10 @@ done:
 	return ret;
 }
 
-static int sd_resume(struct device *dev)
+static int __sd_resume(struct work_struct *work)
 {
-	struct scsi_disk *sdkp = scsi_disk_get_from_dev(dev);
+	struct scsi_disk *sdkp = container_of(work, struct scsi_disk,
+		resume_work);
 	int ret = 0;
 
 	if (!sdkp->device->manage_start_stop)
@@ -3110,6 +3118,16 @@ static int sd_resume(struct device *dev)
 done:
 	scsi_disk_put(sdkp);
 	return ret;
+}
+
+static int sd_resume(struct device *dev)
+{
+	struct scsi_disk *sdkp = scsi_disk_get_from_dev(dev);
+
+	PREPARE_WORK(&sdkp->resume_work, __sd_resume);
+	schedule_work(&sdkp->resume_work);
+
+	return 0;
 }
 
 /**
