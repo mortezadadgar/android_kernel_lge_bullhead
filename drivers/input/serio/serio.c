b7e70fbf750ebff383fa4908a14cbb5c3497a1ad
Shawn Nematbakhsh <shawnn@chromium.org>
2013-03-21 14:34:05 -0700

    CHROMIUM: input: serio - Remove unnecessary port rescans.
    When a serio_interrupt is received, if no driver is attached to the
    port, a rescan event will be queued. Between the time the event is
    queued and processed, it is possible that a driver will become attached
    to the port (ex. through a previously queued ATTACH_DRIVER event). In
    this case, the the driver / device will needlessly be initialized twice.
    
    This change clears the queue of rescan events for a port if a driver
    becomes attached to the port.
    
    TEST=Manual. Verify that rescan does not occur due to trackpad interrupt
    received in early boot on Stout platform.
    BUG=chrome-os-partner:18327
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I43baf609e65c716628d37d157b26194085440850
    Reviewed-on: https://gerrit.chromium.org/gerrit/46183
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-by: Chung-yih Wang <cywang@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/58742
    Tested-by: Sonny Rao <sonnyrao@chromium.org>
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>

--- drivers/input/serio/serio.c
+++ drivers/input/serio/serio.c
@@ -771,10 +771,14 @@ static struct driver_attribute serio_driver_attrs[] = {
 
 static int serio_driver_probe(struct device *dev)
 {
+	int retval;
 	struct serio *serio = to_serio_port(dev);
 	struct serio_driver *drv = to_serio_driver(dev->driver);
 
-	return serio_connect_driver(serio, drv);
+	retval = serio_connect_driver(serio, drv);
+	if (!retval)
+		serio_remove_duplicate_events(serio, SERIO_RESCAN_PORT);
+	return retval;
 }
 
 static int serio_driver_remove(struct device *dev)
