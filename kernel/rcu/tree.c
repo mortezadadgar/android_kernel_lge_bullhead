d5a6d1388eb4487ff1627279a85db2930edfdeec
Borislav Petkov <bp@alien8.de>
2013-04-22 00:12:42 +0200

    rcu: Expedite grace periods during suspend/resume
    CONFIG_RCU_FAST_NO_HZ can increase grace-period durations by up to
    a factor of four, which can result in long suspend and resume times.
    Thus, this commit temporarily switches to expedited grace periods when
    suspending the box and return to normal settings when resuming.  Similar
    logic is applied to hibernation.
    
    Because expedited grace periods are of dubious benefit on very large
    systems, so this commit restricts their automated use during suspend
    and resume to systems of 256 or fewer CPUs.  (Some day a number of
    Linux-kernel facilities, including RCU's expedited grace periods,
    will be more scalable, but I need to see bug reports first.)
    
    [ paulmck: This also papers over an audio/irq bug, but hopefully that will
      be fixed soon. ]
    
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -54,6 +54,7 @@
 #include <linux/delay.h>
 #include <linux/stop_machine.h>
 #include <linux/random.h>
+#include <linux/suspend.h>
 
 #include "tree.h"
 #include <trace/events/rcu.h>
@@ -3134,6 +3135,25 @@ static int __cpuinit rcu_cpu_notify(struct notifier_block *self,
 	return NOTIFY_OK;
 }
 
+static int rcu_pm_notify(struct notifier_block *self,
+			 unsigned long action, void *hcpu)
+{
+	switch (action) {
+	case PM_HIBERNATION_PREPARE:
+	case PM_SUSPEND_PREPARE:
+		if (nr_cpu_ids <= 256) /* Expediting bad for large systems. */
+			rcu_expedited = 1;
+		break;
+	case PM_POST_HIBERNATION:
+	case PM_POST_SUSPEND:
+		rcu_expedited = 0;
+		break;
+	default:
+		break;
+	}
+	return NOTIFY_OK;
+}
+
 /*
  * Spawn the kthread that handles this RCU flavor's grace periods.
  */
@@ -3360,6 +3380,7 @@ void __init rcu_init(void)
 	 * or the scheduler are operational.
 	 */
 	cpu_notifier(rcu_cpu_notify, 0);
+	pm_notifier(rcu_pm_notify, 0);
 	for_each_online_cpu(cpu)
 		rcu_cpu_notify(NULL, CPU_UP_PREPARE, (void *)(long)cpu);
 }
