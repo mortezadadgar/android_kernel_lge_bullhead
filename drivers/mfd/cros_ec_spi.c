4f89f2ebf9f02848d3b8b30ce1baebda3eb4e55e
Doug Anderson <dianders@chromium.org>
2013-05-06 16:17:14 -0700

    mfd: Add mutex to cros_ec_spi
    The main transfer function for cros_ec_spi can be called by more than
    one client at a time.  Make sure that those clients don't stomp on
    each other by locking the bus for the duration of the transfer
    function.
    
    BUG=chrome-os-partner:19249
    TEST=Change the code to use "mutex_trylock" instead of "mutex_lock"
    and print when the "try" failed.  Now use pydevi2c and loop with some
    large transfers like:
      >>> tps = I2CDevice(20, 0x48, force=True)
      >>> while True: x = tps.Get(0, 230)
    While doing this, bang on the keyboard.  See messages like:
      [  100.094000] mutex actually mattered!
      [  100.132138] mutex actually mattered!
    
    Change-Id: Ie7820d1d25c7e61a8250c7a6403ce17c897e7e5b
    Original-Change-Id:  I4910b4b4d22d83f3fe5e1e3f2298b3efe81e3aaa
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Doug Anderson <dianders@google.com>
    Reviewed-by: David Hendricks <dhendrix@google.com>
    Reviewed-by: Simon Glass <sjg@google.com>
    Commit-Queue: Doug Anderson <dianders@google.com>
    (cherry picked from commit 1802db9a61d4055bc5e0ffa28955d43c14e90832)
    Reviewed-on: https://gerrit.chromium.org/gerrit/56516
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/mfd/cros_ec_spi.c
+++ drivers/mfd/cros_ec_spi.c
@@ -61,10 +61,12 @@
  * @spi: SPI device we are connected to
  * @last_transfer_ns: time that we last finished a transfer, or 0 if there
  *	if no record
+ * @lock: mutex to ensure only one user of cros_ec_command_spi_xfer at a time
  */
 struct cros_ec_spi {
 	struct spi_device *spi;
 	s64 last_transfer_ns;
+	struct mutex lock;
 };
 
 static void debug_packet(struct device *dev, const char *name, u8 *ptr,
@@ -201,6 +203,13 @@ static int cros_ec_command_spi_xfer(struct cros_ec_device *ec_dev,
 	int ret = 0, final_ret;
 	struct timespec ts;
 
+	/*
+	 * We have the shared ec_dev buffer plus we do lots of separate spi_sync
+	 * calls, so we need to make sure only one person is using this at a
+	 * time.
+	 */
+	mutex_lock(&ec_spi->lock);
+
 	len = cros_ec_prepare_tx(ec_dev, ec_msg);
 	dev_dbg(ec_dev->dev, "prepared, len=%d\n", len);
 
@@ -242,7 +251,7 @@ static int cros_ec_command_spi_xfer(struct cros_ec_device *ec_dev,
 		ret = final_ret;
 	if (ret < 0) {
 		dev_err(ec_dev->dev, "spi transfer failed: %d\n", ret);
-		return ret;
+		goto exit;
 	}
 
 	/* check response error code */
@@ -251,14 +260,16 @@ static int cros_ec_command_spi_xfer(struct cros_ec_device *ec_dev,
 		dev_warn(ec_dev->dev, "command 0x%02x returned an error %d\n",
 			 ec_msg->cmd, ptr[0]);
 		debug_packet(ec_dev->dev, "in_err", ptr, len);
-		return -EINVAL;
+		ret = -EINVAL;
+		goto exit;
 	}
 	len = ptr[1];
 	sum = ptr[0] + ptr[1];
 	if (len > ec_msg->in_len) {
 		dev_err(ec_dev->dev, "packet too long (%d bytes, expected %d)",
 			len, ec_msg->in_len);
-		return -ENOSPC;
+		ret = -ENOSPC;
+		goto exit;
 	}
 
 	/* copy response packet payload and compute checksum */
@@ -275,10 +286,14 @@ static int cros_ec_command_spi_xfer(struct cros_ec_device *ec_dev,
 		dev_err(ec_dev->dev,
 			"bad packet checksum, expected %02x, got %02x\n",
 			sum, ptr[len + 2]);
-		return -EBADMSG;
+		ret = -EBADMSG;
+		goto exit;
 	}
 
-	return 0;
+	ret = 0;
+exit:
+	mutex_unlock(&ec_spi->lock);
+	return ret;
 }
 
 static int cros_ec_probe_spi(struct spi_device *spi)
@@ -298,6 +313,7 @@ static int cros_ec_probe_spi(struct spi_device *spi)
 	if (ec_spi == NULL)
 		return -ENOMEM;
 	ec_spi->spi = spi;
+	mutex_init(&ec_spi->lock);
 	ec_dev = devm_kzalloc(dev, sizeof(*ec_dev), GFP_KERNEL);
 	if (!ec_dev)
 		return -ENOMEM;
