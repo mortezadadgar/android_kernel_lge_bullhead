023f9c35eadbc7488c59c611315b110633678954
Joseph Lo <josephl@nvidia.com>
2013-06-04 18:47:32 +0800

    UPSTREAM: ARM: tegra: cpuidle: move the init function behind the suspend init function
    One of the state of CPUidle on Tegra can power gate the CPU and the
    vdd_cpu rail. But it depends on some configurations from DT and a common
    hook function for different Tegra SoCs to power gate the CPU rail. And
    these stuffs are initialized after common Tegra suspend init function. So
    we move the CPUidle init behind the suspend init function. And making the
    CPUidle driver more generic.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    (cherry picked from commit e22dc2b25655706751789a8d035b57bf04299cbd)
    
    Change-Id: Iacc63824a655834af5a785d44ef36e751eae0747
    Reviewed-on: https://gerrit.chromium.org/gerrit/64887
    Tested-by: Joseph Lo <josephl@nvidia.com>
    Commit-Queue: Olof Johansson <olofj@chromium.org>
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -30,6 +30,7 @@
 
 #include "board.h"
 #include "common.h"
+#include "cpuidle.h"
 #include "fuse.h"
 #include "iomap.h"
 #include "irq.h"
@@ -109,5 +110,6 @@ void __init tegra_init_early(void)
 void __init tegra_init_late(void)
 {
 	tegra_init_suspend();
+	tegra_cpuidle_init();
 	tegra_powergate_debugfs_init();
 }
