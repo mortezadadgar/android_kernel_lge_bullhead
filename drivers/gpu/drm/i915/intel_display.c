a26dcdbb4bdfe82f8f354f28c359350018fbd8bb
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2013-07-12 16:50:57 +0300

    BACKPORT: drm/i915: move error state to own compilation unit
    Move error state generation and stringification to it's
    own compilation unit. Sysfs also uses this so it can't be
    under CONFIG_DEBUG_FS
    
    This fixes a regression introduced in
    
    commit ef86ddced720fddc3835558447a7f594d3609c73
    Author: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Date:   Thu Jun 6 17:38:54 2013 +0300
    
        drm/i915: add error_state sysfs entry
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=66814
    Signed-off-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 84734a049d0ef2f6f5fb0a1fe060cd51480dd855)

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -10209,9 +10209,6 @@ int intel_modeset_vga_set_state(struct drm_device *dev, bool state)
 	return 0;
 }
 
-#ifdef CONFIG_DEBUG_FS
-#include <linux/seq_file.h>
-
 struct intel_display_error_state {
 
 	u32 power_well_driver;
@@ -10383,4 +10380,3 @@ intel_display_print_error_state(struct drm_i915_error_state_buf *m,
 		err_printf(m, "  VSYNC: %08x\n", error->transcoder[i].vsync);
 	}
 }
-#endif
