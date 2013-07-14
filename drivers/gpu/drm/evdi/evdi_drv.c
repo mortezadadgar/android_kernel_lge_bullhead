a6f5090f05c9f42065f3eb624474553d0eb5610b
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-07-14 16:05:51 -0700

    BACKPORT: sysfs.h: add __ATTR_RW() macro
    A number of parts of the kernel created their own version of this, might
    as well have the sysfs core provide it instead.
    
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    BUG=chrome-os-partner:57708
    TEST=Build
    
    Change-Id: Ie5ed104789159b7bda6f2e554fee577036665464
    (cherry picked from commit b9b3259746d77f4fcb786e2a43c25bcc40773755)
    Conflicts:
    	kernel/events/core.c - code not there
    	drivers/gpu/drm/evdi/evdi_drv.c - removed now duplicate define
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/425204
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- drivers/gpu/drm/evdi/evdi_drv.c
+++ drivers/gpu/drm/evdi/evdi_drv.c
@@ -232,9 +232,6 @@ static ssize_t loglevel_store(struct device *dev,
 	return count;
 }
 
-#define __ATTR_RW(_name)        __ATTR(_name, (S_IWUSR | S_IRUGO),      \
-					_name##_show, _name##_store)
-
 #define __ATTR_WO(_name)        {                                       \
 	.attr = { .name = __stringify(_name), .mode = S_IWUSR },        \
 	.store = _name##_store,                                         \
