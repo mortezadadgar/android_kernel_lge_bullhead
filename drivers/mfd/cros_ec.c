f252b1cc8736ba0e779ad00d33ee696cdf8c693b
Andrew Bresticker <abrestic@chromium.org>
2013-07-21 21:59:12 -0700

    cros_ec: move EC interrupt to cros_ec_keyb
    If we receive EC interrupts after the cros_ec driver has probed, but
    before the cros_ec_keyb driver has probed, the cros_ec IRQ handler
    will not run the cros_ec_keyb notifier and the EC will leave the IRQ
    line asserted.  The cros_ec IRQ handler then returns IRQ_HANDLED and
    the resulting flood of interrupts causes the machine to hang.
    
    Since the EC interrupt is currently only used for the keyboard, move
    the setup and handling of the EC interrupt to the cros_ec_keyb driver.
    
    BUG=chrome-os-partner:20378
    TEST=Boot Pit and bang on the keyboard during boot, before the
    cros_ec_keyb driver probes, and observe that the system no longer hangs.
    Also check that keyboard input still works and that the keyboard can
    be used to wake from idle suspend and s2r.
    
    Change-Id: I3630a4c4ae848e25a074d1df39011dda27a3b1d1
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/62800
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>

--- drivers/mfd/cros_ec.c
+++ drivers/mfd/cros_ec.c
@@ -45,18 +45,6 @@ int cros_ec_prepare_tx(struct cros_ec_device *ec_dev,
 }
 EXPORT_SYMBOL(cros_ec_prepare_tx);
 
-static irqreturn_t ec_irq_thread(int irq, void *data)
-{
-	struct cros_ec_device *ec_dev = data;
-
-	if (device_may_wakeup(ec_dev->dev))
-		pm_wakeup_event(ec_dev->dev, 0);
-
-	blocking_notifier_call_chain(&ec_dev->event_notifier, 1, ec_dev);
-
-	return IRQ_HANDLED;
-}
-
 static struct mfd_cell cros_devs[] = {
 	{
 		.name = "cros-ec-keyb",
@@ -84,8 +72,6 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 	struct device *dev = ec_dev->dev;
 	int err = 0;
 
-	BLOCKING_INIT_NOTIFIER_HEAD(&ec_dev->event_notifier);
-
 	if (ec_dev->din_size) {
 		ec_dev->din = kmalloc(ec_dev->din_size, GFP_KERNEL);
 		if (!ec_dev->din) {
@@ -98,17 +84,6 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 		if (!ec_dev->dout) {
 			err = -ENOMEM;
 			goto fail_dout;
-		}
-	}
-
-	if (ec_dev->irq) {
-		err = request_threaded_irq(ec_dev->irq, NULL, ec_irq_thread,
-					   IRQF_TRIGGER_LOW | IRQF_ONESHOT,
-					   "chromeos-ec", ec_dev);
-		if (err) {
-			dev_err(dev, "request irq %d: error %d\n",
-				ec_dev->irq, err);
-			goto fail_irq;
 		}
 	}
 
@@ -125,9 +100,6 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 	return 0;
 
 fail_mfd:
-	if (ec_dev->irq)
-		free_irq(ec_dev->irq, ec_dev);
-fail_irq:
 	if (ec_dev->dout_size)
 		kfree(ec_dev->dout);
 fail_dout:
@@ -141,8 +113,6 @@ EXPORT_SYMBOL(cros_ec_register);
 int cros_ec_remove(struct cros_ec_device *ec_dev)
 {
 	mfd_remove_devices(ec_dev->dev);
-	if (ec_dev->irq)
-		free_irq(ec_dev->irq, ec_dev);
 	if (ec_dev->dout_size)
 		kfree(ec_dev->dout);
 	if (ec_dev->din_size)
