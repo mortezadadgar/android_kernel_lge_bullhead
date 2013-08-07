5ce2232c6267d916378609b58378c2a1c96b53ee
Rob Clark <robdclark@gmail.com>
2013-08-07 13:41:24 -0400

    BACKPORT: drm/gem: add shmem get/put page helpers
    Basically just extracting some code duplicated in gma500, omapdrm, udl,
    and upcoming msm driver.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit bcc5c9d50e93bb7d949f6f38063b62dd35ca84d1)
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    
    BUG=chromium:478339
    TEST=build and run on nyan and rambi
    
    Change-Id: Ifdbc63f9a2ba2d72ff7215b9c8c08817ab2b6cdb
    Reviewed-on: https://chromium-review.googlesource.com/266234
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -1623,6 +1623,10 @@ int drm_gem_handle_delete(struct drm_file *filp, u32 handle);
 void drm_gem_free_mmap_offset(struct drm_gem_object *obj);
 int drm_gem_create_mmap_offset(struct drm_gem_object *obj);
 
+struct page **drm_gem_get_pages(struct drm_gem_object *obj, gfp_t gfpmask);
+void drm_gem_put_pages(struct drm_gem_object *obj, struct page **pages,
+		bool dirty, bool accessed);
+
 struct drm_gem_object *drm_gem_object_lookup(struct drm_device *dev,
 					     struct drm_file *filp,
 					     u32 handle);
