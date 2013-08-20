ccee28e0ee7e7cc431d414bd8d0814a0229e1d25
Stephen Warren <swarren@nvidia.com>
2013-08-20 15:47:38 -0600

    UPSTREAM: ARM: tegra: remove common.c
    common.c was create to contain code shared across the various Tegra board
    files. There is now only one board file, tegra.c. So, move the code there.
    One exception is the PMC reboot routine, which moves to pmc.c, and now
    takes advantage of the 'standard' tegra_pmc_readl/writel functions.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    (cherry picked from commit 51100bdc067706624f0ffe285e63a6099336e11a)
    
    Conflicts:
    	arch/arm/mach-tegra/board.h
    	arch/arm/mach-tegra/common.c
    	arch/arm/mach-tegra/tegra.c
    
    BUG=None
    TEST=Build and boot on Nyan
    
    Change-Id: I2dcebeab64648c9b8398681cd39d572d09080bf2
    [abrestic: fixed conflict due to move of PCIe and cpufreq drivers]
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/177900
    Reviewed-by: Rhyland Klein <rklein@nvidia.com>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- arch/arm/mach-tegra/board.h
+++ arch/arm/mach-tegra/board.h
@@ -24,15 +24,9 @@
 
 #include <linux/types.h>
 
-void tegra_assert_system_reset(char mode, const char *cmd);
-
-void __init tegra_init_early(void);
 void __init tegra_map_common_io(void);
 void __init tegra_init_irq(void);
-void __init tegra_dt_init_irq(void);
 int __init tegra_pcie_init(bool init_port0, bool init_port1);
-
-void tegra_init_late(void);
 
 #ifdef CONFIG_DEBUG_FS
 int tegra_clk_debugfs_init(void);
