1f3ed33144e496ab55f919aea0c66cf646269970
Peter De Schrijver <pdeschrijver@nvidia.com>
2013-08-22 18:44:06 +0300

    clk: tegra: simplify periph clock data
    This patch determines the register bank for clock enable/disable and reset
    based on the clock ID instead of hardcoding it in the tables describing the
    clocks. This results in less data to be maintained in the tables, making the
    code easier to understand. The full benefit of the change will be realized once
    also other clocktypes will be table based.
    
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    
    http://patchwork.ozlabs.org/patch/285196/
    BUG=None
    TEST=Verified booting to console
    
    Change-Id: Ib923567991acb57344c884d96c4c8c9a7c16e78d
    Signed-off-by: Rhyland Klein <rklein@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/173269
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Commit-Queue: Andrew Bresticker <abrestic@chromium.org>

--- drivers/clk/tegra/clk.h
+++ drivers/clk/tegra/clk.h
@@ -402,8 +402,7 @@ void tegra_periph_reset(struct tegra_clk_periph_gate *gate, bool assert);
 extern const struct clk_ops tegra_clk_periph_gate_ops;
 struct clk *tegra_clk_register_periph_gate(const char *name,
 		const char *parent_name, u8 gate_flags, void __iomem *clk_base,
-		unsigned long flags, int clk_num,
-		struct tegra_clk_periph_regs *pregs, int *enable_refcnt);
+		unsigned long flags, int clk_num, int *enable_refcnt);
 
 /**
  * struct clk-periph - peripheral clock
@@ -445,7 +444,7 @@ struct clk *tegra_clk_register_periph_nodiv(const char *name,
 
 #define TEGRA_CLK_PERIPH(_mux_shift, _mux_mask, _mux_flags,		\
 			 _div_shift, _div_width, _div_frac_width,	\
-			 _div_flags, _clk_num, _enb_refcnt, _regs,	\
+			 _div_flags, _clk_num, _enb_refcnt,		\
 			 _gate_flags, _table)				\
 	{								\
 		.mux = {						\
@@ -464,7 +463,6 @@ struct clk *tegra_clk_register_periph_nodiv(const char *name,
 			.flags = _gate_flags,				\
 			.clk_num = _clk_num,				\
 			.enable_refcnt = _enb_refcnt,			\
-			.regs = _regs,					\
 		},							\
 		.mux_ops = &clk_mux_ops,				\
 		.div_ops = &tegra_clk_frac_div_ops,			\
@@ -485,7 +483,7 @@ struct tegra_periph_init_data {
 
 #define TEGRA_INIT_DATA_TABLE(_name, _con_id, _dev_id, _parent_names, _offset,\
 			_mux_shift, _mux_mask, _mux_flags, _div_shift,	\
-			_div_width, _div_frac_width, _div_flags, _regs,	\
+			_div_width, _div_frac_width, _div_flags,	\
 			_clk_num, _enb_refcnt, _gate_flags, _clk_id, _table,\
 			_flags) \
 	{								\
@@ -497,7 +495,7 @@ struct tegra_periph_init_data {
 					   _mux_flags, _div_shift,	\
 					   _div_width, _div_frac_width,	\
 					   _div_flags, _clk_num,	\
-					   _enb_refcnt, _regs,		\
+					   _enb_refcnt,			\
 					   _gate_flags, _table),	\
 		.offset = _offset,					\
 		.con_id = _con_id,					\
@@ -507,12 +505,12 @@ struct tegra_periph_init_data {
 
 #define TEGRA_INIT_DATA(_name, _con_id, _dev_id, _parent_names, _offset,\
 			_mux_shift, _mux_width, _mux_flags, _div_shift,	\
-			_div_width, _div_frac_width, _div_flags, _regs,	\
+			_div_width, _div_frac_width, _div_flags, \
 			_clk_num, _enb_refcnt, _gate_flags, _clk_id)	\
 	TEGRA_INIT_DATA_TABLE(_name, _con_id, _dev_id, _parent_names, _offset,\
 			_mux_shift, BIT(_mux_width) - 1, _mux_flags,	\
 			_div_shift, _div_width, _div_frac_width, _div_flags, \
-			_regs, _clk_num, _enb_refcnt, _gate_flags, _clk_id,\
+			_clk_num, _enb_refcnt, _gate_flags, _clk_id,\
 			NULL, 0)
 
 /**
@@ -588,6 +586,9 @@ void tegra_init_from_table(struct tegra_clk_init_table *tbl,
 
 void tegra_init_dup_clks(struct tegra_clk_duplicate *dup_list,
 		struct clk *clks[], int clk_max);
+
+struct tegra_clk_periph_regs *get_reg_bank(int clkid);
+int tegra_clk_set_periph_banks(int num);
 
 void tegra114_clock_tune_cpu_trimmers_high(void);
 void tegra114_clock_tune_cpu_trimmers_low(void);
