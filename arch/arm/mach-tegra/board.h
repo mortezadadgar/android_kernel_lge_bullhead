1d11bebdb1464689b984d92da92bfc62a7e5d61a
Vince Hsu <vinceh@nvidia.com>
2013-10-31 10:33:09 +0800

    CHROMIUM: tegra: remove platform dependency for powergate debugfs
    We have more SoCs supporting powergate now. The debugfs code of powergate
    should not depend on Tegra20 anymore.
    
    BUG=None
    TEST=Verified the /sys/kernel/debug/powergate information
    
    Change-Id: Ic82a8fd77ae2a1e7c32724217c71a5b214704fda
    Signed-off-by: Vince Hsu <vinceh@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/175246
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Commit-Queue: Andrew Bresticker <abrestic@chromium.org>

--- arch/arm/mach-tegra/board.h
+++ arch/arm/mach-tegra/board.h
@@ -41,7 +41,7 @@ static inline int tegra_clk_debugfs_init(void) { return 0; }
 #endif
 
 int __init tegra_powergate_init(void);
-#if defined(CONFIG_ARCH_TEGRA_2x_SOC) && defined(CONFIG_DEBUG_FS)
+#ifdef CONFIG_DEBUG_FS
 int __init tegra_powergate_debugfs_init(void);
 #else
 static inline int tegra_powergate_debugfs_init(void) { return 0; }
