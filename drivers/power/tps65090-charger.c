0d6f6cb7b49c36cfe716cf1c8e4318e06dfeb9b6
Doug Anderson <dianders@chromium.org>
2013-11-04 13:35:02 -0800

    mfd: tps65090: Stop polling for the charger during suspend time
    It's not a good idea to keep polling for the charger during the
    suspend process since any devices that we depend on may have already
    suspended.  We'll simply mark ourselves as freezable and let the kernel
    do the work of suspending and resuming our thread.
    
    BUG=chrome-os-partner:23864
    TEST=No more tps65090_charger_isr() errors in logs
    
    Original-Change-Id: I587e36b35a92a8b056d2e49fdd43d089d1979338
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/175653
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    
    (cherry-picked commit from 2825befd7635b295e7a7fa1e6e716a387bbf59b5)
    [bleung : cherry-picked from chromeos-3.8 to chromeos-3.10]
    Signed-off-by: Benson Leung <bleung@chromium.org>
    
    Change-Id: I1a2f43c9d7d8d84dbdcc66237512bc9ec491d662
    Reviewed-on: https://chromium-review.googlesource.com/176447
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Commit-Queue: Benson Leung <bleung@chromium.org>
    Tested-by: Benson Leung <bleung@chromium.org>

--- drivers/power/tps65090-charger.c
+++ drivers/power/tps65090-charger.c
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <linux/err.h>
+#include <linux/freezer.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
@@ -238,8 +239,11 @@ static struct tps65090_platform_data *
 
 static int tps65090_charger_poll_task(void *data)
 {
+	set_freezable();
+
 	while (!kthread_should_stop()) {
 		schedule_timeout_interruptible(POLL_INTERVAL);
+		try_to_freeze();
 		tps65090_charger_isr(-1, data);
 	}
 	return 0;
