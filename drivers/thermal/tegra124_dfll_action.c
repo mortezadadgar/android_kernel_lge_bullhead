0beae86c287252a52ecf77f1f897e3da0d1e90fd
Wei Ni <wni@nvidia.com>
2013-12-04 17:33:58 +0800

    CHROMIUM: thermal: tegra: specify the cdev to cdev-floor
    Specify the cooling device to cdev-floor, we will add cdev-cap
    in the next patches.
    
    BUG=None
    TEST=Verified. Build and boot to console.
    
    Change-Id: I514ef16f6647a3bf6a1a3b152bcd9e2d320b30f0
    Signed-off-by: Wei Ni <wni@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/178920
    Tested-by: Wei Ni <wni.nvidia@gmail.com>
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Commit-Queue: Andrew Bresticker <abrestic@chromium.org>

--- drivers/thermal/tegra124_dfll_action.c
+++ drivers/thermal/tegra124_dfll_action.c
@@ -34,11 +34,11 @@
 
 /**
  * struct tegra124_dfll_cdev_data - DFLL cooling device info
- * @cdev: pointer to this cooling device, after thermal registration
+ * @cdev_floor: pointer to floor cooling device, after thermal registration
  * @dfll_pdev: pointer to the DFLL instance associated with this @cdev
  */
 struct tegra124_dfll_cdev_data {
-	struct thermal_cooling_device *cdev;
+	struct thermal_cooling_device *cdev_floor;
 	struct platform_device *dfll_pdev;
 };
 
@@ -48,45 +48,88 @@ static struct tegra124_dfll_cdev_data tegra124_dfll_cdev_data;
  * Thermal cooling_device interface
  */
 
-static int tegra124_dfll_cdev_get_max_state(struct thermal_cooling_device *cdev,
-					    unsigned long *max_state)
+static int
+tegra124_dfll_cdev_floor_get_max_state(struct thermal_cooling_device *cdev,
+				       unsigned long *max_state)
 {
 	struct platform_device *pdev = (struct platform_device *)cdev->devdata;
 	*max_state = tegra124_dfll_count_therm_floors(pdev);
 	return 0;
 }
 
-static int tegra124_dfll_cdev_get_cur_state(struct thermal_cooling_device *cdev,
-					    unsigned long *cur_state)
+static int
+tegra124_dfll_cdev_floor_get_cur_state(struct thermal_cooling_device *cdev,
+				       unsigned long *cur_state)
 {
 	struct platform_device *pdev = (struct platform_device *)cdev->devdata;
 	*cur_state = tegra124_dfll_get_thermal_index(pdev);
 	return 0;
 }
 
-static int tegra124_dfll_cdev_set_state(struct thermal_cooling_device *cdev,
-					unsigned long cur_state)
+static int
+tegra124_dfll_cdev_floor_set_state(struct thermal_cooling_device *cdev,
+				   unsigned long cur_state)
 {
 	struct platform_device *pdev = (struct platform_device *)cdev->devdata;
 
 	return tegra124_dfll_update_thermal_index(pdev, cur_state);
 }
 
-static struct thermal_cooling_device_ops tegra124_dfll_cooling_ops = {
-	.get_max_state = tegra124_dfll_cdev_get_max_state,
-	.get_cur_state = tegra124_dfll_cdev_get_cur_state,
-	.set_cur_state = tegra124_dfll_cdev_set_state,
+static struct thermal_cooling_device_ops tegra124_dfll_floor_cooling_ops = {
+	.get_max_state = tegra124_dfll_cdev_floor_get_max_state,
+	.get_cur_state = tegra124_dfll_cdev_floor_get_cur_state,
+	.set_cur_state = tegra124_dfll_cdev_floor_set_state,
 };
+
+static int tegra124_dfll_register_therm_floor(struct platform_device *pdev,
+					      struct platform_device *dfll_pdev)
+{
+	struct thermal_cooling_device *tcd;
+	struct device *dev = &pdev->dev;
+	const char *cdev_type;
+	int ret;
+
+	/* just report error - initialized at WC temperature, anyway */
+	cdev_type = of_get_property(dev->of_node, "cdev-floor-type", NULL);
+	if (IS_ERR_OR_NULL(cdev_type)) {
+		dev_err(dev,
+			"Tegra124 DFLL thermal reaction: missing floor type\n");
+		ret = -EINVAL;
+		goto error;
+	}
+	tcd = thermal_cooling_device_register((char *)cdev_type,
+					      (void *)dfll_pdev,
+					      &tegra124_dfll_floor_cooling_ops);
+	if (IS_ERR(tcd)) {
+		dev_err(dev,
+			"Tegra124 DFLL thermal reaction: failed to register\n");
+		ret = PTR_ERR(tcd);
+		goto error;
+	}
+
+	tegra124_dfll_cdev_data.cdev_floor = tcd;
+	tegra124_dfll_cdev_data.dfll_pdev = dfll_pdev;
+	ret = tegra124_dfll_attach_thermal(dfll_pdev, tcd);
+	if (ret) {
+		dev_err(dev,
+			"Tegra124 DFLL thermal reaction: failed to attach\n");
+		thermal_cooling_device_unregister(tcd);
+		goto error;
+	}
+
+	dev_info(dev, "Tegra124 DFLL 'floor cooling device' registered\n");
+
+error:
+	return ret;
+}
 
 static int tegra124_dfll_cdev_probe(struct platform_device *pdev)
 {
-	struct thermal_cooling_device *tcd;
 	struct device_node *dn;
 	const __be32 *prop;
 	struct platform_device *dfll_pdev;
 	struct device *dev = &pdev->dev;
 	struct clk *cpu_dfll_clk;
-	const char *cdev_type;
 	int ret;
 
 	prop = of_get_property(dev->of_node, "act-dev", NULL);
@@ -118,35 +161,7 @@ static int tegra124_dfll_cdev_probe(struct platform_device *pdev)
 		goto error;
 	}
 
-	/* just report error - initialized at WC temperature, anyway */
-	cdev_type = of_get_property(dev->of_node, "cdev-type", NULL);
-	if (IS_ERR_OR_NULL(cdev_type)) {
-		dev_err(dev,
-			"Tegra124 DFLL thermal reaction: missing cdev type\n");
-		ret = -EINVAL;
-		goto error;
-	}
-	tcd = thermal_cooling_device_register((char *)cdev_type,
-					      (void *)dfll_pdev,
-					      &tegra124_dfll_cooling_ops);
-	if (IS_ERR(tcd)) {
-		dev_err(dev,
-			"Tegra124 DFLL thermal reaction: failed to register\n");
-		ret = PTR_ERR(tcd);
-		goto error;
-	}
-
-	tegra124_dfll_cdev_data.cdev = tcd;
-	tegra124_dfll_cdev_data.dfll_pdev = dfll_pdev;
-	ret = tegra124_dfll_attach_thermal(dfll_pdev, tcd);
-	if (ret) {
-		dev_err(dev,
-			"Tegra124 DFLL thermal reaction: failed to attach\n");
-		thermal_cooling_device_unregister(tcd);
-		goto error;
-	}
-
-	dev_info(dev, "Tegra124 DFLL 'cooling device' registered\n");
+	ret = tegra124_dfll_register_therm_floor(pdev, dfll_pdev);
 
 error:
 	of_node_put(dn);
@@ -156,8 +171,8 @@ error:
 static int tegra124_dfll_cdev_remove(struct platform_device *pdev)
 {
 	tegra124_dfll_detach_thermal(tegra124_dfll_cdev_data.dfll_pdev,
-				     tegra124_dfll_cdev_data.cdev);
-	thermal_cooling_device_unregister(tegra124_dfll_cdev_data.cdev);
+				     tegra124_dfll_cdev_data.cdev_floor);
+	thermal_cooling_device_unregister(tegra124_dfll_cdev_data.cdev_floor);
 
 	return 0;
 }
