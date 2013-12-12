abb1e10a8593666ae3896dae8639f3933ff24e2e
Joseph Lo <josephl@nvidia.com>
2013-12-12 17:41:15 +0800

    CHROMIUM: ARM: tegra114: cpuidle: add cluster power off support
    Add one more CPU idle state called cluster power off. This could happen
    when only one CPU core (CPU0) online and go into this state. The power
    (CRAIL) of CPU cluster (include C0NC) or C1NC (The L2$, SCU, vGIC and
    debug) can be shut off depend on go into this state with G or LP cluster.
    
    BUG=none
    TEST=keep system idle in G and LP cluster
         # check the idle state by sysfs
         $> cat /sys/devices/system/cpu/cpu0/cpuidle/state2/usage
    
    Change-Id: I6d121a3aecd8925868b7238c0fed3b1cb5e16981
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/180003
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Commit-Queue: Andrew Bresticker <abrestic@chromium.org>

--- arch/arm/mach-tegra/cpuidle-tegra114.c
+++ arch/arm/mach-tegra/cpuidle-tegra114.c
@@ -19,6 +19,8 @@
 #include <linux/cpuidle.h>
 #include <linux/cpu_pm.h>
 #include <linux/clockchips.h>
+#include <linux/clk/tegra.h>
+#include <linux/tegra-soc.h>
 
 #include <asm/cpuidle.h>
 #include <asm/suspend.h>
@@ -28,24 +30,39 @@
 #include "sleep.h"
 
 #ifdef CONFIG_PM_SLEEP
-#define TEGRA114_MAX_STATES 2
+#define TEGRA114_MAX_STATES 3
 #else
 #define TEGRA114_MAX_STATES 1
 #endif
 
 #ifdef CONFIG_PM_SLEEP
+static void tegra114_cpu_cluster_power_down(void)
+{
+	if (num_online_cpus() > 1 || !tegra_cpu_rail_off_ready()) {
+		cpu_suspend(0, tegra30_sleep_cpu_secondary_finish);
+		return;
+	}
+
+	tegra_idle_lp2_last();
+}
+
 static int tegra114_idle_power_down(struct cpuidle_device *dev,
 				    struct cpuidle_driver *drv,
 				    int index)
 {
+	bool last_cpu;
+
 	local_fiq_disable();
 
-	tegra_set_cpu_in_lp2();
+	last_cpu = tegra_set_cpu_in_lp2();
 	cpu_pm_enter();
 
 	clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_ENTER, &dev->cpu);
 
-	cpu_suspend(0, tegra30_sleep_cpu_secondary_finish);
+	if (index == 2 && last_cpu)
+		tegra114_cpu_cluster_power_down();
+	else
+		cpu_suspend(0, tegra30_sleep_cpu_secondary_finish);
 
 	clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_EXIT, &dev->cpu);
 
@@ -73,6 +90,15 @@ static struct cpuidle_driver tegra_idle_driver = {
 			.flags			= CPUIDLE_FLAG_TIME_VALID,
 			.name			= "powered-down",
 			.desc			= "CPU power gated",
+		},
+		[2] = {
+			.enter			= tegra114_idle_power_down,
+			.exit_latency		= 500,
+			.target_residency	= 10000,
+			.power_usage		= 0,
+			.flags			= CPUIDLE_FLAG_TIME_VALID,
+			.name			= "CPU cluster off",
+			.desc			= "CPU cluster power gated",
 		},
 #endif
 	},
