ceb0a68ab073604b2a4a3a1d357a94155aa2727e
Andrew Bresticker <abrestic@chromium.org>
2013-12-18 22:25:25 -0800

    CHROMIUM: clk: tegra: enable cclk_g at boot
    cclk_g should always be prepared and enabled when running on cluster 0,
    so initialize it to that state.  This also prevents Tegra DVFS from
    undervolting the CPU because of DVFS being started with cclk_g not
    being prepared or enabled.
    
    BUG=chrome-os-partner:24487
    TEST=Nyan no longer hangs at DVFS init when booting with coreboot.
    
    Change-Id: Icab01efe136b8c2971ebcef60bfad2266844a26f
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/180863
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- drivers/clk/tegra/clk-tegra124.c
+++ drivers/clk/tegra/clk-tegra124.c
@@ -1843,6 +1843,7 @@ static struct tegra_clk_init_table init_table[] __initdata = {
 	{TEGRA124_CLK_ISP, TEGRA124_CLK_PLL_C4, 0, 0},
 	{TEGRA124_CLK_VIC03, TEGRA124_CLK_PLL_P, 0, 0},
 	{TEGRA124_CLK_EMC, TEGRA124_CLK_CLK_MAX, 0, 1},
+	{TEGRA124_CLK_CCLK_G, TEGRA124_CLK_CLK_MAX, 0, 1},
 	/* This MUST be the last entry. */
 	{TEGRA124_CLK_CLK_MAX, TEGRA124_CLK_CLK_MAX, 0, 0},
 };
