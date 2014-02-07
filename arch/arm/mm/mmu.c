a2611cf9011ec0739265b3f5ca1cf9aff9b650af
Kees Cook <keescook@chromium.org>
2014-02-06 16:06:45 -0800

    CHROMIUM: ARM: mm: split kernel text from init
    Prepare to split the page table mappings for the kernel and init memory.
    
    BUG=chromium:341583
    TEST=nyan build/boot
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/185500
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    
    Change-Id: Ieaa6f1a21e90497f9b4cbfa597f592240102d3ad
    (cherry picked from commit e421a6b3c952625d9c098e8076a626811b842c3b)
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/188443
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -1311,8 +1311,6 @@ static void __init kmap_init(void)
 static void __init map_lowmem(void)
 {
 	struct memblock_region *reg;
-	unsigned long kernel_x_start = round_down(__pa(_stext), SECTION_SIZE);
-	unsigned long kernel_x_end = round_up(__pa(__init_end), SECTION_SIZE);
 
 	/* Map all the lowmem memory banks. */
 	for_each_memblock(memory, reg) {
@@ -1325,40 +1323,44 @@ static void __init map_lowmem(void)
 		if (start >= end)
 			break;
 
-		if (end < kernel_x_start || start >= kernel_x_end) {
-			map.pfn = __phys_to_pfn(start);
-			map.virtual = __phys_to_virt(start);
-			map.length = end - start;
-			map.type = MT_MEMORY_RWX;
+		map.pfn = __phys_to_pfn(start);
+		map.virtual = __phys_to_virt(start);
+		map.length = end - start;
 
-			create_mapping(&map);
-		} else {
-			/* This better cover the entire kernel */
-			if (start < kernel_x_start) {
-				map.pfn = __phys_to_pfn(start);
-				map.virtual = __phys_to_virt(start);
-				map.length = kernel_x_start - start;
-				map.type = MT_MEMORY_RW;
+		if (map.virtual <= (unsigned long)_stext &&
+		    ((unsigned long)_end < (map.virtual + map.length))) {
+			struct map_desc kernel;
 
+			/* Kernel init. Set RW when freeing initmem. */
+			kernel.pfn = __phys_to_pfn(__pa(__init_begin));
+			kernel.virtual = (unsigned long)__init_begin;
+			kernel.length = _sdata - __init_begin;
+			kernel.type = MT_MEMORY_RWX;
+			create_mapping(&kernel);
+
+			/* Kernel text. Set RX when freeing initmem. */
+			kernel.pfn = __phys_to_pfn(__pa(_stext));
+			kernel.virtual = (unsigned long)_stext;
+			kernel.length = __init_begin - _stext;
+			kernel.type = MT_MEMORY_RWX;
+			create_mapping(&kernel);
+
+			/* Before the kernel, set RX when freeing initmem. */
+			if (map.virtual < (unsigned long)_stext) {
+				map.length = (unsigned long)_stext -
+					     map.virtual;
+				map.type = MT_MEMORY_RWX;
 				create_mapping(&map);
 			}
 
-			map.pfn = __phys_to_pfn(kernel_x_start);
-			map.virtual = __phys_to_virt(kernel_x_start);
-			map.length = kernel_x_end - kernel_x_start;
-			map.type = MT_MEMORY_RWX;
-
-			create_mapping(&map);
-
-			if (kernel_x_end < end) {
-				map.pfn = __phys_to_pfn(kernel_x_end);
-				map.virtual = __phys_to_virt(kernel_x_end);
-				map.length = end - kernel_x_end;
-				map.type = MT_MEMORY_RW;
-
-				create_mapping(&map);
-			}
+			/* Anything after the kernel. */
+			map.pfn = __phys_to_pfn(__pa(_sdata));
+			map.virtual = (unsigned long)_sdata;
+			map.length = end - __pa(_sdata);
 		}
+
+		map.type = MT_MEMORY_RW;
+		create_mapping(&map);
 	}
 }
 
