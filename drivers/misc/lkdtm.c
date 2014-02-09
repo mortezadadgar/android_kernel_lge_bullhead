2a096ff900682c273620a587162c0bacbb79bd1e
Kees Cook <keescook@chromium.org>
2014-02-08 16:37:13 -0800

    Revert "CHROMIUM: add "WARNING" trigger to lkdtm"
    This reverts commit 93d415ad4715a71eec783cb119fae59ca057372c.
    
    BUG=chromium:342511
    TEST=link build/boot passes when this whole series is applied
    
    Change-Id: Ia530c3a6d40f9c2846202350cb50e0a6e38d2386
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/185992
    Reviewed-by: Sameer Nanda <snanda@chromium.org>

--- drivers/misc/lkdtm.c
+++ drivers/misc/lkdtm.c
@@ -68,7 +68,6 @@ enum ctype {
 	CT_NONE,
 	CT_PANIC,
 	CT_BUG,
-	CT_WARNING,
 	CT_EXCEPTION,
 	CT_LOOP,
 	CT_OVERFLOW,
@@ -96,7 +95,6 @@ static char* cp_name[] = {
 static char* cp_type[] = {
 	"PANIC",
 	"BUG",
-	"WARNING",
 	"EXCEPTION",
 	"LOOP",
 	"OVERFLOW",
@@ -285,9 +283,6 @@ static void lkdtm_do_action(enum ctype which)
 		break;
 	case CT_BUG:
 		BUG();
-		break;
-	case CT_WARNING:
-		WARN_ON(1);
 		break;
 	case CT_EXCEPTION:
 		*((int *) 0) = 0;
