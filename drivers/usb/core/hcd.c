bed8b8f58e21ef9cc33392c9a1dbffcf71e65842
Derek Basehore <dbasehore@chromium.org>
2014-04-25 12:09:44 -0700

    usb: change how dark resume works
    This changes how dark resume works for usb devices since dark resume for usb
    devices was broken on rambi based devices. This moves the exit for resume to
    later in the usb resume process. This allows a bit to be cleared for the hcd
    interface.
    
    BUG=chrome-os-partner:28077
    TEST=On rambi run the following
    	echo enabled > /sys/class/rtc/rtc0/device/power/dark_resume_source
    	echo enabled > /sys/bus/usb/devices/usb1/power/dark_resume_active
    	echo enabled > /sys/bus/usb/devices/usb2/power/dark_resume_active
    	echo +10 > /sys/class/rtc/rtc0/wakealarm ; echo mem > /sys/power/state
    	<let the device wake up>
    	echo mem > /sys/power/state
    	<make sure the system suspends properly>
    
    Change-Id: I700c6b442a65b0f60a79f67d3d878df8fea4f947
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/197055
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- drivers/usb/core/hcd.c
+++ drivers/usb/core/hcd.c
@@ -40,6 +40,7 @@
 #include <linux/platform_device.h>
 #include <linux/workqueue.h>
 #include <linux/pm_runtime.h>
+#include <linux/pm_dark_resume.h>
 
 #include <linux/usb.h>
 #include <linux/usb/hcd.h>
@@ -2082,6 +2083,10 @@ int hcd_bus_resume(struct usb_device *rhdev, pm_message_t msg)
 		return -ENOENT;
 	if (HCD_RH_RUNNING(hcd))
 		return 0;
+	if (dev_dark_resume_active(&rhdev->dev)) {
+		clear_bit(HCD_FLAG_WAKEUP_PENDING, &hcd->flags);
+		return 0;
+	}
 
 	hcd->state = HC_STATE_RESUMING;
 	status = hcd->driver->bus_resume(hcd);
