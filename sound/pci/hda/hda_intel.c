deb9c3ffb282732ae007f7c7ff16e843a03bc497
Takashi Iwai <tiwai@suse.de>
2014-07-15 15:19:43 +0200

    BACKPORT: ALSA: hda - Fix broken PM due to incomplete i915 initialization
    When the initialization of Intel HDMI controller fails due to missing
    i915 kernel symbols (e.g. HD-audio is built in while i915 is module),
    the driver discontinues the probe.  However, since the probe was done
    asynchronously, the driver object still remains, thus the relevant PM
    ops are still called at suspend/resume. This results in the bad access
    to the incomplete audio card object, eventually leads to Oops or stall
    at PM.
    
    This patch adds the missing checks of chip->init_failed flag at each
    PM callback in order to fix the problem above.
    
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=79561
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 4da63c6fc426023d1a20e45508c47d7d68c6a53d)
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>
    
    Conflicts:
    	sound/pci/hda/hda_intel.c
    
    The conflict is due to missing chip->disabled checks in runtime
    suspend/resume that were added in the context of upstream commit
    246efa4a07...the scope of commit 246efa4a07 is too large to consider
    backporting at this time.
    
    BUG=chrome-os-partner:32694
    TEST=No audio regressions over HDMI or builtin speakers with and
         without AC power and suspend/resume.
    
    Change-Id: Ib76fb819e36fc256fa120d1108111284abd0ae88
    Reviewed-on: https://chromium-review.googlesource.com/232820
    Tested-by: Ullysses A Eoff <ullysses.a.eoff@intel.com>
    Reviewed-by: Benson Leung <bleung@chromium.org>
    Commit-Queue: Ullysses A Eoff <ullysses.a.eoff@intel.com>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -580,7 +580,7 @@ static int azx_suspend(struct device *dev)
 	struct azx *chip = card->private_data;
 	struct azx_pcm *p;
 
-	if (chip->disabled)
+	if (chip->disabled || chip->init_failed)
 		return 0;
 
 	snd_power_change_state(card, SNDRV_CTL_POWER_D3hot);
@@ -611,7 +611,7 @@ static int azx_resume(struct device *dev)
 	struct snd_card *card = dev_get_drvdata(dev);
 	struct azx *chip = card->private_data;
 
-	if (chip->disabled)
+	if (chip->disabled || chip->init_failed)
 		return 0;
 
 	if (chip->driver_caps & AZX_DCAPS_I915_POWERWELL)
@@ -646,6 +646,9 @@ static int azx_runtime_suspend(struct device *dev)
 	struct snd_card *card = dev_get_drvdata(dev);
 	struct azx *chip = card->private_data;
 
+	if (chip->disabled || chip->init_failed)
+		return 0;
+
 	/* enable controller wake up event */
 	azx_writew(chip, WAKEEN, azx_readw(chip, WAKEEN) |
 		  STATESTS_INT_MASK);
@@ -665,6 +668,9 @@ static int azx_runtime_resume(struct device *dev)
 	struct hda_bus *bus;
 	struct hda_codec *codec;
 	int status;
+
+	if (chip->disabled || chip->init_failed)
+		return 0;
 
 	if (chip->driver_caps & AZX_DCAPS_I915_POWERWELL)
 		hda_display_power(true);
@@ -694,6 +700,9 @@ static int azx_runtime_idle(struct device *dev)
 {
 	struct snd_card *card = dev_get_drvdata(dev);
 	struct azx *chip = card->private_data;
+
+	if (chip->disabled || chip->init_failed)
+		return 0;
 
 	if (!power_save_controller ||
 	    !(chip->driver_caps & AZX_DCAPS_PM_RUNTIME))
