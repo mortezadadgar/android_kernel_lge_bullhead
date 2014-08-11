425dcf684ad01f7f9c8d2e49f485d7ceb995d363
Will Deacon <will.deacon@arm.com>
2014-08-11 14:23:37 +0100

    arm64: compat: wire up memfd_create and getrandom syscalls for aarch32
    arch/arm/ just grew support for the new memfd_create and getrandom
    syscalls, so add them to our compat layer too.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -792,4 +792,5 @@ __SYSCALL(__NR_renameat2, sys_ni_syscall)
 __SYSCALL(__NR_seccomp, sys_seccomp)
 #define __NR_getrandom 384
 __SYSCALL(__NR_getrandom, sys_getrandom)
-
+#define __NR_memfd_create 385
+__SYSCALL(__NR_memfd_create, sys_memfd_create)
