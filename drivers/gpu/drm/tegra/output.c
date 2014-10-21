29ee7135667600c8bc58032a2d38c69710aa37f4
Thierry Reding <treding@nvidia.com>
2014-10-21 14:00:09 +0200

    UPSTREAM: drm/tegra: Enable the hotplug interrupt only when necessary
    The hotplug handling needs access to the DRM device, which only appears
    at ->init() time. Disable interrupts up until that time. Similarly, when
    an output is removed, disable the hotplug interrupt again because the
    DRM device (and with it the hotplug infrastructure) is going away.
    
    Also make sure to only access the DRM device if it's available. Given
    the above change for the hotplug interrupt this should really never
    happen, but the extra check doesn't hurt either.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    (cherry picked from commit 8fc8f7da9719c2d28fb32cdd74af9b6cd9bac20a)
    Signed-off-by: Vince Hsu <vinceh@nvidia.com>
    
    BUG=chrome-os-partner:55924
    TEST=No more intermittent kernel panic
    
    Change-Id: Ifb68c538f3c9979792b4bd66d1a9646beca7eb58
    Reviewed-on: https://chromium-review.googlesource.com/366161
    Commit-Ready: Vince Hsu <vince.h@nvidia.com>
    Tested-by: Vince Hsu <vince.h@nvidia.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/tegra/output.c
+++ drivers/gpu/drm/tegra/output.c
@@ -231,7 +231,8 @@ static irqreturn_t hpd_irq(int irq, void *data)
 {
 	struct tegra_output *output = data;
 
-	drm_helper_hpd_irq_event(output->connector.dev);
+	if (output->connector.dev)
+		drm_helper_hpd_irq_event(output->connector.dev);
 
 	return IRQ_HANDLED;
 }
@@ -303,6 +304,13 @@ int tegra_output_probe(struct tegra_output *output)
 		}
 
 		output->connector.polled = DRM_CONNECTOR_POLL_HPD;
+
+		/*
+		 * Disable the interrupt until the connector has been
+		 * initialized to avoid a race in the hotplug interrupt
+		 * handler.
+		 */
+		disable_irq(output->hpd_irq);
 	}
 
 	return 0;
@@ -378,10 +386,24 @@ int tegra_output_init(struct drm_device *drm, struct tegra_output *output)
 		break;
 	}
 
+	/*
+	 * The connector is now registered and ready to receive hotplug events
+	 * so the hotplug interrupt can be enabled.
+	 */
+	if (gpio_is_valid(output->hpd_gpio))
+		enable_irq(output->hpd_irq);
+
 	return 0;
 }
 
 int tegra_output_exit(struct tegra_output *output)
 {
+	/*
+	 * The connector is going away, so the interrupt must be disabled to
+	 * prevent the hotplug interrupt handler from potentially crashing.
+	 */
+	if (gpio_is_valid(output->hpd_gpio))
+		disable_irq(output->hpd_irq);
+
 	return 0;
 }
