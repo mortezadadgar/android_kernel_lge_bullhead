0ab77d375b41e3c569fe92f2fb801abb8cae2e50
David Ung <davidu@nvidia.com>
2014-12-03 13:08:43 -0800

    UPSTREAM: video: fbdev: Add additional vesa modes
    Add high resolution modes to vesa_modes struct.
    
    Signed-off-by: David Ung <davidu@nvidia.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    (cherry picked from commit 37715f556a0776356300391f8ac41ace91bea447)
    
    BUG=chrome-os-partner:29514
    TEST=None
    
    Change-Id: I38d2b0af7327cad5425808f895b3dd1cb3637850
    Signed-off-by: David Ung <davidu@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/241355
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>
    Trybot-Ready: Daniel Kurtz <djkurtz@chromium.org>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -726,7 +726,7 @@ extern int fb_videomode_from_videomode(const struct videomode *vm,
 				       struct fb_videomode *fbmode);
 
 /* drivers/video/modedb.c */
-#define VESA_MODEDB_SIZE 34
+#define VESA_MODEDB_SIZE 43
 #define CEA_MODEDB_SIZE 65
 #define HDMI_EXT_MODEDB_SIZE 5
 extern void fb_var_to_videomode(struct fb_videomode *mode,
