11d9990e24c354ad20e005edec89cf4e8a001e17
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2014-12-08 20:26:55 -0800

    rcu: Handle gpnum/completed wrap while dyntick idle
    Subtle race conditions can result if a CPU stays in dyntick-idle mode
    long enough for the ->gpnum and ->completed fields to wrap.  For
    example, consider the following sequence of events:
    
    o	CPU 1 encounters a quiescent state while waiting for grace period
    	5 to complete, but then enters dyntick-idle mode.
    
    o	While CPU 1 is in dyntick-idle mode, the grace-period counters
    	wrap around so that the grace period number is now 4.
    
    o	Just as CPU 1 exits dyntick-idle mode, grace period 4 completes
    	and grace period 5 begins.
    
    o	The quiescent state that CPU 1 passed through during the old
    	grace period 5 looks like it applies to the new grace period
    	5.  Therefore, the new grace period 5 completes without CPU 1
    	having passed through a quiescent state.
    
    This could clearly be a fatal surprise to any long-running RCU read-side
    critical section that happened to be running on CPU 1 at the time.  At one
    time, this was not a problem, given that it takes significant time for
    the grace-period counters to overflow even on 32-bit systems.  However,
    with the advent of NO_HZ_FULL and SMP embedded systems, arbitrarily long
    idle periods are now becoming quite feasible.  It is therefore time to
    close this race.
    
    This commit therefore avoids this race condition by having the
    quiescent-state forcing code detect when a CPU is falling too far
    behind, and setting a new rcu_data field ->gpwrap when this happens.
    Whenever this new ->gpwrap field is set, the CPU's ->gpnum and ->completed
    fields are known to be untrustworthy, and can be ignored, along with
    any associated quiescent states.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcu/tree_plugin.h
+++ kernel/rcu/tree_plugin.h
@@ -1612,7 +1612,8 @@ static bool rcu_try_advance_all_cbs(void)
 		 * completed since we last checked and there are
 		 * callbacks not yet ready to invoke.
 		 */
-		if (rdp->completed != rnp->completed &&
+		if ((rdp->completed != rnp->completed ||
+		     unlikely(ACCESS_ONCE(rdp->gpwrap))) &&
 		    rdp->nxttail[RCU_DONE_TAIL] != rdp->nxttail[RCU_NEXT_TAIL])
 			note_gp_changes(rsp, rdp);
 
