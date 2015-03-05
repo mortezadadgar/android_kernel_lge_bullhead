e872be4ad1448b0b6cecb2abfb13f108b2aa04fb
Sivan Reinstein <sivanr@codeaurora.org>
2015-03-05 14:15:27 +0200

    net: adding API to get the current NAPI context
    Adding a new API - get_current_napi_context(), which returns the
    current napi_struct on the current running CPU.
    This change allows network drivers to receive the current
    running napi_struct, to use when calling APIs that require it.
    
    Adding a new field to struct softnet_data: current_napi.
    The field current_napi will hold the current running napi_sturct.
    
    Adding a call to napi_gro_flush() from process_backlog() when there
    are no more packets to process, since process_backlog() doesn't
    call napi_complete() API.
    
    CRs-fixed: 784626
    Change-Id: I05bde8bb00c4ec6440fc1db29e741746b759d1b7
    Signed-off-by: Sivan Reinstein <sivanr@codeaurora.org>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4081,6 +4081,7 @@ static int process_backlog(struct napi_struct *napi, int quota)
 {
 	int work = 0;
 	struct softnet_data *sd = container_of(napi, struct softnet_data, backlog);
+	static int state_changed;
 
 	/* Check if we have pending ipi, its better to send them now,
 	 * not waiting net_rx_action() end.
@@ -4103,7 +4104,14 @@ static int process_backlog(struct napi_struct *napi, int quota)
 			local_irq_disable();
 			input_queue_head_incr(sd);
 			if (++work >= quota) {
+				if (state_changed) {
+					local_irq_enable();
+					napi_gro_flush(napi, false);
+					local_irq_disable();
+					state_changed = 0;
+				}
 				local_irq_enable();
+				sd->current_napi = NULL;
 				return work;
 			}
 		}
@@ -4119,6 +4127,7 @@ static int process_backlog(struct napi_struct *napi, int quota)
 			 * and we dont need an smp_mb() memory barrier.
 			 */
 			napi->state = 0;
+			state_changed = 1;
 			rps_unlock(sd);
 
 			break;
@@ -4129,6 +4138,7 @@ static int process_backlog(struct napi_struct *napi, int quota)
 		rps_unlock(sd);
 	}
 	local_irq_enable();
+	sd->current_napi = NULL;
 
 	return work;
 }
@@ -4151,11 +4161,14 @@ EXPORT_SYMBOL(__napi_schedule);
 
 void __napi_complete(struct napi_struct *n)
 {
+	struct softnet_data *sd = &__get_cpu_var(softnet_data);
+
 	BUG_ON(!test_bit(NAPI_STATE_SCHED, &n->state));
 	BUG_ON(n->gro_list);
 
 	list_del_init(&n->poll_list);
 	smp_mb__before_atomic();
+	sd->current_napi = NULL;
 	clear_bit(NAPI_STATE_SCHED, &n->state);
 }
 EXPORT_SYMBOL(__napi_complete);
@@ -4224,6 +4237,13 @@ void netif_napi_del(struct napi_struct *napi)
 }
 EXPORT_SYMBOL(netif_napi_del);
 
+struct napi_struct *get_current_napi_context(void)
+{
+	struct softnet_data *sd = &__get_cpu_var(softnet_data);
+	return sd->current_napi;
+}
+EXPORT_SYMBOL(get_current_napi_context);
+
 static void net_rx_action(struct softirq_action *h)
 {
 	struct softnet_data *sd = &__get_cpu_var(softnet_data);
@@ -4264,6 +4284,7 @@ static void net_rx_action(struct softirq_action *h)
 		 */
 		work = 0;
 		if (test_bit(NAPI_STATE_SCHED, &n->state)) {
+			sd->current_napi = n;
 			work = n->poll(n, weight);
 			trace_napi_poll(n);
 		}
