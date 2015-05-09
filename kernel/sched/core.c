692e0d2409c9120687ef225f30c12f1753e40ab5
Morten Rasmussen <morten.rasmussen@arm.com>
2015-05-09 19:53:49 +0100

    FROMLIST: sched: Add cpu capacity awareness to wakeup balancing
    Wakeup balancing is completely unaware of cpu capacity, cpu usage and
    task utilization. The task is preferably placed on a cpu which is idle
    in the instant the wakeup happens. New tasks (SD_BALANCE_{FORK,EXEC} are
    placed on an idle cpu in the idlest group if such can be found, otherwise
    it goes on the least loaded one. Existing tasks (SD_BALANCE_WAKE) are
    placed on the previous cpu or an idle cpu sharing the same last level
    cache. Hence existing tasks don't get a chance to migrate to a different
    group at wakeup in case the current one has reduced cpu capacity (due
    RT/IRQ pressure or different uarch e.g. ARM big.LITTLE). They may
    eventually get pulled by other cpus doing periodic/idle/nohz_idle
    balance, but it may take quite a while before it happens.
    
    This patch adds capacity awareness to find_idlest_{group,queue} (used by
    SD_BALANCE_{FORK,EXEC}) such that groups/cpus that can accommodate the
    waking task based on task utilization are preferred. In addition, wakeup
    of existing tasks (SD_BALANCE_WAKE) is sent through
    find_idlest_{group,queue} if the task doesn't fit the capacity of the
    previous cpu to allow it to escape (override wake_affine) when
    necessary instead of relying on periodic/idle/nohz_idle balance to
    eventually sort it out.
    
    The patch doesn't depend on any energy model infrastructure, but it is
    kept behind the energy_aware() static key despite being primarily a
    performance optimization as it may increase scheduler overhead slightly.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    
    (am from https://patchwork.kernel.org/patch/6738361)
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    
    Change-Id: I3d4a4826a3bb4157c561de1d51c2d4a6f3df128f

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6247,7 +6247,7 @@ sd_init(struct sched_domain_topology_level *tl, int cpu)
 					| 1*SD_BALANCE_NEWIDLE
 					| 1*SD_BALANCE_EXEC
 					| 1*SD_BALANCE_FORK
-					| 0*SD_BALANCE_WAKE
+					| 1*SD_BALANCE_WAKE
 					| 1*SD_WAKE_AFFINE
 					| 0*SD_SHARE_CPUCAPACITY
 					| 0*SD_SHARE_PKG_RESOURCES
