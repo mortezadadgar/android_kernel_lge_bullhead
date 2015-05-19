32f98a83f0a398395b888f528a24d7e984e195e1
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2015-05-19 14:18:45 +0300

    CHROMIUM: iwl7000: Merge remote-tracking branch 'auto/master' into MERGE
    iwl7000-tree: 89befa82ae79ee497c226977aaf49238d6343935

--- drivers/net/wireless-3.8/iwl7000/mac80211/ieee80211_i.h
+++ drivers/net/wireless-3.8/iwl7000/mac80211/ieee80211_i.h
@@ -1040,7 +1040,6 @@ enum queue_stop_reason {
 
 #ifdef CPTCFG_MAC80211_LEDS
 struct tpt_led_trigger {
-	struct led_trigger trig;
 	char name[32];
 	const struct ieee80211_tpt_blink *blink_table;
 	unsigned int blink_table_len;
