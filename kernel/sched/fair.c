e25c2426551ca7c86fbba11784593a26a39fc653
Juri Lelli <juri.lelli@arm.com>
2015-06-25 14:12:33 +0100

    FROMLIST: sched/fair: jump to max OPP when crossing UP threshold
    Since the true utilization of a long running task is not detectable while
    it is running and might be bigger than the current cpu capacity, create the
    maximum cpu capacity head room by requesting the maximum cpu capacity once
    the cpu usage plus the capacity margin exceeds the current capacity. This
    is also done to try to harm the performance of a task the least.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    
    (am from https://patchwork.kernel.org/patch/6738071)
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    
    Change-Id: Ic7cded62657d5932adb202a6946d5cccd99ea4bb

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8166,6 +8166,25 @@ static void task_tick_fair(struct rq *rq, struct task_struct *curr, int queued)
 
 	if (!rq->rd->overutilized && cpu_overutilized(task_cpu(curr)))
 		rq->rd->overutilized = true;
+
+	/*
+	 * To make free room for a task that is building up its "real"
+	 * utilization and to harm its performance the least, request a
+	 * jump to max OPP as soon as get_cpu_usage() crosses the UP
+	 * threshold. The UP threshold is built relative to the current
+	 * capacity (OPP), by using same margin used to tell if a cpu
+	 * is overutilized (capacity_margin).
+	 */
+	if (sched_energy_freq()) {
+		int cpu = cpu_of(rq);
+		unsigned long capacity_orig = capacity_orig_of(cpu);
+		unsigned long capacity_curr = capacity_curr_of(cpu);
+
+		if (capacity_curr < capacity_orig &&
+		    (capacity_curr * SCHED_LOAD_SCALE) <
+		    (get_cpu_usage(cpu) * capacity_margin))
+			cpufreq_sched_set_cap(cpu, capacity_orig);
+	}
 }
 
 /*
