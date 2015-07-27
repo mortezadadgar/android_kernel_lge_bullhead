f4ae9211d7554ffaf6f6b924b7e5e6481ff90eab
Oren Givon <oren.givon@intel.com>
2015-07-27 09:25:05 +0300

    CHROMIUM: iwl7000: iwlwiwifi: ftrace: remove empty line from end of file
    There is sometimes a warning when compiling this code because of
    the empty line between the two endif statements.
    After preprocessing, this empty line becomes an empty line at
    the end of the file and may cause warnings at the
    compilation stage.
    
    Signed-off-by: Oren Givon <oren.givon@intel.com>
    iwl7000-tree: a1fec2967b0920c4e59c5c20525d72b2f7136ea0

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-devtrace.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-devtrace.c
@@ -43,5 +43,4 @@ EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_ucode_wrap_event);
 #ifdef CPTCFG_MAC80211_LATENCY_MEASUREMENTS
 EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_tx_latency_thrshld);
 #endif /* CPTCFG_MAC80211_LATENCY_MEASUREMENTS */
-
 #endif /* __CHECKER__ */
