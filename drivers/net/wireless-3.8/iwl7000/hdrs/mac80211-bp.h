e584ba552fcdea15ff49277b384ce796b4f46d54
Johannes Berg <johannes.berg@intel.com>
2015-08-24 09:35:31 +0200

    CHROMIUM: iwl7000: chromeOS: add DECLARE_EWMA
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: f306beb1d494cb9acd8af184be89e24e45b5ef73

--- drivers/net/wireless-3.8/iwl7000/hdrs/mac80211-bp.h
+++ drivers/net/wireless-3.8/iwl7000/hdrs/mac80211-bp.h
@@ -789,3 +789,44 @@ size_t ieee80211_ie_split(const u8 *ies, size_t ielen,
 #define cfg80211_reg_can_beacon_relax(wiphy, chandef, iftype) \
 	cfg80211_reg_can_beacon(wiphy, chandef, iftype)
 #endif
+
+#ifndef DECLARE_EWMA
+#define DECLARE_EWMA(name, _factor, _weight)				\
+	struct ewma_##name {						\
+		unsigned long internal;					\
+	};								\
+	static inline void ewma_##name##_init(struct ewma_##name *e)	\
+	{								\
+		BUILD_BUG_ON(!__builtin_constant_p(_factor));		\
+		BUILD_BUG_ON(!__builtin_constant_p(_weight));		\
+		BUILD_BUG_ON_NOT_POWER_OF_2(_factor);			\
+		BUILD_BUG_ON_NOT_POWER_OF_2(_weight);			\
+		e->internal = 0;					\
+	}								\
+	static inline unsigned long					\
+	ewma_##name##_read(struct ewma_##name *e)			\
+	{								\
+		BUILD_BUG_ON(!__builtin_constant_p(_factor));		\
+		BUILD_BUG_ON(!__builtin_constant_p(_weight));		\
+		BUILD_BUG_ON_NOT_POWER_OF_2(_factor);			\
+		BUILD_BUG_ON_NOT_POWER_OF_2(_weight);			\
+		return e->internal >> ilog2(_factor);			\
+	}								\
+	static inline void ewma_##name##_add(struct ewma_##name *e,	\
+					     unsigned long val)		\
+	{								\
+		unsigned long internal = ACCESS_ONCE(e->internal);	\
+		unsigned long weight = ilog2(_weight);			\
+		unsigned long factor = ilog2(_factor);			\
+									\
+		BUILD_BUG_ON(!__builtin_constant_p(_factor));		\
+		BUILD_BUG_ON(!__builtin_constant_p(_weight));		\
+		BUILD_BUG_ON_NOT_POWER_OF_2(_factor);			\
+		BUILD_BUG_ON_NOT_POWER_OF_2(_weight);			\
+									\
+		ACCESS_ONCE(e->internal) = internal ?			\
+			(((internal << weight) - internal) +		\
+				(val << factor)) >> weight :		\
+			(val << factor);				\
+	}
+#endif /* DECLARE_EWMA */
