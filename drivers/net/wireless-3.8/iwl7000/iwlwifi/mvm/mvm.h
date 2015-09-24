ab8a3858999bf94f92d1f8f17e5692fd94eaab3a
Jonathan De Cesco <jonathan.de.cesco@intel.com>
2015-09-24 16:15:13 +0300

    CHROMIUM: iwl7000: mvm: avoid platform suspend during recovery
    Take a new wakelock before triggering recovery process and release
    upon completion to avoid attempting to suspend during recovery.
    
    Signed-off-by: Jonathan De Cesco <jonathan.de.cesco@intel.com>
    Signed-off-by: Eliad Peller <eliadx.peller@intel.com>
    iwl7000-tree: ceffb22d2440f32e4f20c2f1e4d539949e7a369b

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/mvm/mvm.h
@@ -86,6 +86,9 @@
 #ifdef CPTCFG_IWLMVM_TCM
 #include <linux/average.h>
 #endif
+#ifdef CPTCFG_IWLMVM_WAKELOCK
+#include <linux/wakelock.h>
+#endif
 
 #define IWL_MVM_MAX_ADDRESSES		5
 /* RSSI offset for WkP */
@@ -937,6 +940,9 @@ struct iwl_mvm {
 
 	u32 ciphers[6];
 	struct iwl_mvm_tof_data tof_data;
+#ifdef CPTCFG_IWLMVM_WAKELOCK
+	struct wake_lock recovery_wake_lock;
+#endif
 
 };
 
