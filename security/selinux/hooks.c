7c70ba837879ea6e5968e6797365852f8b8a3a1b
Sangwoo <sangwoo2.park@lge.com>
2015-10-21 17:44:30 -0400

    selinux: Use a kmem_cache for allocation struct file_security_struct
    The size of struct file_security_struct is 16byte at my setup.
    But, the real allocation size for per each file_security_struct
    is 64bytes in my setup that kmalloc min size is 64bytes
    because ARCH_DMA_MINALIGN is 64.
    
    This allocation is called every times at file allocation(alloc_file()).
    So, the total slack memory size(allocated size - request size)
    is increased exponentially.
    
    E.g) Min Kmalloc Size : 64bytes, Unit : bytes
          Allocated Size | Request Size | Slack Size | Allocation Count
        ---------------------------------------------------------------
             770048      |    192512    |   577536   |      12032
    
    At the result, this change reduce memory usage 42bytes per each
    file_security_struct
    
    Signed-off-by: Sangwoo <sangwoo2.park@lge.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    [PM: removed extra subject prefix]
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -133,6 +133,7 @@ int selinux_enabled = 1;
 #endif
 
 static struct kmem_cache *sel_inode_cache;
+static struct kmem_cache *file_security_cache;
 
 /**
  * selinux_secmark_enabled - Check to see if SECMARK is currently enabled
@@ -267,7 +268,7 @@ static int file_alloc_security(struct file *file)
 	struct file_security_struct *fsec;
 	u32 sid = current_sid();
 
-	fsec = kzalloc(sizeof(struct file_security_struct), GFP_KERNEL);
+	fsec = kmem_cache_zalloc(file_security_cache, GFP_KERNEL);
 	if (!fsec)
 		return -ENOMEM;
 
@@ -282,7 +283,7 @@ static void file_free_security(struct file *file)
 {
 	struct file_security_struct *fsec = file->f_security;
 	file->f_security = NULL;
-	kfree(fsec);
+	kmem_cache_free(file_security_cache, fsec);
 }
 
 static int superblock_alloc_security(struct super_block *sb)
@@ -6080,6 +6081,9 @@ static __init int selinux_init(void)
 
 	sel_inode_cache = kmem_cache_create("selinux_inode_security",
 					    sizeof(struct inode_security_struct),
+					    0, SLAB_PANIC, NULL);
+	file_security_cache = kmem_cache_create("selinux_file_security",
+					    sizeof(struct file_security_struct),
 					    0, SLAB_PANIC, NULL);
 	avc_init();
 
