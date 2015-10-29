db0cbe31c3a29ff629ebb3cee8f57f3c7e46bb76
Eliad Peller <eliad@wizery.com>
2015-10-29 11:52:50 +0200

    CHROMIUM: iwl7000: mac80211: don't teardown sdata on sdata stop
    Interfaces are being initialized (setup) on addition,
    and torn down on removal.
    
    However, p2p device is being torn down when stopped,
    resulting in the next p2p start operation being done
    on uninitialized interface.
    
    Solve it by calling ieee80211_teardown_sdata() only
    on interface removal (for the non-netdev case).
    
    Signed-off-by: Eliad Peller <eliadx.peller@intel.com>
    iwl7000-tree: 01c1ee1b8db64fcdea1d9e823ad22041fa32695e

--- drivers/net/wireless-3.8/iwl7000/mac80211/iface.c
+++ drivers/net/wireless-3.8/iwl7000/mac80211/iface.c
@@ -1937,6 +1937,7 @@ void ieee80211_if_remove(struct ieee80211_sub_if_data *sdata)
 	if (sdata->dev) {
 		unregister_netdevice(sdata->dev);
 	} else {
+		ieee80211_teardown_sdata(sdata);
 		cfg80211_unregister_wdev(&sdata->wdev);
 		kfree(sdata);
 	}
@@ -1947,7 +1948,6 @@ void ieee80211_sdata_stop(struct ieee80211_sub_if_data *sdata)
 	if (WARN_ON_ONCE(!test_bit(SDATA_STATE_RUNNING, &sdata->state)))
 		return;
 	ieee80211_do_stop(sdata, true);
-	ieee80211_teardown_sdata(sdata);
 }
 
 void ieee80211_remove_interfaces(struct ieee80211_local *local)
