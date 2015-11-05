c2e5f8ba044dedcf775d54386289e813badad961
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2015-11-05 22:03:21 +0200

    CHROMIUM: iwl7000: chromeOS: backport skb_ensure_writable
    This was implemented in 3.19 is hence not available
    on ChromeOS kernels
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    iwl7000-tree: 24826ec07e966ee9cfda2ed16dbc0493ec2a4346

--- drivers/net/wireless-3.8/iwl7000/hdrs/iwl-chrome.h
+++ drivers/net/wireless-3.8/iwl7000/hdrs/iwl-chrome.h
@@ -296,4 +296,18 @@ iwl_debugfs_create_bool(const char *name, umode_t mode,
 #define debugfs_create_bool iwl_debugfs_create_bool
 #endif /* < 4.4.0 */
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,19,0))
+static inline int
+skb_ensure_writable(struct sk_buff *skb, int write_len)
+{
+	if (!pskb_may_pull(skb, write_len))
+		return -ENOMEM;
+
+	if (!skb_cloned(skb) || skb_clone_writable(skb, write_len))
+		return 0;
+
+	return pskb_expand_head(skb, 0, 0, GFP_ATOMIC);
+}
+#endif
+
 #endif /* __IWL_CHROME */
