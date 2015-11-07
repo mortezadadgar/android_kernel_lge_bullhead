a3a97a71db299b25c3f3ffa165cec7718adbcd71
Geliang Tang <geliangtang@163.com>
2015-11-06 16:29:06 -0800

    UPSTREAM: zram: make is_partial_io/valid_io_request/page_zero_filled return boolean
    Make is_partial_io()/valid_io_request()/page_zero_filled() return boolean,
    since each function only uses either one or zero as its return value.
    
    BUG=chromium:670864
    TEST=build/boot on elm
    
    Change-Id: Ifab5838a9a9ea9f6ecc42b072f13654442b3fab0
    Signed-off-by: Geliang Tang <geliangtang@163.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 1c53e0d2737f3ce4afa27d5703494eb14610ec26)
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/416643
    Reviewed-by: Cheng-Yu Lee <cylee@chromium.org>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -363,7 +363,7 @@ static void zram_set_obj_size(struct zram_meta *meta,
 	meta->table[index].value = (flags << ZRAM_FLAG_SHIFT) | size;
 }
 
-static inline int is_partial_io(struct bio_vec *bvec)
+static inline bool is_partial_io(struct bio_vec *bvec)
 {
 	return bvec->bv_len != PAGE_SIZE;
 }
@@ -371,25 +371,25 @@ static inline int is_partial_io(struct bio_vec *bvec)
 /*
  * Check if request is within bounds and aligned on zram logical blocks.
  */
-static inline int valid_io_request(struct zram *zram,
+static inline bool valid_io_request(struct zram *zram,
 		sector_t start, unsigned int size)
 {
 	u64 end, bound;
 
 	/* unaligned request */
 	if (unlikely(start & (ZRAM_SECTOR_PER_LOGICAL_BLOCK - 1)))
-		return 0;
+		return false;
 	if (unlikely(size & (ZRAM_LOGICAL_BLOCK_SIZE - 1)))
-		return 0;
+		return false;
 
 	end = start + (size >> SECTOR_SHIFT);
 	bound = zram->disksize >> SECTOR_SHIFT;
 	/* out of range range */
 	if (unlikely(start >= bound || end > bound || start > end))
-		return 0;
+		return false;
 
 	/* I/O request is valid */
-	return 1;
+	return true;
 }
 
 static void zram_meta_free(struct zram_meta *meta, u64 disksize)
@@ -448,7 +448,7 @@ static void update_position(u32 *index, int *offset, struct bio_vec *bvec)
 	*offset = (*offset + bvec->bv_len) % PAGE_SIZE;
 }
 
-static int page_zero_filled(void *ptr)
+static bool page_zero_filled(void *ptr)
 {
 	unsigned int pos;
 	unsigned long *page;
@@ -457,10 +457,10 @@ static int page_zero_filled(void *ptr)
 
 	for (pos = 0; pos != PAGE_SIZE / sizeof(*page); pos++) {
 		if (page[pos])
-			return 0;
+			return false;
 	}
 
-	return 1;
+	return true;
 }
 
 static void handle_zero_page(struct bio_vec *bvec)
