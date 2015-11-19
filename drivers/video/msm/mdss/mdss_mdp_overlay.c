995113b8e7415623e3d7e437d2628719637f2cc2
Dhaval Patel <pdhaval@codeaurora.org>
2015-11-19 11:59:51 -0800

    msm: mdss: avoid vsync_handler update in lp2 power state
    vsync_handler added in lp2 (doze_suspend) power
    state enables the vsync on hardware. That can lead
    to unclocked register access because device can go
    in pm_suspend in this power state. This change blocks
    the vsync_handler processing in lp2 power state.
    
    Change-Id: I4386baa6bc2f8303928edade79108b4983f66f42
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2609,7 +2609,8 @@ int mdss_mdp_overlay_vsync_ctrl(struct msm_fb_data_type *mfd, int en)
         mdp5_data->vsync_en = en;
 
 	if (!ctl->panel_data->panel_info.cont_splash_enabled
-			&& !mdss_mdp_ctl_is_power_on(ctl)) {
+		&& (!mdss_mdp_ctl_is_power_on(ctl) ||
+		mdss_panel_is_power_on_ulp(ctl->power_state))) {
 		pr_debug("fb%d vsync pending first update en=%d\n",
 				mfd->index, en);
 		return 0;
