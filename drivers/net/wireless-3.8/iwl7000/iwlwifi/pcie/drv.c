5e7abc7ce2e369af4805cb8d53c07f2e23eb8be6
Oren Givon <oren.givon@intel.com>
2016-01-21 11:11:11 +0200

    CHROMIUM: iwl7000: switch 0x0810 sub-system id to 8000 series
    There has been a mistake where 0x0810 sub-system PCI ID
    was considered a 9260 series NIC instead of 8265.
    This commit fixes it to be 8265.
    
    Signed-off-by: Oren Givon <oren.givon@intel.com>
    iwl7000-tree: d58b88260c3a50a15b11b17f3d97fc4f154952cc

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/pcie/drv.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/pcie/drv.c
@@ -295,6 +295,7 @@ static const struct pci_device_id iwl_hw_card_ids[] = {
 	{IWL_PCI_DEVICE(0x24F3, 0x0000, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x0010, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x8010, iwl8265_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24FD, 0x0810, iwl8265_2ac_cfg)},
 
 /* 9000 Series */
 	{IWL_PCI_DEVICE(0x9DF0, 0x2A10, iwl5165_2ac_cfg)},
@@ -308,7 +309,6 @@ static const struct pci_device_id iwl_hw_card_ids[] = {
 	{IWL_PCI_DEVICE(0x9DF0, 0x0210, iwl9260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x9DF0, 0x0410, iwl9260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x9DF0, 0x0610, iwl9260_2ac_cfg)},
-	{IWL_PCI_DEVICE(0x24FD, 0x0810, iwl9260_2ac_cfg)},
 #endif /* CPTCFG_IWLMVM */
 
 	{0}
