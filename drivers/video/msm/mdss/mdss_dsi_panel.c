20419162cfca896e74459c24971036034d0f65b4
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-28 01:13:45 +0000

    display: add a simple api to query the display state (on/off) at any point in time
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -21,7 +21,7 @@
 #include <linux/leds.h>
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
-
+#include <linux/display_state.h>
 #include "mdss_dsi.h"
 
 #define DT_CMD_HDR 6
@@ -31,6 +31,13 @@ DEFINE_LED_TRIGGER(bl_led_trigger);
 
 static int mdss_bl_ctrl_panel = false;
 static int bl_default_lvl = 1800;
+
+bool display_on = true;
+
+bool is_display_on(void)
+{
+        return display_on;
+}
 
 void mdss_set_bl_ctrl_by_panel(int enable)
 {
@@ -653,6 +660,8 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	display_on = true;
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -711,6 +720,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	if (ctrl->off_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
+
+	display_on = false;
 
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
