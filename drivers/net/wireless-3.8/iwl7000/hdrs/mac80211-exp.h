8e791f4a10e381e14115005f1e913dc963d4a8f0
Sara Sharon <sara.sharon@intel.com>
2016-02-03 15:04:39 +0200

    CHROMIUM: iwl7000: mac80211: provide interface to driver to set VHT MU-MIMO data
    Provide an interface to the lower level driver to set the VHT
    MU-MIMO data. This is needed for example when there is an update
    of the group data during low power state, where the management
    frame will not be passed to the host at all.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    iwl7000-tree: b8dd8c6005fdb4e02b455ef8691c913ccdbdf19b

--- drivers/net/wireless-3.8/iwl7000/hdrs/mac80211-exp.h
+++ drivers/net/wireless-3.8/iwl7000/hdrs/mac80211-exp.h
@@ -136,4 +136,5 @@
 #define ieee80211_radar_detected __iwl7000_ieee80211_radar_detected
 #define ieee80211_update_p2p_noa __iwl7000_ieee80211_update_p2p_noa
 #define ieee80211_parse_p2p_noa __iwl7000_ieee80211_parse_p2p_noa
+#define ieee80211_update_mu_groups __iwl7000_ieee80211_update_mu_groups
 #endif
