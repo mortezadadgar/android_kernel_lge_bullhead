c211c58fde7859dcb146ee5ef85796ce0f4b3e8b
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2016-02-08 12:47:28 +0200

    CHROMIUM: iwl7000: bump API to 22 for
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    iwl7000-tree: a98cb0bba145e0d2b049c297fbc02b17c2920aa5

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-7000.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-7000.c
@@ -73,8 +73,8 @@
 /* Highest firmware API version supported */
 #define IWL7260_UCODE_API_MAX	17
 #define IWL7265_UCODE_API_MAX	17
-#define IWL7265D_UCODE_API_MAX	21
-#define IWL3168_UCODE_API_MAX	21
+#define IWL7265D_UCODE_API_MAX	22
+#define IWL3168_UCODE_API_MAX	22
 
 /* Oldest version we won't warn about */
 #define IWL7260_UCODE_API_OK	13
