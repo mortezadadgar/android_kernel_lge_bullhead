444b9b0796e1030b0a9f4ca4d539722071eacfb6
Andrei Otcheretianski <andrei.otcheretianski@intel.com>
2016-03-01 13:25:43 +0200

    CHROMIUM: iwl7000: make VHT disable default value configurable at compile time
    Introduce a new Kconfig symbol which controls the default setting for
    disable_11ac module parameter.
    
    Signed-off-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>
    iwl7000-tree: 604ca42e14b21c59c547fa9475dc963f78477b6b

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-drv.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-drv.c
@@ -1902,6 +1902,7 @@ struct iwl_mod_params iwlwifi_mod_params = {
 #ifndef CPTCFG_IWLWIFI_UAPSD
 	.uapsd_disable = true,
 #endif /* CPTCFG_IWLWIFI_UAPSD */
+	.disable_11ac = IS_ENABLED(CPTCFG_IWLWIFI_VHT_DEFAULT_DISABLE),
 	/* the rest are 0 by default */
 };
 IWL_EXPORT_SYMBOL(iwlwifi_mod_params);
@@ -2136,4 +2137,8 @@ MODULE_PARM_DESC(d0i3_timeout, "Timeout to D0i3 entry when idle (ms)");
 
 module_param_named(disable_11ac, iwlwifi_mod_params.disable_11ac, bool,
 		   S_IRUGO);
-MODULE_PARM_DESC(disable_11ac, "Disable VHT capabilities");
+#ifdef CPTCFG_IWLWIFI_VHT_DEFAULT_DISABLE
+MODULE_PARM_DESC(disable_11ac, "Disable VHT capabilities (default: true)");
+#else
+MODULE_PARM_DESC(disable_11ac, "Disable VHT capabilities (default: false)");
+#endif
