ef1d28077e1b9decf7fd6ccd8f0526175356f208
Matti Gottlieb <matti.gottlieb@intel.com>
2016-03-10 16:37:29 +0200

    CHROMIUM: iwl7000: xvt: Paging: Make sure FW contains the right amount of sections
    Paging contains 3 sections in the fw. The first for the paging separator,
    The second for the CSS block, the third with the paging data.
    
    Currently if the driver finds the paging separator, and there is only
    section left (CSS), once reading the CSS section, the driver will
    attempt to read the paging data and will go out of the arrays bounds.
    
    Make sure that the FW image contains the right amount of sections for
    paging.
    
    Signed-off-by: Matti Gottlieb <matti.gottlieb@intel.com>
    iwl7000-tree: 999336125a9a7b23a87ee2e9e6dad020e3949c9c

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/xvt/fw.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/xvt/fw.c
@@ -133,8 +133,12 @@ static int iwl_xvt_fill_paging_mem(struct iwl_xvt *xvt,
 		}
 	}
 
-	if (sec_idx >= IWL_UCODE_SECTION_MAX) {
-		IWL_ERR(xvt, "driver didn't find paging image\n");
+	/*
+	 * If paging is enabled there should be at least 2 more sections left
+	 * (one for CSS and one for Paging data)
+	 */
+	if (sec_idx >= ARRAY_SIZE(image->sec) - 1) {
+		IWL_ERR(xvt, "Paging: Missing CSS and/or paging sections\n");
 		iwl_xvt_free_fw_paging(xvt);
 		return -EINVAL;
 	}
