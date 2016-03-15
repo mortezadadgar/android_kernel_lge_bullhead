738fddf6a2121f3903b837290d0852db87214f6a
Sara Sharon <sara.sharon@intel.com>
2016-03-15 19:33:55 +0200

    CHROMIUM: iwl7000: mvm: fix mcast filtering
    [use 1 << N in vendor-cmd.h to fix the conflict]
    
    When driver rx filters command was added to the driver it
    broke any platform that don't support rx filters command.
    Since rx filter is not configured, the mcast_active_filter_cmd
    command does not contains anything but mcast dns.
    This has the devastating result of IPv6 not working at all -
    OS configuration is getting overridden by the empty rx filter
    and all NS messages get filtered out.
    Fix that by configuring and considering mcast_active_filter_cmd
    only if some rx filter was configured.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    iwl7000-tree: 012b5625c7e5e2f30d3311fa7034bb8b655e55c0

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/mvm/vendor-cmd.h
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/mvm/vendor-cmd.h
@@ -350,13 +350,15 @@ enum iwl_mvm_vendor_significant_change_result {
  * @IWL_MVM_VENDOR_RXFILTER_BCAST: control broadcast Rx filter
  * @IWL_MVM_VENDOR_RXFILTER_MCAST4: control IPv4 multicast Rx filter
  * @IWL_MVM_VENDOR_RXFILTER_MCAST6: control IPv4 multicast Rx filter
+ * @IWL_MVM_VENDOR_RXFILTER_EINVAL: no Rx filter command was set
  *
  */
 enum iwl_mvm_vendor_rxfilter_flags {
-	IWL_MVM_VENDOR_RXFILTER_UNICAST = BIT(0),
-	IWL_MVM_VENDOR_RXFILTER_BCAST = BIT(1),
-	IWL_MVM_VENDOR_RXFILTER_MCAST4 = BIT(2),
-	IWL_MVM_VENDOR_RXFILTER_MCAST6 = BIT(3),
+	IWL_MVM_VENDOR_RXFILTER_UNICAST = 1 << 0,
+	IWL_MVM_VENDOR_RXFILTER_BCAST = 1 << 1,
+	IWL_MVM_VENDOR_RXFILTER_MCAST4 = 1 << 2,
+	IWL_MVM_VENDOR_RXFILTER_MCAST6 = 1 << 3,
+	IWL_MVM_VENDOR_RXFILTER_EINVAL = 1 << 7,
 };
 
 /**
