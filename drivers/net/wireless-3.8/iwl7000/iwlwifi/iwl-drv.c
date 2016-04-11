9ff3b3f1453478dfe5221a7560e7317b41a11285
Sara Sharon <sara.sharon@intel.com>
2016-04-11 15:01:46 +0300

    CHROMIUM: iwl7000: 8000: fix MODULE_FIRMWARE input
    The firwmare name for 8000 is iwlwifi-8000C. The C is
    appended based on a value read from a register. This
    allows to load different firwmare versions based on
    the hardware step during development. Now that the
    hardware development is completed, we can hard code
    the 'C' and along the way, fix the input to
    MODULE_FIRMWARE.
    
    This fixes:
    https://bugzilla.kernel.org/show_bug.cgi?id=116041
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    iwl7000-tree: 8f3ea75871010335cb8682f6900d7256f41e1d64

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-drv.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-drv.c
@@ -441,19 +441,6 @@ static int iwl_request_firmware(struct iwl_drv *drv, bool first)
 	snprintf(drv->firmware_name, sizeof(drv->firmware_name), "%s%s.ucode",
 		 name_pre, tag);
 
-	/*
-	 * Starting 8000B - FW name format has changed. This overwrites the
-	 * previous name and uses the new format.
-	 */
-	if (drv->trans->cfg->device_family == IWL_DEVICE_FAMILY_8000) {
-		char rev_step = 'A' + CSR_HW_REV_STEP(drv->trans->hw_rev);
-
-		if (rev_step != 'A')
-			snprintf(drv->firmware_name,
-				 sizeof(drv->firmware_name), "%s%c-%s.ucode",
-				 name_pre, rev_step, tag);
-	}
-
 #ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES
 #ifdef CPTCFG_IWLWIFI_DEVICE_TESTMODE
 	/* check if upload firmware code is required */
