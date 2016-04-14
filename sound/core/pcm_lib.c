64f390769ebfeb3f5b7625a56652610cbbc4535c
Takashi Iwai <tiwai@suse.de>
2016-04-14 18:02:37 +0200

    UPSTREAM: ALSA: pcm : Call kill_fasync() in stream lock
    Currently kill_fasync() is called outside the stream lock in
    snd_pcm_period_elapsed().  This is potentially racy, since the stream
    may get released even during the irq handler is running.  Although
    snd_pcm_release_substream() calls snd_pcm_drop(), this doesn't
    guarantee that the irq handler finishes, thus the kill_fasync() call
    outside the stream spin lock may be invoked after the substream is
    detached, as recently reported by KASAN.
    
    As a quick workaround, move kill_fasync() call inside the stream
    lock.  The fasync is rarely used interface, so this shouldn't have a
    big impact from the performance POV.
    
    Ideally, we should implement some sync mechanism for the proper finish
    of stream and irq handler.  But this oneliner should suffice for most
    cases, so far.
    
    BUG=chromium:677988
    TEST=Build and test
    
    Change-Id: I6e3a018f7eee37ebb11664a6d3d1d1161d79e6a7
    Reported-by: Baozeng Ding <sploving1@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    (cherry picked from commit 3aa02cb664c5fb1042958c8d1aa8c35055a2ebc4)
    Reviewed-on: https://chromium-review.googlesource.com/424377
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    (cherry picked from commit 54fc8953707762529b63f605a907539d1a4b3dfe)
    Reviewed-on: https://chromium-review.googlesource.com/427246
    (cherry picked from commit a6f749f634e33600c0d3fd5d6d96ceade8eedd55)
    Reviewed-on: https://chromium-review.googlesource.com/428139

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -1851,8 +1851,8 @@ void snd_pcm_period_elapsed(struct snd_pcm_substream *substream)
 	if (substream->timer_running)
 		snd_timer_interrupt(substream->timer, 1);
  _end:
-	snd_pcm_stream_unlock_irqrestore(substream, flags);
 	kill_fasync(&runtime->fasync, SIGIO, POLL_IN);
+	snd_pcm_stream_unlock_irqrestore(substream, flags);
 }
 
 EXPORT_SYMBOL(snd_pcm_period_elapsed);
