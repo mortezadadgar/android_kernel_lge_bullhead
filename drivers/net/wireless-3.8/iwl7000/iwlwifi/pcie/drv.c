20e76f4af123bbbb119ce2d9d11678588e03b802
Oren Givon <oren.givon@intel.com>
2016-05-23 09:58:17 +0300

    CHROMIUM: iwl7000: add new 8265 and 8260 PCI IDs
    Add 2 new 8265 series PCI IDs:
      - (0x24FD, 0x1130)
      - (0x24FD, 0x0130)
    Add 1 new 8260 series PCI IDs:
      - (0x24F3, 0x10B0)
    
    CC: <stable@vger.kernel.org> [4.6]
    Signed-off-by: Oren Givon <oren.givon@intel.com>
    iwl7000-tree: f98f740bd691c76f83e193e7c74a44c3ff69fb8c

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/pcie/drv.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/pcie/drv.c
@@ -251,6 +251,7 @@ static const struct pci_device_id iwl_hw_card_ids[] = {
 /* 8000 Series */
 	{IWL_PCI_DEVICE(0x24F3, 0x0010, iwl8260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24F3, 0x1010, iwl8260_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24F3, 0x10B0, iwl8260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24F3, 0x0130, iwl8260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24F3, 0x1130, iwl8260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24F3, 0x0132, iwl8260_2ac_cfg)},
@@ -299,6 +300,8 @@ static const struct pci_device_id iwl_hw_card_ids[] = {
 	{IWL_PCI_DEVICE(0x24FD, 0x0010, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x0110, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x1110, iwl8265_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24FD, 0x1130, iwl8265_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24FD, 0x0130, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x1010, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x0050, iwl8265_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24FD, 0x0150, iwl8265_2ac_cfg)},
