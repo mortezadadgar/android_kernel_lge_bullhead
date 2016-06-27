797da8a415dcdbf62c5dfcb22a70b6c7e3ba9231
Denis Kenzior <denkenz@gmail.com>
2016-06-27 11:01:12 -0500

    UPSTREAM: Bluetooth: Fix bt_sock_recvmsg return value
    If recvmsg is called with a destination buffer that is too small to
    receive the contents of skb in its entirety, the return value from
    recvmsg was inconsistent with common SOCK_SEQPACKET or SOCK_DGRAM
    semantics.
    
    If destination buffer provided by userspace is too small (e.g. len <
    copied), then MSG_TRUNC flag is set and copied is returned.  Instead, it
    should return the length of the message, which is consistent with how
    other datagram based sockets act.  Quoting 'man recv':
    
    "All  three calls return the length of the message on successful comple
    tion.  If a message is too long to fit in the supplied  buffer,  excess
    bytes  may  be discarded depending on the type of socket the message is
    received from."
    
    and
    
    "MSG_TRUNC (since Linux 2.2)
    
        For   raw   (AF_PACKET),   Internet   datagram   (since    Linux
        2.4.27/2.6.8),  netlink  (since Linux 2.6.22), and UNIX datagram
        (since Linux 3.4) sockets: return the real length of the packet
        or datagram, even when it was longer than the passed buffer."
    
    BUG=chromium:704196
    TEST=Deploy v5.44 BlueZ system daemon with v4.9 BlueZ kernel on Nyan_Big and
    perform automated tests (suite:bluetooth) and manual tests against mouses,
    keyboards and headphones.
    
    Change-Id: I9c471032f1a700ab6e0341667a5fa19692c56766
    Signed-off-by: Denis Kenzior <denkenz@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Miao-chen Chou <mcchou@chromium.org>
    (cherry picked from commit b5f34f9420b50c9b5876b9a2b68e96be6d629054)
    Reviewed-on: https://chromium-review.googlesource.com/480121
    Tested-by: Shyh-In Hwang <josephsih@chromium.org>
    Reviewed-by: Shyh-In Hwang <josephsih@chromium.org>

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -215,6 +215,7 @@ int bt_sock_recvmsg(struct kiocb *iocb, struct socket *sock,
 	struct sock *sk = sock->sk;
 	struct sk_buff *skb;
 	size_t copied;
+	size_t skblen;
 	int err;
 
 	BT_DBG("sock %p sk %p len %zu", sock, sk, len);
@@ -231,6 +232,7 @@ int bt_sock_recvmsg(struct kiocb *iocb, struct socket *sock,
 		return err;
 	}
 
+	skblen = skb->len;
 	copied = skb->len;
 	if (len < copied) {
 		msg->msg_flags |= MSG_TRUNC;
@@ -250,6 +252,9 @@ int bt_sock_recvmsg(struct kiocb *iocb, struct socket *sock,
 	}
 
 	skb_free_datagram(sk, skb);
+
+	if (msg->msg_flags & MSG_TRUNC)
+		copied = skblen;
 
 	return err ? : copied;
 }
