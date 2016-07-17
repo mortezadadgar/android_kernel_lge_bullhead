1c6b8c1a8e0a6589aa433bce07e8bed6bc51c813
Marcel Holtmann <marcel@holtmann.org>
2016-07-17 19:55:16 +0200

    UPSTREAM: Bluetooth: Add debugfs fields for hardware and firmware info
    Some Bluetooth controllers allow for reading hardware and firmware
    related vendor specific infos. If they are available, then they can be
    exposed via debugfs now.
    
    BUG=chromium:704196
    TEST=Deploy v5.44 BlueZ system daemon with v4.9 BlueZ kernel on Nyan_Big and
    perform automated tests (suite:bluetooth) and manual tests against mouses,
    keyboards and headphones.
    
    Change-Id: I553f8e1b7b05ffe0475f3d05b4be937fb1afd12c
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Miao-chen Chou <mcchou@chromium.org>
    (cherry picked from commit 5177a83827cd0b8cf6ce0391b00dd4417352d2f1)
    Reviewed-on: https://chromium-review.googlesource.com/480137
    Tested-by: Shyh-In Hwang <josephsih@chromium.org>
    Reviewed-by: Shyh-In Hwang <josephsih@chromium.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -3170,6 +3170,8 @@ void hci_unregister_dev(struct hci_dev *hdev)
 	device_del(&hdev->dev);
 
 	debugfs_remove_recursive(hdev->debugfs);
+	kfree_const(hdev->hw_info);
+	kfree_const(hdev->fw_info);
 
 	destroy_workqueue(hdev->workqueue);
 	destroy_workqueue(hdev->req_workqueue);
@@ -3272,6 +3274,28 @@ int hci_recv_diag(struct hci_dev *hdev, struct sk_buff *skb)
 	return 0;
 }
 EXPORT_SYMBOL(hci_recv_diag);
+
+void hci_set_hw_info(struct hci_dev *hdev, const char *fmt, ...)
+{
+	va_list vargs;
+
+	va_start(vargs, fmt);
+	kfree_const(hdev->hw_info);
+	hdev->hw_info = kvasprintf(GFP_KERNEL, fmt, vargs);
+	va_end(vargs);
+}
+EXPORT_SYMBOL(hci_set_hw_info);
+
+void hci_set_fw_info(struct hci_dev *hdev, const char *fmt, ...)
+{
+	va_list vargs;
+
+	va_start(vargs, fmt);
+	kfree_const(hdev->fw_info);
+	hdev->fw_info = kvasprintf(GFP_KERNEL, fmt, vargs);
+	va_end(vargs);
+}
+EXPORT_SYMBOL(hci_set_fw_info);
 
 /* ---- Interface to upper protocols ---- */
 
