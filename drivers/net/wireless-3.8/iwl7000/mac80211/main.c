cf4003176bae313a3466285a44d3fa3462ebf586
Sara Sharon <sara.sharon@intel.com>
2016-09-15 17:17:28 +0300

    CHROMIUM: iwl7000: mac80211: add a HW flag for supporting HW TX fragmentation
    Currently mac80211 determines whether HW does fragmentation
    by checking whether the set_frag_threshold callback is set
    or not.
    However, some drivers may want to set the HW fragmentation
    capability depending on HW generation.
    Allow this by checking a HW flag instead of checking the
    callback.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    iwl7000-tree: a9c59cb31710e14c72ea98e66b1e408fe3cffe14

--- drivers/net/wireless-3.8/iwl7000/mac80211/main.c
+++ drivers/net/wireless-3.8/iwl7000/mac80211/main.c
@@ -837,6 +837,10 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 	     !local->ops->tdls_recv_channel_switch))
 		return -EOPNOTSUPP;
 
+	if (WARN_ON(ieee80211_hw_check(hw, SUPPORTS_TX_FRAG) &&
+		    !local->ops->set_frag_threshold))
+		return -EINVAL;
+
 	if (WARN_ON(ieee80211_has_nan_iftype(local->hw.wiphy->interface_modes) &&
 		    (!local->ops->start_nan || !local->ops->stop_nan)))
 		return -EINVAL;
