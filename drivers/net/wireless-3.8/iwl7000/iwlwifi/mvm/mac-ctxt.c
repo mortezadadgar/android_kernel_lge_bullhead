2befd3ca41ef668e5916a5ba651d260bcf014fd0
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2016-10-13 11:49:31 +0300

    CHROMIUM: iwl7000: mvm: workaround for a FW bug and pick the beacon transmission's antenna
    The firmware is currently buggy and can't handle the case
    in which we let the firmware choose the antenna to use for
    the beacon transmission. As a workaround, always choose the
    antenna in the driver until the bug is fixed in the
    firmware.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    iwl7000-tree: 6ef53bb387f6d2350ca3c532962f77cba4a5cee2

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/mvm/mac-ctxt.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/mvm/mac-ctxt.c
@@ -1095,8 +1095,12 @@ static int iwl_mvm_mac_ctxt_send_beacon(struct iwl_mvm *mvm,
 						TX_CMD_FLG_BT_PRIO_POS;
 	u.beacon_cmd_v6.tx.tx_flags = cpu_to_le32(tx_flags);
 
+	/*
+	 * TODO: the firwmare advertises this, but has a bug. We should revert
+	 *	 this when the firmware will be fixed.
+	 */
 	if (!fw_has_capa(&mvm->fw->ucode_capa,
-			 IWL_UCODE_TLV_CAPA_BEACON_ANT_SELECTION)) {
+			 IWL_UCODE_TLV_CAPA_BEACON_ANT_SELECTION) || true) {
 		mvm->mgmt_last_antenna_idx =
 			iwl_mvm_next_antenna(mvm, iwl_mvm_get_valid_tx_ant(mvm),
 					     mvm->mgmt_last_antenna_idx);
