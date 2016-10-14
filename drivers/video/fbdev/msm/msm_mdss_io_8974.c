70b2a339a4f08260b942094006c842ba67680c1c
Angelo G. Del Regno <kholk11@gmail.com>
2016-10-14 12:13:00 +0200

    video: msm: msm_mdss_io_8974: Add DSI PHY revision hack for MSM8992
    MSM8994's dsi phy revision gets somehow detected as 2.0, but its
    revision is 1.0.
    Hack it for now.
    
    [md:s/MSM8994/MSM8992]
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/video/fbdev/msm/msm_mdss_io_8974.c
+++ drivers/video/fbdev/msm/msm_mdss_io_8974.c
@@ -485,6 +485,10 @@ static void mdss_dsi_phy_sw_reset_sub(struct mdss_dsi_ctrl_pdata *ctrl)
 	sdata = ctrl->shared_data;
 	octrl = mdss_dsi_get_other_ctrl(ctrl);
 
+#ifdef CONFIG_ARCH_MSM8992
+	ctrl->shared_data->phy_rev = DSI_PHY_REV_10;
+#endif
+
 	if (ctrl->shared_data->phy_rev == DSI_PHY_REV_20) {
 		if (mdss_dsi_is_ctrl_clk_master(ctrl))
 			sctrl = mdss_dsi_get_ctrl_clk_slave();
@@ -585,11 +589,13 @@ static void mdss_dsi_phy_regulator_disable(struct mdss_dsi_ctrl_pdata *ctrl)
 		return;
 	}
 
+#ifndef CONFIG_ARCH_MSM8992
 	if (ctrl->shared_data->phy_rev == DSI_PHY_REV_20)
 		return;
 
 	if (ctrl->shared_data->phy_rev == DSI_PHY_REV_30)
 		return;
+#endif
 
 	MIPI_OUTP(ctrl->phy_regulator_io.base + 0x018, 0x000);
 }
@@ -600,6 +606,10 @@ static void mdss_dsi_phy_shutdown(struct mdss_dsi_ctrl_pdata *ctrl)
 		pr_err("%s: Invalid input data\n", __func__);
 		return;
 	}
+
+#ifdef CONFIG_ARCH_MSM8992
+	ctrl->shared_data->phy_rev = DSI_PHY_REV_10;
+#endif
 
 	if (ctrl->shared_data->phy_rev == DSI_PHY_REV_20) {
 		MIPI_OUTP(ctrl->phy_io.base + DSIPHY_PLL_CLKBUFLR_EN, 0);
@@ -627,10 +637,10 @@ void mdss_dsi_lp_cd_rx(struct mdss_dsi_ctrl_pdata *ctrl)
 		pr_err("%s: Invalid input data\n", __func__);
 		return;
 	}
-
+#ifndef CONFIG_ARCH_MSM8992
 	if (ctrl->shared_data->phy_rev == DSI_PHY_REV_20)
 		return;
-
+#endif
 	pd = &(((ctrl->panel_data).panel_info.mipi).dsi_phy_db);
 
 	/* Strength ctrl 1, LP Rx + CD Rxcontention detection */
@@ -1308,6 +1318,9 @@ static void mdss_dsi_phy_regulator_ctrl(struct mdss_dsi_ctrl_pdata *ctrl,
 
 	sdata = ctrl->shared_data;
 	other_ctrl = mdss_dsi_get_other_ctrl(ctrl);
+#ifdef CONFIG_ARCH_MSM8992
+	ctrl->shared_data->phy_rev = DSI_PHY_REV_10;
+#endif
 
 	mutex_lock(&sdata->phy_reg_lock);
 	if (enable) {
@@ -1359,6 +1372,10 @@ static void mdss_dsi_phy_ctrl(struct mdss_dsi_ctrl_pdata *ctrl, bool enable)
 		pr_err("%s: Invalid input data\n", __func__);
 		return;
 	}
+
+#ifdef CONFIG_ARCH_MSM8992
+	ctrl->shared_data->phy_rev = DSI_PHY_REV_10;
+#endif
 
 	if (enable) {
 
