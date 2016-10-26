2458de1be22cc1816a271ad1175c4f599cf9519b
Luca Coelho <luciano.coelho@intel.com>
2016-10-26 16:04:43 +0300

    CHROMIUM: iwl7000: mvm: bump max API to 27
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    (cherry picked from commit 3fa00219f85744a50b5e1f20ac484e280118473d)
    iwl7000-tree: 8940cfe80fdceae68bce6cde3ca66ebacd2999bc

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-8000.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-8000.c
@@ -70,8 +70,8 @@
 #include "iwl-agn-hw.h"
 
 /* Highest firmware API version supported */
-#define IWL8000_UCODE_API_MAX	26
-#define IWL8265_UCODE_API_MAX	26
+#define IWL8000_UCODE_API_MAX	27
+#define IWL8265_UCODE_API_MAX	27
 
 /* Lowest firmware API version supported */
 #define IWL8000_UCODE_API_MIN	17
