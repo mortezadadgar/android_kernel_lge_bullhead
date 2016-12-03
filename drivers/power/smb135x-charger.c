96fd7f25afa65ef8fbc180c4d1ecfbe50c23a036
Francisco Franco <franciscofranco.1990@gmail.com>
2016-12-03 04:24:34 +0000

    power: smb135x-charger: queue work on system_power_efficient_wq
    There doesn't seem to be any real dependency of scheduling these on
    the cpu which scheduled them, so moving every *_delayed to the
    power efficient wq save potential needlessly idle cpu wake ups
    leaving the scheduler to decide the most appropriate cpus to wake up.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -2604,7 +2604,8 @@ static int otg_oc_handler(struct smb135x_chg *chip, u8 rt_stat)
 	}
 
 	pr_debug("rt_stat = 0x%02x\n", rt_stat);
-	schedule_delayed_work(&chip->reset_otg_oc_count_work,
+	queue_delayed_work(system_power_efficient_wq,
+		&chip->reset_otg_oc_count_work,
 			msecs_to_jiffies(RESET_OTG_OC_COUNT_MS));
 	mutex_unlock(&chip->otg_oc_count_lock);
 	return 0;
@@ -2626,7 +2627,8 @@ static int handle_dc_removal(struct smb135x_chg *chip)
 static int handle_dc_insertion(struct smb135x_chg *chip)
 {
 	if (chip->dc_psy_type == POWER_SUPPLY_TYPE_WIRELESS)
-		schedule_delayed_work(&chip->wireless_insertion_work,
+		queue_delayed_work(system_power_efficient_wq,
+			&chip->wireless_insertion_work,
 			msecs_to_jiffies(DCIN_UNSUSPEND_DELAY_MS));
 	if (chip->dc_psy_type != -EINVAL)
 		power_supply_set_online(&chip->dc_psy,
