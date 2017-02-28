ff9cafd0461994fbf3c2ca8299cd8d49277707ae
Johannes Berg <johannes.berg@intel.com>
2017-02-28 22:45:25 +0100

    CHROMIUM: iwl7000: remove module loading failure message
    When CONFIG_DEBUG_TEST_DRIVER_REMOVE is set, iwlwifi crashes
    when the opmode module cannot be loaded, due to completing
    the completion before using drv->dev, which can then already
    be freed.
    
    Fix this by removing the (fairly useless) message. Moving the
    completion later causes a deadlock instead, so that's not an
    option.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 5781d6d035d5b49fa290f713dd870f668d50cc84

--- drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-drv.c
+++ drivers/net/wireless-3.8/iwl7000/iwlwifi/iwl-drv.c
@@ -1738,15 +1738,8 @@ static void iwl_req_fw_callback(const struct firmware *ucode_raw, void *context)
 	 * else from proceeding if the module fails to load
 	 * or hangs loading.
 	 */
-	if (load_module) {
+	if (load_module)
 		err = request_module("%s", op->name);
-#ifdef CPTCFG_IWLWIFI_OPMODE_MODULAR
-		if (err)
-			IWL_ERR(drv,
-				"failed to load module %s (error %d), is dynamic loading enabled?\n",
-				op->name, err);
-#endif
-	}
 	kfree(pieces);
 	return;
 
