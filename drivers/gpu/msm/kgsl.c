5f2b1ee3e2a9db47f28e228e6dad82142c5a9c4c
Hareesh Gundu <hareeshg@codeaurora.org>
2017-06-07 14:50:15 +0530

    msm: kgsl: Defer issue commands to worker thread
    Currently submit ioctl getting blocked till the commands
    gets added to ringbuffer incase inflight count is less
    than context burst count. If the submit command happens
    in GPU slumber state, it will add the GPU wakeup time to
    submit IOCTL. This will add latency in preparing next frame
    in CPU side. Defer commands submission to dispatcher worker,
    if the GPU is in slumber state.
    
    CRs-Fixed: 2055107
    Change-Id: I099ba721e02bbcd8ccadb1bc518c7c1ef4fb7e21
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4735,6 +4735,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		device->id, device->reg_phys, device->reg_len);
 
 	rwlock_init(&device->context_lock);
+	spin_lock_init(&device->submit_lock);
 
 	result = kgsl_drm_init(device->pdev);
 	if (result)
