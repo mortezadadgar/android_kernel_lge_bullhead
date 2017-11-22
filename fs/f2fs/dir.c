84a88402bae50e5eb4566d96653bca2ca02fcc99
Sheng Yong <shengyong1@huawei.com>
2017-11-22 18:23:38 +0800

    f2fs: introduce sysfs readdir_ra to readahead inode block in readdir
    This patch introduces a sysfs interface readdir_ra to enable/disable
    readaheading inode block in f2fs_readdir. When readdir_ra is enabled,
    it improves the performance of "readdir + stat".
    
    For 300,000 files:
    	time find /data/test > /dev/null
    disable readdir_ra: 1m25.69s real  0m01.94s user  0m50.80s system
    enable  readdir_ra: 0m18.55s real  0m00.44s user  0m15.39s system
    
    Signed-off-by: Sheng Yong <shengyong1@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -801,6 +801,7 @@ int f2fs_fill_dentries(struct file *file, void *dirent, filldir_t filldir,
 	struct f2fs_dir_entry *de = NULL;
 	struct fscrypt_str de_name = FSTR_INIT(NULL, 0);
 	int over;
+	struct f2fs_sb_info *sbi = F2FS_I_SB(d->inode);
 
 	while (bit_pos < d->max) {
 		d_type = DT_UNKNOWN;
@@ -841,6 +842,9 @@ int f2fs_fill_dentries(struct file *file, void *dirent, filldir_t filldir,
 			file->f_pos += bit_pos - start_bit_pos;
 			return 1;
 		}
+
+		if (sbi->readdir_ra == 1)
+			ra_node_page(sbi, le32_to_cpu(de->ino));
 
 		bit_pos += GET_DENTRY_SLOTS(le16_to_cpu(de->name_len));
 	}
