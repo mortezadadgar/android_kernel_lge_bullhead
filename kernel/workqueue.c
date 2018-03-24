99e52532d01dce43ca97e2008029acbcaaf85f14
Sultanxda <sultanxda@gmail.com>
2018-03-24 13:54:18 -0700

    Revert "kernel: Force as many kernel tasks as possible onto CPU0"
    Holy sweet mother of fail, the meat of this commit was placed into
    sched_setnuma(), which isn't even used in this kernel! Also, actually
    forcing all kthreads onto CPU0 leads to poor performance, so this wasn't
    a good idea.
    
    This reverts commit 1a0fdf6483d5af2f3c201ab2147edf865d8cb251.

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1336,6 +1336,7 @@ static void __queue_work(int cpu, struct workqueue_struct *wq,
 	struct worker_pool *last_pool;
 	struct list_head *worklist;
 	unsigned int work_flags;
+	unsigned int req_cpu = cpu;
 
 	/*
 	 * While a work item is PENDING && off queue, a task trying to
@@ -1352,6 +1353,9 @@ static void __queue_work(int cpu, struct workqueue_struct *wq,
 	    WARN_ON_ONCE(!is_chained_work(wq)))
 		return;
 retry:
+	if (req_cpu == WORK_CPU_UNBOUND)
+		cpu = raw_smp_processor_id();
+
 	/* pwq which will be used unless @work is executing elsewhere */
 	if (!(wq->flags & WQ_UNBOUND))
 		pwq = per_cpu_ptr(wq->cpu_pwqs, cpu);
@@ -1402,7 +1406,7 @@ retry:
 	}
 
 	/* pwq determined, queue */
-	trace_workqueue_queue_work(cpu, pwq, work);
+	trace_workqueue_queue_work(req_cpu, pwq, work);
 
 	if (WARN_ON(!list_empty(&work->entry))) {
 		spin_unlock(&pwq->pool->lock);
@@ -1492,7 +1496,10 @@ static void __queue_delayed_work(int cpu, struct workqueue_struct *wq,
 	dwork->cpu = cpu;
 	timer->expires = jiffies + delay;
 
-	add_timer_on(timer, cpu);
+	if (unlikely(cpu != WORK_CPU_UNBOUND))
+		add_timer_on(timer, cpu);
+	else
+		add_timer(timer);
 }
 
 /**
@@ -4480,7 +4487,7 @@ bool current_is_workqueue_rescuer(void)
  * no synchronization around this function and the test result is
  * unreliable and only useful as advisory hints or for debugging.
  *
- * If @cpu is WORK_CPU_UNBOUND, the test is performed on CPU0.
+ * If @cpu is WORK_CPU_UNBOUND, the test is performed on the local CPU.
  * Note that both per-cpu and unbound workqueues may be associated with
  * multiple pool_workqueues which have separate congested states.  A
  * workqueue being congested on one CPU doesn't mean the workqueue is also
@@ -4495,6 +4502,10 @@ bool workqueue_congested(int cpu, struct workqueue_struct *wq)
 	bool ret;
 
 	rcu_read_lock_sched();
+
+	if (cpu == WORK_CPU_UNBOUND)
+		cpu = smp_processor_id();
+
 	if (!(wq->flags & WQ_UNBOUND))
 		pwq = per_cpu_ptr(wq->cpu_pwqs, cpu);
 	else
