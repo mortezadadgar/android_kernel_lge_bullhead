3e62e8aa3490488cae73bd3da49bb5f6eb8d28e3
mortezadadgar <mortezadadgar97@gmail.com>
2018-03-27 14:31:40 +0430

    fpc1020: Adapt fingerprint process for oreo
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -219,7 +219,7 @@ void thaw_fingerprintd(void)
 
 	read_lock(&tasklist_lock);
 	for_each_process(p) {
-		if (!memcmp(p->comm, "fingerprintd", 13)) {
+		if (!memcmp(p->comm, "fingerprint", 12) || !memcmp(p->comm, "fingerprint@2.1", 16) ) {
 			__thaw_task(p);
 			break;
 		}
