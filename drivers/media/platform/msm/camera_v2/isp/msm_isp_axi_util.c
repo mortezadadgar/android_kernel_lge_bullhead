569260826de6b77662ca4fd2e4f572fa58fcbef6
Vijay Kumar TM <vtmuni@codeaurora.org>
2018-04-13 17:04:00 +0530

    mm-camera2:isp2: Release buffer lock after use
    At start axi stream, we are acquiring the
    buffer lock and releasing after completing
    the stream configuration operations. In case
    of live snapshot,this is causing the buffer
    operations to halt and leading to sof freeze.
    
    CRs-Fixed: 2149998
    Change-Id: I7f57c8befc85c105a5690ac71e0276f42b9f1929
    Signed-off-by: Meera Gande <mgande@codeaurora.org>
    Signed-off-by: Vijay Kumar TM <vtmuni@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1969,10 +1969,11 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev,
 		vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id = 0;
 		vfe_dev->axi_data.src_info[VFE_PIX_0].camif_sof_frame_id = 0;
 	}
-
+	mutex_lock(&vfe_dev->buf_mgr->lock);
 	for (i = 0; i < stream_cfg_cmd->num_streams; i++) {
 		if (HANDLE_TO_IDX(stream_cfg_cmd->stream_handle[i]) >=
 			MAX_NUM_STREAM) {
+			mutex_unlock(&vfe_dev->buf_mgr->lock);
 			return -EINVAL;
 		}
 		stream_info = &axi_data->stream_info[
@@ -1982,6 +1983,7 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev,
 				SRC_TO_INTF(stream_info->stream_src)].active;
 		else {
 			ISP_DBG("%s: invalid src info index\n", __func__);
+			mutex_unlock(&vfe_dev->buf_mgr->lock);
 			return -EINVAL;
 		}
 
@@ -1993,6 +1995,7 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev,
 			pr_err("%s: No buffer for stream%d\n", __func__,
 				HANDLE_TO_IDX(
 				stream_cfg_cmd->stream_handle[i]));
+			mutex_unlock(&vfe_dev->buf_mgr->lock);
 			return rc;
 		}
 
@@ -2019,6 +2022,7 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev,
 				stream_info->stream_src)].active = 1;
 		}
 	}
+	mutex_unlock(&vfe_dev->buf_mgr->lock);
 	msm_isp_update_stream_bandwidth(vfe_dev);
 	vfe_dev->hw_info->vfe_ops.axi_ops.reload_wm(vfe_dev,
 		vfe_dev->vfe_base, wm_reload_mask);
