07bff7e6d64bb95cb00a452b82b428fb259d3cc3
Mohammed Javid <mjavid@codeaurora.org>
2018-05-11 13:54:54 +0530

    msm: ipa: fix to not allow NAT DMA command without device initialization
    Without NAT device initialization sending NAT DMA
    commands leads to XPU violation. Added checks to
    verify device initialized or not before sending DMA
    command.
    
    Change-Id: I7440abc14a81e1621573f0e2808a410d60b2458d
    Acked-by: Ashok Vuyyuru <avuyyuru@qti.qualcomm.com>
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/platform/msm/ipa/ipa_nat.c
+++ drivers/platform/msm/ipa/ipa_nat.c
@@ -324,6 +324,11 @@ int ipa_nat_init_cmd(struct ipa_ioc_v4_nat_init *init)
 	u32 offset = 0;
 	size_t tmp;
 
+	if (!ipa_ctx->nat_mem.is_dev_init) {
+		IPAERR("Nat table not initialized\n");
+		return -EPERM;
+	}
+
 	IPADBG("\n");
 	if (init->table_entries == 0) {
 		IPADBG("Table entries is zero\n");
@@ -565,6 +570,11 @@ int ipa_nat_dma_cmd(struct ipa_ioc_nat_dma_cmd *dma)
 	u16 size = 0, cnt = 0;
 	int ret = 0;
 
+	if (!ipa_ctx->nat_mem.is_dev_init) {
+		IPAERR("Nat table not initialized\n");
+		return -EPERM;
+	}
+
 	IPADBG("\n");
 	if (dma->entries <= 0) {
 		IPADBG("Invalid number of commands\n");
@@ -722,6 +732,16 @@ int ipa_nat_del_cmd(struct ipa_ioc_v4_nat_del *del)
 	u8 mem_type = IPA_NAT_SHARED_MEMORY;
 	u32 base_addr = IPA_NAT_PHYS_MEM_OFFSET;
 	int result;
+
+	if (!ipa_ctx->nat_mem.is_dev_init) {
+		IPAERR("Nat table not initialized\n");
+		return -EPERM;
+	}
+
+	if (ipa_ctx->nat_mem.public_ip_addr) {
+		IPAERR("Public IP addr not assigned and trying to delete\n");
+		return -EPERM;
+	}
 
 	IPADBG("\n");
 	if (ipa_ctx->nat_mem.is_tmp_mem) {
