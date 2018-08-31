0a9012499071e96ff63888d25c6e32e2e257686e
Alok Kumar <alokkuma@codeaurora.org>
2018-08-31 17:00:14 +0530

    qcacld-2.0: Implement descriptor pool for fw stats
    The kernel address is used as cookie to keep track
    of stats request. This address can be disclosed to
    target leading to a security vulnerability.
    
    Implement a FW stats descriptor pool, and use a
    descriptor ID to keep track of stats requests,
    instead of the kernel address, to prevent
    kernel address leak.
    
    Change-Id: Ib49150da899c0b9314f614868a90867f4aa92d3d
    CRs-Fixed: 2276007

--- CORE/SERVICES/WMA/wma.c
+++ CORE/SERVICES/WMA/wma.c
@@ -13964,7 +13964,7 @@ static int32_t wma_txrx_fw_stats_reset(tp_wma_handle wma_handle,
 	}
 	vos_mem_zero(&req, sizeof(req));
 	req.stats_type_reset_mask = value;
-	ol_txrx_fw_stats_get(vdev, &req);
+	ol_txrx_fw_stats_get(vdev, &req, false);
 
 	return 0;
 }
@@ -14037,7 +14037,7 @@ static int32_t wma_set_txrx_fw_stats_level(tp_wma_handle wma_handle,
 			value);
 		return -EINVAL;
 	}
-	ol_txrx_fw_stats_get(vdev, &req);
+	ol_txrx_fw_stats_get(vdev, &req, true);
 
 	return 0;
 }
