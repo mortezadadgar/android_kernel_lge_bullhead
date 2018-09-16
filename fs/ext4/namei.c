82ee55e37eff48658bec4034070b245337dc722d
Pranav Vashi <neobuddy89@gmail.com>
2018-09-16 22:53:11 +0530

    ext4: Clean up tmpfile ifdef leftover from 3.18 backport
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -2496,46 +2496,6 @@ retry:
 	return err;
 }
 
-#if 0
-static int ext4_tmpfile(struct inode *dir, struct dentry *dentry, umode_t mode)
-{
-	handle_t *handle;
-	struct inode *inode;
-	int err, retries = 0;
-
-	dquot_initialize(dir);
-
-retry:
-	inode = ext4_new_inode_start_handle(dir, mode,
-					    NULL, 0, NULL,
-					    EXT4_HT_DIR,
-			EXT4_MAXQUOTAS_INIT_BLOCKS(dir->i_sb) +
-			  4 + EXT4_XATTR_TRANS_BLOCKS);
-	handle = ext4_journal_current_handle();
-	err = PTR_ERR(inode);
-	if (!IS_ERR(inode)) {
-		inode->i_op = &ext4_file_inode_operations;
-		inode->i_fop = &ext4_file_operations;
-		ext4_set_aops(inode);
-		d_tmpfile(dentry, inode);
-		err = ext4_orphan_add(handle, inode);
-		if (err)
-			goto err_unlock_inode;
-		mark_inode_dirty(inode);
-		unlock_new_inode(inode);
-	}
-	if (handle)
-		ext4_journal_stop(handle);
-	if (err == -ENOSPC && ext4_should_retry_alloc(dir->i_sb, &retries))
-		goto retry;
-	return err;
-err_unlock_inode:
-	ext4_journal_stop(handle);
-	unlock_new_inode(inode);
-	return err;
-}
-#endif
-
 static int ext4_tmpfile(struct inode *dir, struct dentry *dentry, umode_t mode)
 {
 	handle_t *handle;
@@ -3849,9 +3809,7 @@ const struct inode_operations ext4_dir_inode_operations = {
 	.mkdir		= ext4_mkdir,
 	.rmdir		= ext4_rmdir,
 	.mknod		= ext4_mknod,
-#if 0
 	.tmpfile	= ext4_tmpfile,
-#endif
 	.rename		= ext4_rename2,
 	.setattr	= ext4_setattr,
 	.setxattr	= generic_setxattr,
