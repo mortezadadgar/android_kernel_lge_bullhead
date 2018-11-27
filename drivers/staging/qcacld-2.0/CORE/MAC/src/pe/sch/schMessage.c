0425722b199a822a8cffc90045af6045136500b3
Qian Zhang <zhangq@codeaurora.org>
2018-11-27 11:44:34 +0800

    qcacld-2.0: Flush beacon template after country code changes
    Country IE dose not match with the region domain setting from
    user layer. It will aLways be the default code in hostapd.conf.
    Flush beacon template after country code is changed.
    
    Change-Id: I49d5aad14c0e409c2ca60831faea8d7e13c44212
    CRs-Fixed: 2354690
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/sch/schMessage.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/sch/schMessage.c
@@ -407,6 +407,8 @@ schQosUpdateBroadcast(tpAniSirGlobal pMac, tpPESession psessionEntry)
 
     if (schSetFixedBeaconFields(pMac,psessionEntry) != eSIR_SUCCESS)
         PELOGE(schLog(pMac, LOGE, "Unable to set beacon fields!");)
+    else
+        limSendBeaconInd(pMac, psessionEntry);
 }
 
 void
