a62a552638ba0af1a89bbd21673190938da6eda0
Josh Choo <dev@joshuous.com>
2019-02-08 23:33:46 +0800

    sched: Turn on MIN_CAPACITY_CAPPING feature
    Inform scheduler about capacity restrictions, such as during frequency
    boosting.

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -106,4 +106,4 @@ SCHED_FEAT(ENERGY_AWARE, true)
  * If enabled, this can be used to inform the scheduler about capacity
  * restrictions.
  */
-SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
+SCHED_FEAT(MIN_CAPACITY_CAPPING, true)
