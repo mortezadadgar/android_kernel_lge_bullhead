55ca275a9db91a12603fc2551a3562971540fd96
Xiaoyu Ye <benyxy@codeaurora.org>
2019-03-03 21:14:10 -0800

    dsp: codecs: fix range check for audio buffer copying
    The range checking for audio buffer copying in function
    "audio_in_write" is using the incorrect buffer size.
    Change it to the actual allocated audio buffer size.
    
    Change-Id: Ib7aaa2163c0d99161369eb85d09dc2d23d8c787b
    Signed-off-by: Xiaoyu Ye <benyxy@codeaurora.org>

--- drivers/misc/qcom/qdsp6v2/audio_utils.c
+++ drivers/misc/qcom/qdsp6v2/audio_utils.c
@@ -873,8 +873,7 @@ ssize_t audio_in_write(struct file *file,
 						__func__, audio->ac->session);
 			}
 		}
-		xfer = (count > (audio->pcm_cfg.buffer_size)) ?
-				(audio->pcm_cfg.buffer_size) : count;
+		xfer = (count > size) ? size : count;
 
 		if (copy_from_user(cpy_ptr, buf, xfer)) {
 			rc = -EFAULT;
