10672f9dea1106dec164bd0df7479779f228c1c6
Chao Yu <yuchao0@huawei.com>
2019-03-05 17:52:33 +0800

    f2fs: fix to use kvfree instead of kzfree
    As Jiqun Li reported in bugzilla:
    
    https://bugzilla.kernel.org/show_bug.cgi?id=202747
    
    System can panic due to using wrong allocate/free function pair
    in xattr interface:
    - use kvmalloc to allocate memory
    - use kzfree to free memory
    
    Let's fix to use kvfree instead of kzfree, BTW, we are safe to
    get rid of kzfree, since there is no such confidential data stored
    as xattr, we don't need to zero it before free memory.
    
    Fixes: 5222595d093e ("f2fs: use kvmalloc, if kmalloc is failed")
    Reported-by: Jiqun Li <jiqun.li@unisoc.com>
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/xattr.c
+++ fs/f2fs/xattr.c
@@ -387,7 +387,7 @@ check:
 	*base_addr = txattr_addr;
 	return 0;
 out:
-	kzfree(txattr_addr);
+	kvfree(txattr_addr);
 	return err;
 }
 
@@ -430,7 +430,7 @@ static int read_all_xattrs(struct inode *inode, struct page *ipage,
 	*base_addr = txattr_addr;
 	return 0;
 fail:
-	kzfree(txattr_addr);
+	kvfree(txattr_addr);
 	return err;
 }
 
@@ -557,7 +557,7 @@ int f2fs_getxattr(struct inode *inode, int index, const char *name,
 	}
 	error = size;
 out:
-	kzfree(base_addr);
+	kvfree(base_addr);
 	return error;
 }
 
@@ -596,7 +596,7 @@ ssize_t f2fs_listxattr(struct dentry *dentry, char *buffer, size_t buffer_size)
 	}
 	error = buffer_size - rest;
 cleanup:
-	kzfree(base_addr);
+	kvfree(base_addr);
 	return error;
 }
 
@@ -727,7 +727,7 @@ static int __f2fs_setxattr(struct inode *inode, int index,
 	if (!error && S_ISDIR(inode->i_mode))
 		set_sbi_flag(F2FS_I_SB(inode), SBI_NEED_CP);
 exit:
-	kzfree(base_addr);
+	kvfree(base_addr);
 	return error;
 }
 
