9077b7789189643ee981e0d2fbe42b9bcd4acf98
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-08 23:02:21 +0900

    blk: disable IO_STAT completely
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- block/blk.h
+++ block/blk.h
@@ -176,9 +176,7 @@ static inline int queue_congestion_off_threshold(struct request_queue *q)
  */
 static inline int blk_do_io_stat(struct request *rq)
 {
-	return rq->rq_disk &&
-	       (rq->cmd_flags & REQ_IO_STAT) &&
-		(rq->cmd_type == REQ_TYPE_FS);
+	return false;
 }
 
 /*
