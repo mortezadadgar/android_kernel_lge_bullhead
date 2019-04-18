25d44a88aceedc50474ebafa40861bd5d06d32ef
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-17 20:06:20 -0700

    msm: kgsl: Remove sync debug name generation from hot path
    Generating a sync debug name with snprintf in a hot path results in
    excessive CPU time wasted on unneeded debug info. Remove the name
    generation entirely to cut down CPU waste in the GPU's rendering hot
    path.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -155,7 +155,6 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 	struct sync_pt *pt;
 	struct sync_fence *fence = NULL;
 	int ret = -EINVAL;
-	char fence_name[sizeof(fence->name)] = {};
 	unsigned int cur;
 
 	priv.fence_fd = -1;
@@ -179,13 +178,8 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 		ret = -ENOMEM;
 		goto unlock;
 	}
-	snprintf(fence_name, sizeof(fence_name),
-		"%s-pid-%d-ctx-%d-ts-%d",
-		device->name, current->group_leader->pid,
-		context_id, timestamp);
 
-
-	fence = sync_fence_create(fence_name, pt);
+	fence = sync_fence_create("", pt);
 	if (fence == NULL) {
 		/* only destroy pt when not added to fence */
 		kgsl_sync_pt_destroy(pt);
