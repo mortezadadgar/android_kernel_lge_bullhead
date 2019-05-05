6009adfbdd5a1373000dcdbe9683a9561456a007
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-05 14:54:15 -0700

    qcacld-2.0: Nuke as much debug bloat as possible
    The overhead from all the debugging in this monstrosity of a driver is
    measurably significant. Chop it all out.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [md: Adapted for 3.10]
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/staging/qcacld-2.0/CORE/SERVICES/BMI/ol_fw.h
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/BMI/ol_fw.h
@@ -134,7 +134,7 @@ void ol_target_failure(void *instance, A_STATUS status);
 u_int8_t ol_get_number_of_peers_supported(struct ol_softc *scn);
 
 #ifdef REMOVE_PKT_LOG
-static inline void ol_pktlog_init(void *)
+static inline void ol_pktlog_init(void *hif_sc)
 {
 }
 #else
--- drivers/staging/qcacld-2.0/CORE/SERVICES/COMMON/pktlog_ac_fmt.h
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/COMMON/pktlog_ac_fmt.h
@@ -25,9 +25,9 @@
  * to the Linux Foundation.
  */
 
-#ifndef REMOVE_PKT_LOG
 #ifndef _PKTLOG_FMT_H_
 #define _PKTLOG_FMT_H_
+#ifndef REMOVE_PKT_LOG
 
 #define CUR_PKTLOG_VER          10010  /* Packet log version */
 #define PKTLOG_MAGIC_NUM        7735225
@@ -70,27 +70,6 @@ struct ath_pktlog_hdr {
     u_int16_t size;
     u_int32_t timestamp;
 }__ATTRIB_PACK;
-
-
-/**
- * enum pkt_type - packet type
- * @START_MONITOR: indicates parser to start packetdump parsing
- * @STOP_MONITOR: indicates parser to stop packetdump parsing
- * @TX_MGMT_PKT: TX management Packet
- * @TX_DATA_PKT: TX data Packet
- * @RX_MGMT_PKT: RX management Packet
- * @RX_DATA_PKT: RX data Packet
- *
- * This enum has packet types
- */
-enum pkt_type {
-	START_MONITOR = 1,
-	STOP_MONITOR,
-	TX_MGMT_PKT,
-	TX_DATA_PKT,
-	RX_MGMT_PKT,
-	RX_DATA_PKT,
-};
 
 #define ATH_PKTLOG_HDR_FLAGS_MASK 0xffff
 #define ATH_PKTLOG_HDR_FLAGS_SHIFT 0
@@ -425,6 +404,8 @@ enum tx_pkt_fate {
 	TX_PKT_FATE_DRV_DROP_OTHER,
 };
 
+#endif /* REMOVE_PKT_LOG */
+
 /**
  * enum rx_pkt_fate - tx packet fate
  * @RX_PKT_FATE_SUCCESS: Valid and delivered to
@@ -466,5 +447,24 @@ enum rx_pkt_fate {
 	RX_PKT_FATE_DRV_DROP_OTHER,
 };
 
+/**
+ * enum pkt_type - packet type
+ * @START_MONITOR: indicates parser to start packetdump parsing
+ * @STOP_MONITOR: indicates parser to stop packetdump parsing
+ * @TX_MGMT_PKT: TX management Packet
+ * @TX_DATA_PKT: TX data Packet
+ * @RX_MGMT_PKT: RX management Packet
+ * @RX_DATA_PKT: RX data Packet
+ *
+ * This enum has packet types
+ */
+enum pkt_type {
+	START_MONITOR = 1,
+	STOP_MONITOR,
+	TX_MGMT_PKT,
+	TX_DATA_PKT,
+	RX_MGMT_PKT,
+	RX_DATA_PKT,
+};
+
 #endif  /* _PKTLOG_FMT_H_ */
-#endif /* REMOVE_PKT_LOG */
