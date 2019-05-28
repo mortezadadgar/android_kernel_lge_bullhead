83a6f615ba3be8f0550636f30e736abf19eca1f1
Chao Yu <yuchao0@huawei.com>
2019-05-28 17:23:33 +0800

    f2fs: fix sparse warning
    make C=2 CHECKFLAGS="-D__CHECK_ENDIAN__"
    
    CHECK   dir.c
    dir.c:842:50: warning: cast from restricted __le32
    CHECK   node.c
    node.c:2759:40: warning: restricted __le32 degrades to integer
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -829,8 +829,8 @@ int f2fs_fill_dentries(struct file *file, void *dirent, filldir_t filldir,
 			int save_len = fstr->len;
 
 			err = fscrypt_fname_disk_to_usr(d->inode,
-						(u32)de->hash_code, 0,
-						&de_name, fstr);
+						(u32)le32_to_cpu(de->hash_code),
+						0, &de_name, fstr);
 			if (err)
 				goto out;
 
