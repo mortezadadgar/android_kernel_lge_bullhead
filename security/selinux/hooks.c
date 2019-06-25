b9caa1da482d96f2bfd25947bb26020a50059489
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-24 20:04:40 -0700

    FUCK YOU @kerneltoast
    Revert "media: v4l2-ioctl: Use a larger on-stack video copy buffer"
    
    This reverts commit 3a8515db9356399642e5f7d939499d58d2d3c910.
    
    Revert "msm: camera: Avoid dynamic memory allocation for small buffers"
    
    This reverts commit 09725ac9ea1f5500488b92bdff154848b0225b50.
    
    Revert "selinux: Avoid dynamic memory allocation for small context buffers"
    
    This reverts commit dd2e7d80cd9f2c4cadf11665213bd7ff40467a6c.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1244,7 +1244,7 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 	struct inode_security_struct *isec = inode->i_security;
 	u32 sid;
 	struct dentry *dentry;
-	char context_onstack[SZ_1K];
+#define INITCONTEXTLEN 255
 	char *context = NULL;
 	unsigned len = 0;
 	int rc = 0;
@@ -1297,11 +1297,19 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 			goto out_unlock;
 		}
 
-		context_onstack[sizeof(context_onstack) - 1] = '\0';
+		len = INITCONTEXTLEN;
+		context = kmalloc(len+1, GFP_NOFS);
+		if (!context) {
+			rc = -ENOMEM;
+			dput(dentry);
+			goto out_unlock;
+		}
+		context[len] = '\0';
 		rc = inode->i_op->getxattr(dentry, XATTR_NAME_SELINUX,
-					   context_onstack,
-					   sizeof(context_onstack));
+					   context, len);
 		if (rc == -ERANGE) {
+			kfree(context);
+
 			/* Need a larger buffer.  Query for the right size. */
 			rc = inode->i_op->getxattr(dentry, XATTR_NAME_SELINUX,
 						   NULL, 0);
@@ -1320,8 +1328,6 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 			rc = inode->i_op->getxattr(dentry,
 						   XATTR_NAME_SELINUX,
 						   context, len);
-		} else {
-			context = context_onstack;
 		}
 		dput(dentry);
 		if (rc < 0) {
@@ -1329,8 +1335,7 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 				printk(KERN_WARNING "SELinux: %s:  getxattr returned "
 				       "%d for dev=%s ino=%ld\n", __func__,
 				       -rc, inode->i_sb->s_id, inode->i_ino);
-				if (context != context_onstack)
-					kfree(context);
+				kfree(context);
 				goto out_unlock;
 			}
 			/* Map ENODATA to the default file SID */
@@ -1354,15 +1359,13 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 					       "returned %d for dev=%s ino=%ld\n",
 					       __func__, context, -rc, dev, ino);
 				}
-				if (context != context_onstack)
-					kfree(context);
+				kfree(context);
 				/* Leave with the unlabeled SID */
 				rc = 0;
 				break;
 			}
 		}
-		if (context != context_onstack)
-			kfree(context);
+		kfree(context);
 		isec->sid = sid;
 		break;
 	case SECURITY_FS_USE_TASK:
