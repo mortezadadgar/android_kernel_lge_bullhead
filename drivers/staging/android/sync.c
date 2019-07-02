289f8bf0b64bbf170a63ff3f01fd81132f51731e
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-02 01:48:24 -0700

    staging: sync: Use an on-stack allocation for fence info ioctl
    Since the fence info ioctl limits output data length to 4096 bytes, we
    can just use a 4096-byte on-stack buffer for it. We have 16 KiB of stack
    space, and this ioctl does not have deep function call depths, so using
    4 KiB of the stack for a temporary buffer here is safe. This ioctl is
    used for every frame rendered to the display, so eliminating dynamic
    memory allocation overhead here improves performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -869,7 +869,8 @@ static int sync_fill_pt_info(struct sync_pt *pt, void *data, int size)
 static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 					unsigned long arg)
 {
-	struct sync_fence_info_data *data;
+	u8 data_buf[4096] __aligned(8);
+	struct sync_fence_info_data *data = (typeof(data))data_buf;
 	struct list_head *pos;
 	__u32 size;
 	__u32 len = 0;
@@ -883,10 +884,6 @@ static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 
 	if (size > 4096)
 		size = 4096;
-
-	data = kzalloc(size, GFP_KERNEL);
-	if (data == NULL)
-		return -ENOMEM;
 
 #ifdef CONFIG_SYNC_DEBUG
 	strlcpy(data->name, fence->name, sizeof(data->name));
@@ -914,8 +911,6 @@ static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 		ret = 0;
 
 out:
-	kfree(data);
-
 	return ret;
 }
 
