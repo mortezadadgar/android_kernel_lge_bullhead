ef93e553c9425ced40ac2fab13fc98cb0da7a896
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-09 21:54:31 +0900

    cgroup: use kmem_cache pool for struct cgrp_cset_link
    These get allocated and freed millions of times on Android.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -336,6 +336,8 @@ struct cg_cgroup_link {
 	struct css_set *cg;
 };
 
+static struct kmem_cache *cg_link_pool;
+
 /* The default css_set - used by init and its children prior to any
  * hierarchies being mounted. It contains a pointer to the root state
  * for each subsystem. Also used to anchor the list of css_sets. Not
@@ -422,7 +424,7 @@ static void __put_css_set(struct css_set *cg, int taskexit)
 		}
 		rcu_read_unlock();
 
-		kfree(link);
+		kmem_cache_free(cg_link_pool, link);
 	}
 
 	write_unlock(&css_set_lock);
@@ -580,7 +582,7 @@ static void free_cg_links(struct list_head *tmp)
 
 	list_for_each_entry_safe(link, saved_link, tmp, cgrp_link_list) {
 		list_del(&link->cgrp_link_list);
-		kfree(link);
+		kmem_cache_free(cg_link_pool, link);
 	}
 }
 
@@ -595,7 +597,7 @@ static int allocate_cg_links(int count, struct list_head *tmp)
 	int i;
 	INIT_LIST_HEAD(tmp);
 	for (i = 0; i < count; i++) {
-		link = kmalloc(sizeof(*link), GFP_KERNEL);
+		link = kmem_cache_alloc(cg_link_pool, GFP_KERNEL);
 		if (!link) {
 			free_cg_links(tmp);
 			return -ENOMEM;
@@ -1756,7 +1758,7 @@ static void cgroup_kill_sb(struct super_block *sb) {
 				 cgrp_link_list) {
 		list_del(&link->cg_link_list);
 		list_del(&link->cgrp_link_list);
-		kfree(link);
+		kmem_cache_free(cg_link_pool, link);
 	}
 	write_unlock(&css_set_lock);
 
@@ -4678,6 +4680,8 @@ int __init cgroup_init(void)
 	int err;
 	int i;
 	unsigned long key;
+
+	cg_link_pool = KMEM_CACHE(cg_cgroup_link, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
 
 	err = bdi_init(&cgroup_backing_dev_info);
 	if (err)
