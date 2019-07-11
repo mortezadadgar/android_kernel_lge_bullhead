21b7cad27da9bc193978319d16db25960f5a8fac
mortezadadgar <mortezadadgar97@gmail.com>
2019-07-11 10:35:45 +0430

    msm: mdss: Don't dynamically allocate memory for overlays
    overlays alloc are in critical path of rendering display
    frames, move it to stack and improving performance.
    
    Co-authored-by: Yaroslav Furman <yaro330@gmail.com>
    Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -4196,7 +4196,9 @@ static int __handle_ioctl_overlay_prepare(struct msm_fb_data_type *mfd,
 {
 	struct mdp_overlay_list ovlist;
 	struct mdp_overlay *req_list[OVERLAY_MAX];
-	struct mdp_overlay *overlays;
+	static struct mdp_overlay overlays[OVERLAY_MAX]
+		____cacheline_aligned_in_smp;
+
 	int i, ret;
 
 	if (!mfd_to_ctl(mfd))
@@ -4210,12 +4212,6 @@ static int __handle_ioctl_overlay_prepare(struct msm_fb_data_type *mfd,
 		return -EINVAL;
 	}
 
-	overlays = kmalloc(ovlist.num_overlays * sizeof(*overlays), GFP_KERNEL);
-	if (!overlays) {
-		pr_err("Unable to allocate memory for overlays\n");
-		return -ENOMEM;
-	}
-
 	if (copy_from_user(req_list, ovlist.overlay_list,
 				sizeof(struct mdp_overlay *) *
 				ovlist.num_overlays)) {
@@ -4225,7 +4221,7 @@ static int __handle_ioctl_overlay_prepare(struct msm_fb_data_type *mfd,
 
 	for (i = 0; i < ovlist.num_overlays; i++) {
 		if (copy_from_user(overlays + i, req_list[i],
-				sizeof(struct mdp_overlay))) {
+				sizeof(overlays))) {
 			ret = -EFAULT;
 			goto validate_exit;
 		}
@@ -4246,7 +4242,6 @@ static int __handle_ioctl_overlay_prepare(struct msm_fb_data_type *mfd,
 		ret = -EFAULT;
 
 validate_exit:
-	kfree(overlays);
 
 	return ret;
 }
