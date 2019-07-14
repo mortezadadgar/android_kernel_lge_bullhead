8f58286b07fcfe398da86be4604d1e09d1d5f09b
mortezadadgar <mortezadadgar97@gmail.com>
2019-07-15 01:23:16 +0430

    devfreq: Correct cpubw for bullhead
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/devfreq/devfreq_devbw.c
+++ drivers/devfreq/devfreq_devbw.c
@@ -232,7 +232,7 @@ int devfreq_add_devbw(struct device *dev)
 		return PTR_ERR(d->df);
 	}
 
-	if (!strcmp(dev_name(dev), "soc:qcom,cpubw"))
+	if (!strcmp(dev_name(dev), "qcom,cpubw.27"))
 		devfreq_register_boost_device(DEVFREQ_MSM_CPUBW, d->df);
 
 	return 0;
