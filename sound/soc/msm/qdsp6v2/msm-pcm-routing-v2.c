98df4a09ef25ad72af5abd77a291fe660d11c5a5
Soumya Managoli <smanag@codeaurora.org>
2019-10-16 16:48:20 +0530

    asoc: msm-routing: Fix array out of bounds issue
    It seems there is out of bound access chances for lsm_app_type_cfg
    array within msm_routing_get_lsm_app_type_cfg_control() callback.
    Added case check to return invalid value if user tries to exceed
    maximum allocated size of array to avoid it.
    
    Change-Id: Ied86e6c9a957255c55bb126a09741fbde429be32
    Signed-off-by: Soumya Managoli <smanag@codeaurora.org>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -3850,17 +3850,20 @@ static int msm_routing_put_app_type_cfg_control(struct snd_kcontrol *kcontrol,
 					  struct snd_ctl_elem_value *ucontrol)
 {
 	int i = 0, j;
-	int num_app_types = ucontrol->value.integer.value[i++];
+	int num_app_types;
 
 	pr_debug("%s\n", __func__);
 
-	memset(app_type_cfg, 0, MAX_APP_TYPES*
-				sizeof(struct msm_pcm_routing_app_type_data));
-	if (num_app_types > MAX_APP_TYPES) {
+	if (ucontrol->value.integer.value[0] > MAX_APP_TYPES) {
 		pr_err("%s: number of app types exceed the max supported\n",
 			__func__);
 		return -EINVAL;
 	}
+
+	num_app_types = ucontrol->value.integer.value[i++];
+	memset(app_type_cfg, 0, MAX_APP_TYPES*
+			sizeof(struct msm_pcm_routing_app_type_data));
+
 	for (j = 0; j < num_app_types; j++) {
 		app_type_cfg[j].app_type =
 				ucontrol->value.integer.value[i++];
