f71a36e6e7d7c7652e1938e7c4e3eb17081a20d0
mortezadadgar <mortezadadgar97@gmail.com>
2019-11-15 04:40:35 +0330

    qcacld-2.0: Nuke rw_wakelock entirely
    It does fire up very frequency which affect
    power consumption directly
    Probably it's safe to remove the code entirely.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/staging/qcacld-2.0/CORE/HDD/inc/wlan_hdd_cfg.h
+++ drivers/staging/qcacld-2.0/CORE/HDD/inc/wlan_hdd_cfg.h
@@ -3805,10 +3805,6 @@ enum dot11p_mode {
 #define CFG_EDCA_BE_AIFS_VALUE_MAX        (15)
 #define CFG_EDCA_BE_AIFS_VALUE_DEFAULT    (3)
 
-#define CFG_RX_WAKELOCK_TIMEOUT_NAME         "rx_wakelock_timeout"
-#define CFG_RX_WAKELOCK_TIMEOUT_DEFAULT      (50)
-#define CFG_RX_WAKELOCK_TIMEOUT_MIN          (0)
-#define CFG_RX_WAKELOCK_TIMEOUT_MAX          (100)
 /*
  * In static display use case when APPS is in stand alone power save mode enable
  * active offload mode which helps FW to filter out MC/BC data packets to avoid
@@ -4641,7 +4637,6 @@ struct hdd_config {
    uint32_t                    edca_vi_aifs;
    uint32_t                    edca_bk_aifs;
    uint32_t                    edca_be_aifs;
-   uint32_t                    rx_wakelock_timeout;
    bool                        active_mode_offload;
 
    bool probe_req_ie_whitelist;
--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg.c
@@ -4541,13 +4541,6 @@ REG_TABLE_ENTRY g_registry_table[] =
                 CFG_EDCA_BE_AIFS_VALUE_MIN,
                 CFG_EDCA_BE_AIFS_VALUE_MAX),
 
-   REG_VARIABLE(CFG_RX_WAKELOCK_TIMEOUT_NAME, WLAN_PARAM_Integer,
-                hdd_config_t, rx_wakelock_timeout,
-                VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
-                CFG_RX_WAKELOCK_TIMEOUT_DEFAULT,
-                CFG_RX_WAKELOCK_TIMEOUT_MIN,
-                CFG_RX_WAKELOCK_TIMEOUT_MAX),
-
    REG_VARIABLE(CFG_ACTIVE_MODE_OFFLOAD, WLAN_PARAM_Integer,
                 hdd_config_t, active_mode_offload,
                 VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
