194e32680b0a66d3276df3d42490deb9d77e5c6b
Qian Zhang <zhangq@codeaurora.org>
2020-01-07 15:22:45 +0800

    qcacld-2.0: Add max index check for dscp_to_up_map array
    qcacld-3.0 to qcacld-2.0 propagation.
    
    In SME layer, boundary check for dscp_to_up_map array is not present.
    
    The dscpmapping is an array of 0x40 elements. Values in dscp_exceptions
    are used to index dscpmapping. The indices are not validated to be less
    than 0x40. The dscp_exceptions array is received from association
    response frame. A malicious AP can send values up to 0xff, causing OOB
    write of dscpmapping array.
    
    Hence, max index check is added to avoid OOB write of dscpmapping array.
    
    Change-Id: I73526849677e867673fc0bd0024ed2b003e4f89e
    CRs-Fixed: 2585141

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2020 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -1382,7 +1382,7 @@ struct hdd_adapter_s
     v_BOOL_t offloads_configured;
 
     /* DSCP to UP QoS Mapping */
-    sme_QosWmmUpType hddWmmDscpToUpMap[WLAN_HDD_MAX_DSCP+1];
+    sme_QosWmmUpType hddWmmDscpToUpMap[WLAN_MAX_DSCP+1];
 
 #ifdef WLAN_FEATURE_LINK_LAYER_STATS
    v_BOOL_t isLinkLayerStatsSet;
