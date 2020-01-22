c222983e007ed46612d8eba24f513eed407b8afe
mortezadadgar <mortezadadgar97@gmail.com>
2020-01-22 17:26:48 +0330

    msm: msm_bus: Reset to 83a00415397464b07e6b3063c7e196ad73ea6dc1
    My perf analyses shown significant CPU overhead is spent
    on msm_bus, reset it LA.BR.1.3.7_rb1.8 and eliminates overhead.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/platform/msm/msm_bus/msm_bus_dbg.c
+++ drivers/platform/msm/msm_bus/msm_bus_dbg.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012, 2014-2017, The Linux Foundation. All rights
+/* Copyright (c) 2010-2012, 2014-2015, 2017 The Linux Foundation. All rights
  * reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -381,7 +381,7 @@ int msm_bus_dbg_rec_transaction(const struct msm_bus_client_handle *pdata,
 			rt_mutex_unlock(&msm_bus_dbg_cllist_lock);
 			return -EINVAL;
 		}
-		pr_err("\n%s setting up debugfs %s", __func__, pdata->name);
+		pr_debug("\n%s setting up debugfs %s", __func__, pdata->name);
 		cldata->file = debugfs_create_file(pdata->name, S_IRUGO,
 				clients, (void *)pdata, &client_data_fops);
 	}
@@ -394,8 +394,8 @@ int msm_bus_dbg_rec_transaction(const struct msm_bus_client_handle *pdata,
 	}
 	buf = cldata->buffer;
 	ts = ktime_to_timespec(ktime_get());
-	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "\n%d.%d\n",
-		(int)ts.tv_sec, (int)ts.tv_nsec);
+	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "\n%ld.%09lu\n",
+		ts.tv_sec, ts.tv_nsec);
 	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "master: ");
 
 	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "%d  ", pdata->mas);
@@ -411,8 +411,7 @@ int msm_bus_dbg_rec_transaction(const struct msm_bus_client_handle *pdata,
 	rt_mutex_unlock(&msm_bus_dbg_cllist_lock);
 
 	trace_bus_update_request((int)ts.tv_sec, (int)ts.tv_nsec,
-		pdata->name, pdata->mas, pdata->slv, ab, ib,
-		pdata->active_only);
+		pdata->name, pdata->mas, pdata->slv, ab, ib);
 
 	return i;
 }
@@ -510,8 +509,8 @@ static int msm_bus_dbg_fill_cl_buffer(const struct msm_bus_scale_pdata *pdata,
 	}
 	buf = cldata->buffer;
 	ts = ktime_to_timespec(ktime_get());
-	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "\n%d.%d\n",
-		(int)ts.tv_sec, (int)ts.tv_nsec);
+	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "\n%ld.%09lu\n",
+		ts.tv_sec, ts.tv_nsec);
 	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "curr   : %d\n", index);
 	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "masters: ");
 
@@ -538,8 +537,7 @@ static int msm_bus_dbg_fill_cl_buffer(const struct msm_bus_scale_pdata *pdata,
 		pdata->usecase[index].vectors[j].src,
 		pdata->usecase[index].vectors[j].dst,
 		pdata->usecase[index].vectors[j].ab,
-		pdata->usecase[index].vectors[j].ib,
-		pdata->active_only);
+		pdata->usecase[index].vectors[j].ib);
 
 	cldata->index = index;
 	cldata->size = i;
@@ -750,8 +748,8 @@ static int msm_bus_dbg_fill_fab_buffer(const char *fabname,
 	buf = fablist->buffer;
 	mutex_unlock(&msm_bus_dbg_fablist_lock);
 	ts = ktime_to_timespec(ktime_get());
-	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "\n%d.%d\n",
-		(int)ts.tv_sec, (int)ts.tv_nsec);
+	i += scnprintf(buf + i, MAX_BUFF_SIZE - i, "\n%ld.%09lu\n",
+		ts.tv_sec, ts.tv_nsec);
 
 	msm_bus_rpm_fill_cdata_buffer(&i, buf, MAX_BUFF_SIZE, cdata,
 		nmasters, nslaves, ntslaves);
@@ -765,6 +763,50 @@ static int msm_bus_dbg_fill_fab_buffer(const char *fabname,
 static const struct file_operations msm_bus_dbg_update_request_fops = {
 	.open = client_data_open,
 	.write = msm_bus_dbg_update_request_write,
+};
+
+static int msm_bus_dbg_dump_clients_open(struct inode *inode, struct file *file)
+{
+	file->private_data = inode->i_private;
+	return 0;
+}
+
+static ssize_t msm_bus_dbg_dump_clients_read(struct file *file,
+	char __user *buf, size_t count, loff_t *ppos)
+{
+	int j, cnt;
+	char msg[50];
+	struct msm_bus_cldata *cldata = NULL;
+
+	cnt = scnprintf(msg, 50,
+		"\nDumping curent client votes to trace log\n");
+	if (*ppos)
+		goto exit_dump_clients_read;
+
+	rt_mutex_lock(&msm_bus_dbg_cllist_lock);
+	list_for_each_entry(cldata, &cl_list, list) {
+		if (IS_ERR_OR_NULL(cldata->pdata))
+			continue;
+		for (j = 0; j < cldata->pdata->usecase->num_paths; j++) {
+			if (cldata->index == -1)
+				continue;
+			trace_bus_client_status(
+			cldata->pdata->name,
+			cldata->pdata->usecase[cldata->index].vectors[j].src,
+			cldata->pdata->usecase[cldata->index].vectors[j].dst,
+			cldata->pdata->usecase[cldata->index].vectors[j].ab,
+			cldata->pdata->usecase[cldata->index].vectors[j].ib,
+			cldata->pdata->active_only);
+		}
+	}
+	rt_mutex_unlock(&msm_bus_dbg_cllist_lock);
+exit_dump_clients_read:
+	return simple_read_from_buffer(buf, count, ppos, msg, cnt);
+}
+
+static const struct file_operations msm_bus_dbg_dump_clients_fops = {
+	.open		= msm_bus_dbg_dump_clients_open,
+	.read		= msm_bus_dbg_dump_clients_read,
 };
 
 /**
@@ -903,6 +945,10 @@ static int __init msm_bus_debugfs_init(void)
 		}
 	}
 	rt_mutex_unlock(&msm_bus_dbg_cllist_lock);
+
+	if (debugfs_create_file("dump_clients", S_IRUGO | S_IWUSR,
+		clients, NULL, &msm_bus_dbg_dump_clients_fops) == NULL)
+		goto err;
 
 	mutex_lock(&msm_bus_dbg_fablist_lock);
 	list_for_each_entry(fablist, &fabdata_list, list) {
