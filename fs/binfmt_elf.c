321b053eb977e2c161b3537cf0e8ebb008477568
Linus Torvalds <torvalds@linux-foundation.org>
2020-01-30 18:09:05 +0330

    Revert "binfmt_elf: switch to new creds when switching to new mm"
    Get fuck...
    
    This reverts commit a74a14b3a1afadd8a302d21bdfa945002d23111a.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -930,7 +930,6 @@ static int load_elf_binary(struct linux_binprm *bprm)
 		current->flags |= PF_RANDOMIZE;
 
 	setup_new_exec(bprm);
-	install_exec_creds(bprm);
 
 	/* Do this so that we can load the interpreter, if need be.  We will
 	   change some of these later */
@@ -1157,6 +1156,7 @@ static int load_elf_binary(struct linux_binprm *bprm)
 
 	kfree(elf_phdata);
 
+	install_exec_creds(bprm);
 	retval = create_elf_tables(bprm, &loc.elf_ex,
 			  load_addr, interp_load_addr);
 	if (retval < 0) {
