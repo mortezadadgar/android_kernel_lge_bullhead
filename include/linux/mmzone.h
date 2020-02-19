b726b41cf190d421323e6ba09383d37e243a54a9
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-18 22:52:50 -0800

    mm: Stop kswapd early when nothing's waiting for it to free pages
    Keeping kswapd running when all the failed allocations that invoked it
    are satisfied incurs a high overhead due to unnecessary page eviction
    and writeback, as well as spurious VM pressure events to various
    registered shrinkers. When kswapd doesn't need to work to make an
    allocation succeed anymore, stop it prematurely to save resources.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -776,6 +776,7 @@ typedef struct pglist_data {
 					     range, including holes */
 	int node_id;
 	nodemask_t reclaim_nodes;	/* Nodes allowed to reclaim from */
+	atomic_t kswapd_waiters;
 	wait_queue_head_t kswapd_wait;
 	wait_queue_head_t pfmemalloc_wait;
 	struct task_struct *kswapd;	/* Protected by lock_memory_hotplug() */
