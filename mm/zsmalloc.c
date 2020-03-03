96d62dd45dee83e527d79aa115d86c81eeb83947
mortezadadgar <mortezadadgar97@gmail.com>
2020-03-03 12:34:40 +0330

    zsmalloc: Fix register shrinker
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1876,7 +1876,8 @@ static int zs_register_shrinker(struct zs_pool *pool)
 	pool->shrinker.batch = 0;
 	pool->shrinker.seeks = DEFAULT_SEEKS;
 
-	return register_shrinker(&pool->shrinker);
+	register_shrinker(&pool->shrinker);
+	return 0;
 }
 
 /**
