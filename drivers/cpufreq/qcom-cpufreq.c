cc5b49a3b0c892f1ba11a591cfc4e7f3adf01b81
mortezadadgar <mortezadadgar97@gmail.com>
2020-03-04 14:14:04 +0330

    cpufreq: Add missing include
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -24,6 +24,7 @@
 #include <linux/cpumask.h>
 #include <linux/suspend.h>
 #include <linux/clk.h>
+#include <linux/clk/msm-clk-provider.h>
 #include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
