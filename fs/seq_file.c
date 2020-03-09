c8fa93fb0c73742cfb3fa81efeda7be5c3532471
mortezadadgar <mortezadadgar97@gmail.com>
2020-03-09 14:49:10 +0330

    Revert "seq_file: use kmem_cache pool for single_open() and single_close()"
    This reverts commit 65bb514e59254f73c0ddfec965cf9431820e2cec.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -17,7 +17,6 @@
 #include <asm/page.h>
 
 static struct kmem_cache *seq_file_cache __ro_after_init;
-static struct kmem_cache *seq_ops_cache __ro_after_init;
 
 /*
  * seq_files have a buffer which can may overflow. When this happens a larger
@@ -602,7 +601,7 @@ static void single_stop(struct seq_file *p, void *v)
 int single_open(struct file *file, int (*show)(struct seq_file *, void *),
 		void *data)
 {
-	struct seq_operations *op = kmem_cache_alloc(seq_ops_cache, GFP_KERNEL);
+	struct seq_operations *op = kmalloc(sizeof(*op), GFP_KERNEL);
 	int res = -ENOMEM;
 
 	if (op) {
@@ -614,7 +613,7 @@ int single_open(struct file *file, int (*show)(struct seq_file *, void *),
 		if (!res)
 			((struct seq_file *)file->private_data)->private = data;
 		else
-			kmem_cache_free(seq_ops_cache, op);
+			kfree(op);
 	}
 	return res;
 }
@@ -642,7 +641,7 @@ int single_release(struct inode *inode, struct file *file)
 {
 	const struct seq_operations *op = ((struct seq_file *)file->private_data)->op;
 	int res = seq_release(inode, file);
-	kmem_cache_free(seq_ops_cache, (void*)op);
+	kfree(op);
 	return res;
 }
 EXPORT_SYMBOL(single_release);
@@ -961,5 +960,4 @@ EXPORT_SYMBOL(seq_hlist_next_rcu);
 void __init seq_file_init(void)
 {
 	seq_file_cache = KMEM_CACHE(seq_file, SLAB_PANIC);
-	seq_ops_cache = KMEM_CACHE(seq_operations, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
 }
