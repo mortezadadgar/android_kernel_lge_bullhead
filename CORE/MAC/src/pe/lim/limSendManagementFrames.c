mydatakeeper/qcacld-2.0-imx

qcacld-2.0: Add Support for FILS Association
Adds following support:
1) Add FILS IE's to assoc request.
2) AEAD encrypt/decrypt the assoc request/response frames for FILS
3) Process/Verify the Assoc response for the FILS params
4) Plumb Auth keys to firmware after successful FILS assoc
5) Update userspace with Auth key details

CRs-Fixed: 2033938
Change-Id: I57f6319cecdf10e08047b510321b885c3cd711b5

CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -52,7 +52,7 @@
 #ifdef WLAN_FEATURE_11W
 #include "wni_cfg.h"
 #endif
-
+#include<crypto/aes.h>
 #ifdef WLAN_FEATURE_VOWIFI_11R
 #include "limFTDefs.h"
 #endif
@@ -64,7 +64,7 @@
 #if defined WLAN_FEATURE_VOWIFI
 #include "rrmApi.h"
 #endif
-
+#include "qdf_crypto.h"
 #include "wlan_qct_wda.h"
 
 #ifdef WLAN_FEATURE_FILS_SK
@@ -2074,6 +2074,10 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
     tANI_U8            *pIe = NULL;
     tANI_U32            ieLen = 0;
     tANI_U32            fixed_param_len = 0;
+#ifdef WLAN_FEATURE_FILS_SK
+    uint32_t aes_block_size_len = 0;
+    VOS_STATUS vos_status;
+#endif
 
     if(NULL == psessionEntry)
     {
@@ -2358,6 +2362,14 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
                                             &pFrm->QComVendorIE,
                                             psessionEntry);
 
+#ifdef WLAN_FEATURE_FILS_SK
+    if (lim_is_fils_connection(psessionEntry)) {
+        populate_dot11f_fils_params(pMac, pFrm, psessionEntry);
+        aes_block_size_len = AES_BLOCK_SIZE;
+    }
+#endif
+
+
     nStatus = dot11fGetPackedAssocRequestSize(pMac, pFrm, &nPayload);
     if (DOT11F_FAILED(nStatus))
     {
@@ -2374,7 +2386,11 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
              nStatus);
     }
 
-    nBytes = nPayload + sizeof(tSirMacMgmtHdr) + nAddIELen;
+    nBytes = nPayload + 
+            sizeof(tSirMacMgmtHdr) + nAddIELen;
+#ifdef WLAN_FEATURE_FILS_SK
+    nBytes += aes_block_size_len;
+#endif
 
     halstatus = palPktAlloc( pMac->hHdd, HAL_TXRX_FRM_802_11_MGMT,
             ( tANI_U16 )nBytes, ( void** ) &pFrame,
@@ -2447,7 +2463,6 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
         psessionEntry->assocReq = NULL;
         psessionEntry->assocReqLen = 0;
     }
-
     if( nAddIELen )
     {
         vos_mem_copy( pFrame + sizeof(tSirMacMgmtHdr) + nPayload,
@@ -2455,6 +2470,22 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
                       nAddIELen );
         nPayload += nAddIELen;
     }
+
+#ifdef WLAN_FEATURE_FILS_SK
+    if (lim_is_fils_connection(psessionEntry)) {
+        vos_status = aead_encrypt_assoc_req(pMac, psessionEntry,
+                                            pFrame, &nPayload);
+        if (!VOS_IS_STATUS_SUCCESS(vos_status)) {
+            limDiagEventReport(pMac,
+                        WLAN_PE_DIAG_ASSOC_IND_EVENT,
+                        psessionEntry, eSIR_FAILURE, eSIR_FAILURE);
+            palPktFree( pMac->hHdd, HAL_TXRX_FRM_802_11_MGMT,
+                        ( void* ) pFrame, ( void* ) pPacket );
+            vos_mem_free(pFrm);
+            return;
+        }
+    }
+#endif
 
     psessionEntry->assocReq = vos_mem_malloc(nPayload);
     if ( NULL == psessionEntry->assocReq )

CORE/SYS/legacy/src/utils/src/parserApi.c
@@ -2320,6 +2320,32 @@ tSirRetStatus sirvalidateandrectifyies(tpAniSirGlobal pMac,
 }
 
 #ifdef WLAN_FEATURE_FILS_SK
+void populate_dot11f_fils_params(tpAniSirGlobal mac_ctx,
+        tDot11fAssocRequest *frm,
+        tpPESession pe_session)
+{
+    struct pe_fils_session *fils_info = pe_session->fils_info;
+
+    /* Populate RSN IE with FILS AKM */
+    PopulateDot11fRSNOpaque(mac_ctx, &(pe_session->pLimJoinReq->rsnIE),
+                                &frm->RSNOpaque);
+
+    /* Populate FILS session IE */
+    frm->fils_session.present = true;
+    vos_mem_copy(frm->fils_session.session,
+             fils_info->fils_session, FILS_SESSION_LENGTH);
+
+    /* Populate FILS Key confirmation IE */
+    if (fils_info->key_auth_len) {
+        frm->fils_key_confirmation.present = true;
+        frm->fils_key_confirmation.num_key_auth =
+                        fils_info->key_auth_len;
+
+        vos_mem_copy(frm->fils_key_confirmation.key_auth,
+                 fils_info->key_auth, fils_info->key_auth_len);
+    }
+}
+
 /**
  * update_fils_data: update fils params from beacon/probe response
  * @fils_ind: pointer to sir_fils_indication
@@ -2923,8 +2949,59 @@ sirConvertAssocReqFrame2Struct(tpAniSirGlobal pMac,
 
 } // End sirConvertAssocReqFrame2Struct.
 
+#ifdef WLAN_FEATURE_FILS_SK
+/**
+ * fils_convert_assoc_rsp_frame2_struct() - Copy FILS IE's to Assoc rsp struct
+ * @ar: frame parser Assoc response struct
+ * @pAssocRsp: LIM Assoc response
+ *
+ * Return: None
+ */
+static void fils_convert_assoc_rsp_frame2_struct(tpAniSirGlobal pMac,
+                         tDot11fAssocResponse *ar,
+                         tpSirAssocRsp pAssocRsp)
+{
+    if (ar->fils_session.present) {
+        limLog(pMac, LOGE, FL("fils session IE present"));
+        pAssocRsp->fils_session.present = true;
+        vos_mem_copy(pAssocRsp->fils_session.session,
+                ar->fils_session.session,
+                DOT11F_IE_FILS_SESSION_MAX_LEN);
+    }
+
+    if (ar->fils_key_confirmation.present) {
+        limLog(pMac, LOGE, FL("fils key conf IE present"));
+        pAssocRsp->fils_key_auth.num_key_auth =
+            ar->fils_key_confirmation.num_key_auth;
+        vos_mem_copy(pAssocRsp->fils_key_auth.key_auth,
+                ar->fils_key_confirmation.key_auth,
+                pAssocRsp->fils_key_auth.num_key_auth);
+    }
+
+    if (ar->fils_kde.present) {
+        limLog(pMac, LOGE,
+                  FL("fils kde IE present %d"),ar->fils_kde.num_kde_list);
+        pAssocRsp->fils_kde.num_kde_list =
+            ar->fils_kde.num_kde_list;
+        vos_mem_copy(pAssocRsp->fils_kde.key_rsc,
+                ar->fils_kde.key_rsc, KEY_RSC_LEN);
+        vos_mem_copy(&pAssocRsp->fils_kde.kde_list,
+                &ar->fils_kde.kde_list,
+                pAssocRsp->fils_kde.num_kde_list);
+    }
+}
+#else
+static inline void fils_convert_assoc_rsp_frame2_struct(tpAniSirGlobal pMac,
+                                 tDot11fAssocResponse *ar,
+                                 tpSirAssocRsp pAssocRsp)
+{ }
+#endif
+
 tSirRetStatus
 sirConvertAssocRespFrame2Struct(tpAniSirGlobal pMac,
+#ifdef WLAN_FEATURE_FILS_SK
+                                tpPESession psessionEntry,
+#endif
                                 tANI_U8            *pFrame,
                                 tANI_U32            nFrame,
                                 tpSirAssocRsp  pAssocRsp)
@@ -2936,6 +3013,18 @@ sirConvertAssocRespFrame2Struct(tpAniSirGlobal pMac,
     // Zero-init our [out] parameter,
     vos_mem_set( ( tANI_U8* )pAssocRsp, sizeof(tSirAssocRsp), 0 );
 
+#ifdef WLAN_FEATURE_FILS_SK
+    /* decrypt the cipher text using AEAD decryption */
+    if (lim_is_fils_connection(psessionEntry)) {
+        status = aead_decrypt_assoc_rsp(pMac, psessionEntry,
+                        &ar, pFrame, &nFrame);
+        if (!VOS_IS_STATUS_SUCCESS(status)) {
+            limLog(pMac, LOGE,
+                  FL("FILS assoc rsp AEAD decrypt fails"));
+            return eSIR_FAILURE;
+        }
+    }
+#endif
     // delegate to the framesc-generated code,
     status = dot11fUnpackAssocResponse( pMac, pFrame, nFrame, &ar);
     if ( DOT11F_FAILED( status ) )
@@ -3151,6 +3240,7 @@ sirConvertAssocRespFrame2Struct(tpAniSirGlobal pMac,
             pAssocRsp->vendor_sub20_capability =
              ar.QComVendorIE.Sub20Info.capability;
 
+    fils_convert_assoc_rsp_frame2_struct(pMac, &ar, pAssocRsp);
     return eSIR_SUCCESS;
 
 } // End sirConvertAssocRespFrame2Struct.

mortezadadgar/android_kernel_lge_bullhead

CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -52,7 +52,7 @@
 #ifdef WLAN_FEATURE_11W
 #include "wni_cfg.h"
 #endif
-
+#include<crypto/aes.h>
 #ifdef WLAN_FEATURE_VOWIFI_11R
 #include "limFTDefs.h"
 #endif
@@ -64,7 +64,7 @@
 #if defined WLAN_FEATURE_VOWIFI
 #include "rrmApi.h"
 #endif
-
+#include "qdf_crypto.h"
 #include "wlan_qct_wda.h"
 #include "lim_process_fils.h"
 
@@ -2071,6 +2071,8 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
     tANI_U8            *pIe = NULL;
     tANI_U32            ieLen = 0;
     tANI_U32            fixed_param_len = 0;
+    VOS_STATUS          vos_status;
+    tANI_U32            aes_block_size_len = 0;
 
     if(NULL == psessionEntry)
     {
@@ -2355,6 +2357,11 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
                                             &pFrm->QComVendorIE,
                                             psessionEntry);
 
+    if (lim_is_fils_connection(psessionEntry)) {
+        populate_dot11f_fils_params(pMac, pFrm, psessionEntry);
+        aes_block_size_len = AES_BLOCK_SIZE;
+    }
+
     nStatus = dot11fGetPackedAssocRequestSize(pMac, pFrm, &nPayload);
     if (DOT11F_FAILED(nStatus))
     {
@@ -2371,7 +2378,8 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
              nStatus);
     }
 
-    nBytes = nPayload + sizeof(tSirMacMgmtHdr) + nAddIELen;
+    nBytes = nPayload + sizeof(tSirMacMgmtHdr) 
+                      + nAddIELen + aes_block_size_len;
 
     halstatus = palPktAlloc( pMac->hHdd, HAL_TXRX_FRM_802_11_MGMT,
             ( tANI_U16 )nBytes, ( void** ) &pFrame,
@@ -2441,13 +2449,26 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
         psessionEntry->assocReq = NULL;
         psessionEntry->assocReqLen = 0;
     }
-
     if( nAddIELen )
     {
         vos_mem_copy( pFrame + sizeof(tSirMacMgmtHdr) + nPayload,
                       pAddIE,
                       nAddIELen );
         nPayload += nAddIELen;
+    }
+
+    if (lim_is_fils_connection(psessionEntry)) {
+        vos_status = aead_encrypt_assoc_req(pMac, psessionEntry,
+                                            pFrame, &nPayload);
+        if (!VOS_IS_STATUS_SUCCESS(vos_status)) {
+            limDiagEventReport(pMac,
+                        WLAN_PE_DIAG_ASSOC_IND_EVENT,
+                        psessionEntry, eSIR_FAILURE, eSIR_FAILURE);
+            palPktFree( pMac->hHdd, HAL_TXRX_FRM_802_11_MGMT,
+                        ( void* ) pFrame, ( void* ) pPacket );
+            vos_mem_free(pFrm);
+            return;
+        }
     }
 
     psessionEntry->assocReq = vos_mem_malloc(nPayload);

CORE/SYS/legacy/src/utils/src/parserApi.c
@@ -2341,6 +2341,32 @@ void sir_copy_hs20_ie(tDot11fIEhs20vendor_ie *dest, tDot11fIEhs20vendor_ie *src)
 }
 
 #ifdef WLAN_FEATURE_FILS_SK
+void populate_dot11f_fils_params(tpAniSirGlobal mac_ctx,
+        tDot11fAssocRequest *frm,
+        tpPESession pe_session)
+{
+    struct pe_fils_session *fils_info = pe_session->fils_info;
+
+    /* Populate RSN IE with FILS AKM */
+    PopulateDot11fRSNOpaque(mac_ctx, &(pe_session->pLimJoinReq->rsnIE),
+                                &frm->RSNOpaque);
+
+    /* Populate FILS session IE */
+    frm->fils_session.present = true;
+    vos_mem_copy(frm->fils_session.session,
+             fils_info->fils_session, FILS_SESSION_LENGTH);
+
+    /* Populate FILS Key confirmation IE */
+    if (fils_info->key_auth_len) {
+        frm->fils_key_confirmation.present = true;
+        frm->fils_key_confirmation.num_key_auth =
+                        fils_info->key_auth_len;
+
+        vos_mem_copy(frm->fils_key_confirmation.key_auth,
+                 fils_info->key_auth, fils_info->key_auth_len);
+    }
+}
+
 /**
  * update_fils_data: update fils params from beacon/probe response
  * @fils_ind: pointer to sir_fils_indication
@@ -2405,11 +2431,6 @@ sir_convert_fils_data_to_probersp_struct(tpSirProbeRespBeacon probe_resp,
 static inline void
 sir_convert_fils_data_to_probersp_struct(tpSirProbeRespBeacon probe_resp,
         tDot11fProbeResponse *pr)
-{
-}
-static inline void populate_dot11f_fils_params(tpAniSirGlobal mac_ctx,
-                 tDot11fAssocRequest *frm,
-                 tpPESession pe_session)
 {
 }
 #endif
@@ -2947,8 +2968,57 @@ sirConvertAssocReqFrame2Struct(tpAniSirGlobal pMac,
 
 } // End sirConvertAssocReqFrame2Struct.
 
+#ifdef WLAN_FEATURE_FILS_SK
+/**
+ * fils_convert_assoc_rsp_frame2_struct() - Copy FILS IE's to Assoc rsp struct
+ * @ar: frame parser Assoc response struct
+ * @pAssocRsp: LIM Assoc response
+ *
+ * Return: None
+ */
+static void fils_convert_assoc_rsp_frame2_struct(tpAniSirGlobal pMac,
+                         tDot11fAssocResponse *ar,
+                         tpSirAssocRsp pAssocRsp)
+{
+    if (ar->fils_session.present) {
+        limLog(pMac, LOG1, FL("fils session IE present"));
+        pAssocRsp->fils_session.present = true;
+        vos_mem_copy(pAssocRsp->fils_session.session,
+                ar->fils_session.session,
+                DOT11F_IE_FILS_SESSION_MAX_LEN);
+    }
+
+    if (ar->fils_key_confirmation.present) {
+        limLog(pMac, LOG1, FL("fils key conf IE present"));
+        pAssocRsp->fils_key_auth.num_key_auth =
+            ar->fils_key_confirmation.num_key_auth;
+        vos_mem_copy(pAssocRsp->fils_key_auth.key_auth,
+                ar->fils_key_confirmation.key_auth,
+                pAssocRsp->fils_key_auth.num_key_auth);
+    }
+
+    if (ar->fils_kde.present) {
+        limLog(pMac, LOG1,
+                  FL("fils kde IE present %d"),ar->fils_kde.num_kde_list);
+        pAssocRsp->fils_kde.num_kde_list =
+            ar->fils_kde.num_kde_list;
+        vos_mem_copy(pAssocRsp->fils_kde.key_rsc,
+                ar->fils_kde.key_rsc, KEY_RSC_LEN);
+        vos_mem_copy(&pAssocRsp->fils_kde.kde_list,
+                &ar->fils_kde.kde_list,
+                pAssocRsp->fils_kde.num_kde_list);
+    }
+}
+#else
+static inline void fils_convert_assoc_rsp_frame2_struct(tpAniSirGlobal pMac,
+                                 tDot11fAssocResponse *ar,
+                                 tpSirAssocRsp pAssocRsp)
+{ }
+#endif
+
 tSirRetStatus
 sirConvertAssocRespFrame2Struct(tpAniSirGlobal pMac,
+                                tpPESession psessionEntry,
                                 tANI_U8            *pFrame,
                                 tANI_U32            nFrame,
                                 tpSirAssocRsp  pAssocRsp)
@@ -2960,6 +3030,18 @@ sirConvertAssocRespFrame2Struct(tpAniSirGlobal pMac,
     // Zero-init our [out] parameter,
     vos_mem_set( ( tANI_U8* )pAssocRsp, sizeof(tSirAssocRsp), 0 );
 
+#ifdef WLAN_FEATURE_FILS_SK
+    /* decrypt the cipher text using AEAD decryption */
+    if (lim_is_fils_connection(psessionEntry)) {
+        status = aead_decrypt_assoc_rsp(pMac, psessionEntry,
+                        &ar, pFrame, &nFrame);
+        if (!VOS_IS_STATUS_SUCCESS(status)) {
+            limLog(pMac, LOGE,
+                  FL("FILS assoc rsp AEAD decrypt fails"));
+            return eSIR_FAILURE;
+        }
+    }
+#endif
     // delegate to the framesc-generated code,
     status = dot11fUnpackAssocResponse( pMac, pFrame, nFrame, &ar);
     if ( DOT11F_FAILED( status ) )
@@ -3175,6 +3257,7 @@ sirConvertAssocRespFrame2Struct(tpAniSirGlobal pMac,
             pAssocRsp->vendor_sub20_capability =
              ar.QComVendorIE.Sub20Info.capability;
 
+    fils_convert_assoc_rsp_frame2_struct(pMac, &ar, pAssocRsp);
     return eSIR_SUCCESS;
 
 } // End sirConvertAssocRespFrame2Struct.