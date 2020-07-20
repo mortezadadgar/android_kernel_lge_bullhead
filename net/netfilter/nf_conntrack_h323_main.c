d606323955f009009749750210c8da81a20a9ae5
mortezadadgar <mortezadadgar97@gmail.com>
2020-07-20 14:31:20 +0000

    netfilter: nf_conntrack_h323: fix maybe-uninitialized warning
    net/netfilter/nf_conntrack_h323_main.c: In function 'h245_help':
    net/netfilter/nf_conntrack_h323_main.c:178:11: warning: 'dataoff' may be used uninitialized in this function [-Wmaybe-uninitialized]
      178 |   tpktoff = *dataoff + *datalen;
          |   ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~
    net/netfilter/nf_conntrack_h323_main.c:588:6: note: 'dataoff' was declared here
      588 |  int dataoff;
          |      ^~~~~~~
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- net/netfilter/nf_conntrack_h323_main.c
+++ net/netfilter/nf_conntrack_h323_main.c
@@ -584,8 +584,8 @@ static int h245_help(struct sk_buff *skb, unsigned int protoff,
 {
 	static MultimediaSystemControlMessage mscm;
 	unsigned char *data = NULL;
-	int datalen;
-	int dataoff;
+	int datalen = 0;
+	int dataoff = 0;
 	int ret;
 
 	/* Until there's been traffic both ways, don't look in packets. */
@@ -1159,8 +1159,8 @@ static int q931_help(struct sk_buff *skb, unsigned int protoff,
 {
 	static Q931 q931;
 	unsigned char *data = NULL;
-	int datalen;
-	int dataoff;
+	int datalen = 0;
+	int dataoff = 0;
 	int ret;
 
 	/* Until there's been traffic both ways, don't look in packets. */
@@ -1237,7 +1237,7 @@ static unsigned char *get_udp_data(struct sk_buff *skb, unsigned int protoff,
 {
 	const struct udphdr *uh;
 	struct udphdr _uh;
-	int dataoff;
+	int dataoff = 0;
 
 	uh = skb_header_pointer(skb, protoff, sizeof(_uh), &_uh);
 	if (uh == NULL)
