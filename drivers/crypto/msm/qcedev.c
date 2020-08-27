adf8248c76f152ab45285bb8b797a825913a42db
mortezadadgar <mortezadadgar97@gmail.com>
2020-08-27 23:39:50 +0000

    crypto: remove extraneous parentheses
    drivers/crypto/msm/qcedev.c:1406:19: warning: equality comparison with extraneous parentheses [-Wparentheses-equality]
                            if ((req->mode == QCEDEV_AES_MODE_XTS)) {
                                 ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1403,7 +1403,7 @@ static int qcedev_check_cipher_key(struct qcedev_cipher_op_req *req,
 			/* if not using HW key make sure key
 			 * length is valid
 			 */
-			if ((req->mode == QCEDEV_AES_MODE_XTS)) {
+			if (req->mode == QCEDEV_AES_MODE_XTS) {
 				if ((req->encklen != QCEDEV_AES_KEY_128*2) &&
 				(req->encklen != QCEDEV_AES_KEY_256*2)) {
 					pr_err("%s: unsupported key size: %d\n",
