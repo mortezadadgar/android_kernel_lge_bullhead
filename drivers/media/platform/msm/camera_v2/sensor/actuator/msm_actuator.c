4a12621687e0e139b68ea1898b3ac06dbb8356ec
mortezadadgar <mortezadadgar97@gmail.com>
2020-08-28 01:09:40 +0000

    msm: sensor: Fix pointer bool conversion
    drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c:96:13: warning: address of array 'a_ctrl->reg_tbl' will always evaluate to 'true' [-Wpointer-bool-conversion]
                    (!a_ctrl->reg_tbl) ||
                     ~~~~~~~~~^~~~~~~
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -93,7 +93,6 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 	CDBG("Enter\n");
 
 	if ((!a_ctrl) ||
-		(!a_ctrl->reg_tbl) ||
 		(!a_ctrl->i2c_reg_tbl)) {
 		pr_err("failed. NULL actuator pointers");
 		return;
