158ff5770e2358bbe0af80153bde02fe71192727
mortezadadgar <mortezadadgar97@gmail.com>
2020-10-04 23:24:07 +0000

    Revert "msm: mdss: Fix race condition between IOMMUv1 attach and sending DCS cmds"
    this mutex lock is moved to mdss_smmu_attach.
    
    This reverts commit d61121adce5bbf7311a3a17b075e38b4d2e3b765.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/video/msm/mdss/mdss_smmu.c
+++ drivers/video/msm/mdss/mdss_smmu.c
@@ -160,7 +160,6 @@ static int mdss_smmu_attach_v1(struct mdss_data_type *mdata)
 	struct mdss_iommu_map_type *iomap;
 	int i, rc = 0;
 
-	mutex_lock(&mdp_iommu_lock);
 	for (i = 0; i < MDSS_IOMMU_MAX_DOMAIN; i++) {
 
 		if (!mdss_smmu_is_valid_domain_type(mdata, i))
@@ -188,7 +187,6 @@ static int mdss_smmu_attach_v1(struct mdss_data_type *mdata)
 		}
 	}
 end:
-	mutex_unlock(&mdp_iommu_lock);
 	return rc;
 }
 
@@ -308,7 +306,6 @@ static int mdss_smmu_detach_v1(struct mdss_data_type *mdata)
 	struct mdss_iommu_map_type *iomap;
 	int i;
 
-	mutex_lock(&mdp_iommu_lock);
 	for (i = 0; i < MDSS_IOMMU_MAX_DOMAIN; i++) {
 		if (!mdss_smmu_is_valid_domain_type(mdata, i))
 			continue;
@@ -323,7 +320,6 @@ static int mdss_smmu_detach_v1(struct mdss_data_type *mdata)
 		}
 		iommu_detach_device(domain, iomap->ctx);
 	}
-	mutex_unlock(&mdp_iommu_lock);
 	return 0;
 }
 
