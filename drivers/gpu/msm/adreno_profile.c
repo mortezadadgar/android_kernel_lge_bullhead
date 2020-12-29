13b76b2d1a9bae767803d97eefb8fcde3c813a0c
Harshitha Sai Neelati <hsaine@codeaurora.org>
2020-12-29 09:08:09 +0530

    msm: kgsl: Compare pid pointer instead of TGID for a new process
    There is a possibility of sharing process_private between two unrelated
    processes due to PID wrapping. In kgsl_process_private_new(), instead
    of checking numeric TGID, compare the unique pid pointer of the current
    process with that of the existing processes in kgsl process list to
    allow sharing of process_private data judiciously. Also, in all required
    functions get TGID/PID of a process from its struct pid.
    
    Change-Id: I0e3d5d79275cdb3f3c304fb36322ad56b0d0b227
    Signed-off-by: Archana Sriram <apsrir@codeaurora.org>
    Signed-off-by: Harshitha Sai Neelati <hsaine@codeaurora.org>

--- drivers/gpu/msm/adreno_profile.c
+++ drivers/gpu/msm/adreno_profile.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014,2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -142,7 +142,7 @@ static void _build_pre_ib_cmds(struct adreno_profile *profile,
 	IB_CMD(ibcmds, CP_MEM_WRITE, gpuaddr + data_offset,
 			drawctxt->base.id, data_offset);
 	IB_CMD(ibcmds, CP_MEM_WRITE, gpuaddr + data_offset,
-			drawctxt->base.proc_priv->pid, data_offset);
+			pid_nr(drawctxt->base.proc_priv->pid), data_offset);
 	IB_CMD(ibcmds, CP_MEM_WRITE, gpuaddr + data_offset,
 			drawctxt->base.tid, data_offset);
 	IB_CMD(ibcmds, CP_MEM_WRITE, gpuaddr + data_offset,
