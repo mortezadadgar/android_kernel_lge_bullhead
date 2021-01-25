cfb70b40b0ed22c70561a7e1f9b7a1d8c11fe839
mortezadadgar <mortezadadgar97@gmail.com>
2021-01-25 13:21:36 +0000

    build: Improve it
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- build.sh
+++ build.sh
@@ -1,47 +1,42 @@
-#!/bin/bash
-date=$(date +%m%d%H%M)
+#!/bin/sh
+
+# Variables
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 last_commit=$(git rev-parse --verify --short=8 HEAD)
-version="Shadow-${date}-${last_commit}"
-cpus=$(nproc --all)
 export LOCALVERSION="-Shadow-Kernel-${branch_name}/${last_commit}"
-RED='\033[0;31m'
-Green='\033[0;32m'
-Yellow='\033[0;33m'
+cpus=$(nproc --all)
+Red='\033[0;31m'
 Blue='\033[0;34m'
 Color_Off='\033[0m'
+defconfig="shadow_defconfig"
+spaces="  "
 
-function die()
+die()
 {
-	echo -e ${RED}
-	echo "${1}" | boxes -d stone
-	exit
+	echo "$spaces | $Red${1} |"
+	exit 1
 }
 
-cd $HOME/bullhead
-echo -e ${Blue}
-echo "Kernel Compilation Started!" |boxes -d stone
-echo -e ${Color_Off}
-# making defconfig
-make shadow_defconfig
-if [ ! $? -eq 0 ]; then
+# fire it!
+echo "\n$spaces| ${Blue}Kernel Compilation Started!${Color_Off} |"
+
+# make defconfig
+if ! make -s "$defconfig"; then
 	die "Defconfig Generation Failed!"
 fi
 
-# making kernel
-make CROSS_COMPILE=aarch64-linux-gnu- -j ${cpus}
-if [ ! $? -eq 0 ]; then
+# make kernel
+if ! make CROSS_COMPILE=aarch64-linux-gnu- -j "${cpus}"; then
 	die "Kernel Compilation failed!"
 fi
 
-# AnyKernel
-cp arch/arm64/boot/Image.gz-dtb $HOME/AnyKernel
-cd $HOME/AnyKernel
-echo -e ${Blue}
-echo "Zipping!" |boxes -d stone
-echo -e ${Color_Off}
-zip ${version}.zip -r * -x \*.zip
-echo -e ${Blue}
-echo "Done! -> ${version}.zip" |boxes -d stone
-echo -e ${Color_Off}
-exit
+# set version
+date=$(date +%m%d%H%M)
+version="Shadow-${date}-${last_commit}"
+
+# create ramdisk
+cp arch/arm64/boot/Image.gz-dtb "$HOME"/bullhead/AnyKernel
+cd "$HOME"/bullhead/AnyKernel || die "cd to anykernel dir failed"
+echo "$spaces| ${Blue}Zipping!${Color_Off} |"
+zip "$version".zip -r ./* -x \*.zip
+echo "$spaces| ${Blue}Done! -> ""$version"".zip${Color_Off} |"
