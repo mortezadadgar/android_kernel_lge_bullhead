e387f94b38441ddd647c9d4fd2e6212912224661
mortezadadgar <mortezadadgar97@gmail.com>
2021-01-28 12:58:14 +0000

    build: build with ccache
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- build.sh
+++ build.sh
@@ -7,18 +7,19 @@ export LOCALVERSION="-Shadow-Kernel-${branch_name}/${last_commit}"
 cpus=$(nproc --all)
 Red='\033[0;31m'
 Blue='\033[0;34m'
-Color_Off='\033[0m'
+color_Off='\033[0m'
 defconfig="shadow_defconfig"
 spaces="  "
+cross_compile="aarch64-linux-gnu-"
+ccache="$(which ccache)"
 
-die()
-{
+die() {
 	echo "$spaces | $Red${1} |"
 	exit 1
 }
 
 # fire it!
-echo "\n$spaces| ${Blue}Kernel Compilation Started!${Color_Off} |"
+echo "\n$spaces| ${Blue}Kernel Compilation Started!${color_Off} |"
 
 # make defconfig
 if ! make -s "$defconfig"; then
@@ -26,7 +27,9 @@ if ! make -s "$defconfig"; then
 fi
 
 # make kernel
-if ! make CROSS_COMPILE=aarch64-linux-gnu- -j "${cpus}"; then
+if ! make -j "${cpus}" \
+	CROSS_COMPILE="$cross_compile" \
+	CC="$ccache ${cross_compile}gcc"; then
 	die "Kernel Compilation failed!"
 fi
 
@@ -37,8 +40,8 @@ version="Shadow-${date}-${last_commit}"
 # create ramdisk
 cp arch/arm64/boot/Image.gz-dtb "$HOME"/bullhead/AnyKernel
 cd "$HOME"/bullhead/AnyKernel || die "cd to anykernel dir failed"
-echo "$spaces| ${Blue}Zipping!${Color_Off} |"
+echo "$spaces| ${Blue}Zipping!${color_Off} |"
 zip "$version".zip -r ./* -x \*.zip
-echo "$spaces| ${Blue}Done! -> ""$version"".zip${Color_Off} |"
+echo "$spaces| ${Blue}Done! -> ""$version"".zip${color_Off} |"
 
 notify-send "Kernel" "Compilation is done!"
