e7619770153fa09d5a3c2d6a6bb6d5809750c10e
mortezadadgar <mortezadadgar97@gmail.com>
2021-01-28 13:26:27 +0000

    soc: msm-pcm: Fix forbids mixed declarations
    sound/soc/msm/qdsp6v2/msm-pcm-loopback-v2.c: In function 'msm_pcm_volume_ctl_put':
    sound/soc/msm/qdsp6v2/msm-pcm-loopback-v2.c:362:2: warning: ISO C90 forbids mixed declarations and code [-Wdeclaration-after-statement]
      362 |  struct msm_pcm_loopback *prtd = substream->runtime->private_data;
          |  ^~~~~~
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- sound/soc/msm/qdsp6v2/msm-pcm-loopback-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-loopback-v2.c
@@ -358,10 +358,11 @@ static int msm_pcm_volume_ctl_put(struct snd_kcontrol *kcontrol,
 	int rc = 0;
 	struct snd_pcm_volume *vol = kcontrol->private_data;
 	struct snd_pcm_substream *substream = vol->pcm->streams[0].substream;
-	mutex_lock(&loopback_session_lock);
-	struct msm_pcm_loopback *prtd = substream->runtime->private_data;
+	struct msm_pcm_loopback *prtd;
 	int volume = ucontrol->value.integer.value[0];
 
+	mutex_lock(&loopback_session_lock);
+	prtd = substream->runtime->private_data;
 	rc = pcm_loopback_set_volume(prtd, volume);
 	mutex_unlock(&loopback_session_lock);
 	return rc;
