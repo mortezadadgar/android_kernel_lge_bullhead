993650b09453122305758b906cb3c74ba256f612
mortezadadgar <mortezadadgar97@gmail.com>
2021-03-02 18:29:37 +0000

    build: use zsh print command
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
 # Variables
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 last_commit=$(git rev-parse --verify --short=8 HEAD)
 export LOCALVERSION="-Shadow-Kernel-${branch_name}/${last_commit}"
 cpus=$(nproc --all)
-Red='\033[0;31m'
-Blue='\033[0;34m'
 color_Off='\033[0m'
 defconfig="shadow_defconfig"
 spaces="  "
@@ -14,12 +12,12 @@ cross_compile="aarch64-linux-gnu-"
 ccache="$(which ccache)"
 
 die() {
-	echo "$spaces | $Red${1} |"
+	print -P "$spaces | %F{red}${1} |$f"
 	exit 1
 }
 
 # fire it!
-echo "\n$spaces| ${Blue}Kernel Compilation Started!${color_Off} |"
+print -P "\n$spaces| %F{blue}Kernel Compilation Started! |%f"
 
 # make defconfig
 if ! make -s "$defconfig"; then
@@ -40,8 +38,8 @@ version="Shadow-${date}-${last_commit}"
 # create ramdisk
 cp arch/arm64/boot/Image.gz-dtb "$HOME"/bullhead/AnyKernel
 cd "$HOME"/bullhead/AnyKernel || die "cd to anykernel dir failed"
-echo "$spaces| ${Blue}Zipping!${color_Off} |"
+print -P "$spaces| %F{blue}Zipping!%f |"
 zip "$version".zip -r ./* -x \*.zip
-echo "$spaces| ${Blue}Done! -> ""$version"".zip${color_Off} |"
+print -P "$spaces| %F{green}Done!%f -> %F{blue}""$version"".zip%f |"
 
 notify-send "Kernel" "Compilation is done!"
