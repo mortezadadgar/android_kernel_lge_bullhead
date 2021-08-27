8feaf454e1382c7a8291f9f2511176adb17b5dfa
mortezadadgar <mortezadadgar97@gmail.com>
2021-08-27 19:18:06 +0430

    Revert "build: use zsh print command"
    This reverts commit 993650b09453122305758b906cb3c74ba256f612.

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-#!/bin/zsh
+#!/bin/sh
 
 # Variables
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 last_commit=$(git rev-parse --verify --short=8 HEAD)
 export LOCALVERSION="-Shadow-Kernel-${branch_name}/${last_commit}"
 cpus=$(nproc --all)
+Red='\033[0;31m'
+Blue='\033[0;34m'
 color_Off='\033[0m'
 defconfig="shadow_defconfig"
 spaces="  "
@@ -12,12 +14,12 @@ cross_compile="aarch64-linux-gnu-"
 ccache="$(which ccache)"
 
 die() {
-	print -P "$spaces | %F{red}${1} |$f"
+	echo "$spaces | $Red${1} |"
 	exit 1
 }
 
 # fire it!
-print -P "\n$spaces| %F{blue}Kernel Compilation Started! |%f"
+echo "\n$spaces| ${Blue}Kernel Compilation Started!${color_Off} |"
 
 # make defconfig
 if ! make -s "$defconfig"; then
@@ -38,8 +40,8 @@ version="Shadow-${date}-${last_commit}"
 # create ramdisk
 cp arch/arm64/boot/Image.gz-dtb "$HOME"/bullhead/AnyKernel
 cd "$HOME"/bullhead/AnyKernel || die "cd to anykernel dir failed"
-print -P "$spaces| %F{blue}Zipping!%f |"
+echo "$spaces| ${Blue}Zipping!${color_Off} |"
 zip "$version".zip -r ./* -x \*.zip
-print -P "$spaces| %F{green}Done!%f -> %F{blue}""$version"".zip%f |"
+echo "$spaces| ${Blue}Done! -> ""$version"".zip${color_Off} |"
 
 notify-send "Kernel" "Compilation is done!"
