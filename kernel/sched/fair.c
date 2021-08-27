d28695b8c9eafe42a8f28abb1e8ef665ac5ef89c
mortezadadgar <mortezadadgar97@gmail.com>
2021-08-27 18:44:15 +0430

    Revert "sched: check pinned tasks before balance"
    This reverts commit d080b1345f7ff0f22c3729f4a55e1d81d56e80a2.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4879,10 +4879,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	if (!se) {
 		update_rq_runnable_avg(rq, rq->nr_running);
 		inc_nr_running(rq);
-
-		if (unlikely(p->nr_cpus_allowed == 1))
-			rq->nr_pinned_tasks++;
-
 		inc_rq_hmp_stats(rq, p, 1);
 	}
 	hrtick_update(rq);
@@ -4946,10 +4942,6 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!se) {
 		dec_nr_running(rq);
-
-		if (unlikely(p->nr_cpus_allowed == 1))
-			rq->nr_pinned_tasks--;
-
 		update_rq_runnable_avg(rq, 1);
 		dec_rq_hmp_stats(rq, p, 1);
 	}
@@ -8107,22 +8099,6 @@ static inline int _nohz_kick_needed_hmp(struct rq *rq, int cpu, int *type)
 		(rq->nr_running - rq->hmp_stats.nr_small_tasks >= 2 ||
 		rq->nr_running > rq->mostly_idle_nr_run ||
 		cpu_load(cpu) > rq->mostly_idle_load)) {
-
-		if (unlikely(rq->nr_pinned_tasks > 0)) {
-			int delta = rq->nr_running - rq->nr_pinned_tasks;
-
-			/*
-			 * Check if it is possible to "unload" this CPU in case
-			 * of having pinned/affine tasks. Do not disturb idle core
-			 * if one of the below condition is true:
-			 *
-			 * - there is one pinned task and it is not "current"
-			 * - all tasks are pinned to this CPU
-			 */
-			if (delta < 2)
-				if (current->nr_cpus_allowed > 1 || !delta)
-					return 0;
-		}
 
 		if (rq->capacity == max_capacity)
 			return 1;
